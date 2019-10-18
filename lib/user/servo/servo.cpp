b6a727d9375a106fd96e63d5819f253e81aebd24
KrillNye <43658159+KrillNye@users.noreply.github.com>
2019-10-17 23:31:30 -0400

    Incomplete

---
+++ lib/user/servo/servo.cpp
@@ -0,0 +1,94 @@
+#include "mbed.h"
+#include "servo.h"
+
+Servo::Servo(int rotate_type_, int range_, float max_pulse_ms_, float min_pulse_ms_): 
+rotate_type(rotate_type_),
+range(range_),
+max_pulse_ms(max_pulse_ms_),
+min_pulse_ms(min_pulse_ms_) {}
+
+Servo::Servo(int rotate_type_, int range_):
+rotate_type(rotate_type_),
+range(range_)
+{
+    if(rotate_type == LIMITED_SERVO)
+    {
+        max_pulse_ms = DEFAULT_MAX;
+        min_pulse_ms = DEFAULT_MIN;
+    }
+    else
+    {
+        max_pulse_ms = NULL;
+        min_pulse_ms = NULL;
+    }
+}
+
+Servo::Servo(int rotate_type_):
+rotate_type(rotate_type_)
+{
+    if(rotate_type == LIMITED_SERVO)
+    {
+        max_pulse_ms = DEFAULT_MAX;
+        min_pulse_ms = DEFAULT_MIN;
+        range = DEFAULT_RANGE;
+    }
+    else
+    {
+        max_pulse_ms = NULL;
+        min_pulse_ms = NULL;
+        range = NULL;
+    }
+}
+
+Servo::Servo(PinName pin_, int rotate_type_, int range_, float max_pulse_ms_, float min_pulse_ms_): 
+pin(pin_),
+rotate_type(rotate_type_),
+range(range_),
+max_pulse_ms(max_pulse_ms_),
+min_pulse_ms(min_pulse_ms_) {}
+
+Servo::Servo(PinName pin_, int rotate_type_, int range_):
+pin(pin_),
+rotate_type(rotate_type_),
+range(range_);
+{
+    if(rotate_type == LIMITED_SERVO)
+    {
+        max_pulse_ms = DEFAULT_MAX;
+        min_pulse_ms = DEFAULT_MIN;
+    }
+    else
+    {
+        max_pulse_ms = NULL;
+        min_pulse_ms = NULL;
+    }
+}
+
+Servo::Servo(PinName pin_, int rotate_type_):
+pin(pin_),
+rotate_type(rotate_type_)
+{
+    if(rotate_type == LIMITED_SERVO)
+    {
+        max_pulse_ms = DEFAULT_MAX;
+        min_pulse_ms = DEFAULT_MIN;
+        range = DEFAULT_RANGE;
+    }
+    else
+    {
+        max_pulse_ms = NULL;
+        min_pulse_ms = NULL;
+        range = NULL;
+    }
+}
+
+bool Servo::move(float angle)
+{
+    if(rotate_type != LIMITED_SERVO || angle > range || pin == NULL)
+        return false;
+    else
+    {
+        
+    }
+        
+}
--- lib/user/servo/servo.h
+++ lib/user/servo/servo.h
@@ -1,39 +1,43 @@
 #ifndef SERVO_H
 #define SERVO_H
 
-#include mbed.h
+#include "mbed.h"
 
-#define CONT_SERVO = 0              //CONTINUOUS Rotation
-#define LIMIT_SERVO = 1             //LIMITED Range Rotation
+#define CONT_SERVO = 0                      //CONTINUOUS Rotation
+#define LIMITED_SERVO = 1                     //LIMITED Range Rotation
 
-#define LENGTH_CTRL = 0             //PULSE LENGTH Positional Control; Position scales with length of waves, 40Hz < frequency < 200Hz is acceptable [defaulting to 50Hz]
-#define DUTY_CTRL = 1               //DUTY CYCLE Positional Control; Position scales with on-time of PWM wave, frequency is set at 50Hz
-#define PWM_FREQ = 50               //DEFAULT PWM FREQUENCY, should work for both length and positional control
+#define PWM_FREQ = 50                       //DEFAULT PWM FREQUENCY, should work for both length and positional control
+#define DEFAULT_MAX = 2                     //DEFAULT MAX WAVE LENGTH in MILISECONDS
+#define DEFAULT_MIN = 1                     //DEFAULT MIN WAVE LENGTH in MILISECONDS
+#define DEFAULT_RANGE = 180                 //DEFAULT RANGE for LIMITED Servos
 
-#define NULL = -1                   //PLACEHOLDER for irrelevant values in constructors
+#define NULL = -1                           //PLACEHOLDER for irrelevant values in constructors
 
     class Servo{
         private:
-            int     rotate_type,        //Either CONTINUOUS or LIMITED rotation servo, see define statements.
-                    control_type,       //If LENGTH_CTRL, linearly scale using pulse_ms; if DUTY_CTRL, linearly scale using duty cycle
-                                        //Typically duty cycle maps 10% to MAX and 5% to MIN of range; Assuming so unless duty cycle is explicitly stated
+            PinName pin;                    //PIN the servo PWM signal is attached to
 
-                    range;              //range of motion, only valid for LIMIT_SERVO types
+            int     rotate_type,            //Either CONTINUOUS or LIMITED rotation servo, see define statements.
+                    range;                  //range of motion, only valid for LIMIT_SERVO types
 
-            float   max_pulse_ms,       //PULSE LENGTH for MAX ANGLE 
-                    min_pulse_ms,       //PULSE LENGTH for MIN ANGLE
-                    max_cycle,          //DUTY CYCLE for MAX ANGLE
-                    min_cycle;          //DUTY CYCLE for MIN ANGLE
+            float   max_pulse_ms,           //PULSE LENGTH for MAX ANGLE 
+                    min_pulse_ms,           //PULSE LENGTH for MIN ANGLE
+                    pos,                    //POSITION of servo
+                    speed;                  //ROTATING SPEED in ANGLES PER SECOND
 
             const int PERIOD = 1000 / PWM_FREQ; //DEFAULT PERIOD LENGTH in MILISECONDS
 
         public:
-            Servo(int rotate_type_, int control_type_, int range_, float max_pulse_ms_, float min_pulse_ms_, float max_cycle_, float min_cycle_);
-            Servo(int rotate_type_, int control_type_);
-            Servo(int rotate_type_, int control_type_, int range_);
+            Servo(int rotate_type_, int range_, float max_pulse_ms_, float min_pulse_ms_);
+            Servo(int rotate_type_);                //If LIMITED, ASSUMING DEFAULT MAX, MIN, RANGE
+            Servo(int rotate_type_, int range_);    //If LIMITED, ASSUMING DEFAULT MAX, MIN
 
             bool set_range(int range);      //Returns FALSE if Servo Type is CONTINUOUS
             bool move(float angle);         //Returns FALSE if ANGLE > RANGE or Servo Type is CONTINUOUS
-    }
+            bool set_speed(float speed);    //Returns FALSE if Servo Type is LIMITED
 
+            void attach(PinName pin_);
+
+            float read(void);               //Returns SPEED if CONTINUOUS and POSITION if LIMITED
+    }
 #endif
