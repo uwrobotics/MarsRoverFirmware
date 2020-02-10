134a2f8ac1cb6c6a53e63d19e018b5b3425c2fe8
Alex Petkovic <16639190+jetkov@users.noreply.github.com>
2020-02-10 02:54:58 -0500

    CAN Improvements (#51)
    * WIP
    
    * Actuator Framework (#36) (#40)
    
    * WIP
    
    * Remove macOS cancer
    
    * Update gitignore
    
    * Cleanup
    
    * WIP
    
    * Actuator Controller WIP --> Using abstract encoders etc.
    
    * Actuator Controller WIP
    
    * WIP
    
    * Ported PID library from 2019 repo, added autotuning + minor adjustments. Created PID test app (#29) (#31)
    
    * Added old PID library, made improvements, added autotuning and added test PID app
    
    * Removed old struct not needed
    
    Co-authored-by: tandronescu <30327537+tandronescu@users.noreply.github.com>
    
    * Rough actuator class done
    
    * WIP
    
    * Actuator Controller compiles
    
    * Added relative PWM encoder & relevant code
    
    * Config cleanup/addition
    
    * Added QEI w/ EWMA smoothing for velocity
    
    * WIP - Fixed some virtual stuff
    
    * wip
    
    * Acutator controller compiling with driver
    
    * Acutator controller compiling with driver
    
    * undelete blinky
    
    Co-authored-by: tandronescu <30327537+tandronescu@users.noreply.github.com>
    
    Co-authored-by: tandronescu <30327537+tandronescu@users.noreply.github.com>
    
    * Add improved CAN message wrapper
    
    * WIP
    
    * WIP - compiling CAN buffer
    
    * WIP
    
    * WIP
    
    * Ready to merge -- to be tested by arm application
    
    Co-authored-by: tandronescu <30327537+tandronescu@users.noreply.github.com>

---
+++ lib/can/CANMsg.h
@@ -0,0 +1,79 @@
+#pragma once
+
+#include "mbed.h"
+
+class CANMsg : public CANMessage {
+    
+public:
+
+    template <class T>
+    union CANPayload {
+        unsigned char data[8];
+        T value;
+    };
+
+    /** Creates empty CAN message.
+     */
+    CANMsg() :
+        CANMessage() {}
+
+    /** Creates CAN message with specific content.
+     */
+    CANMsg(int _id, const char *_data, char _len = 8, CANType _type = CANData, CANFormat _format = CANStandard) :
+        CANMessage(_id, _data, _len, _type, _format) {}
+
+    /** Creates CAN remote message.
+     */
+    CANMsg(int _id, CANFormat _format = CANStandard) :
+        CANMessage(_id, _format) {}
+
+    /** Clears CAN message content
+     */
+    void clear(void) {
+        len    = 0;
+        type   = CANData;
+        format = CANStandard;
+        id     = 0;
+        memset(data, 0, 8);
+    };
+
+    /** Set the payload data
+     */
+    template<class T>
+    void setPayload(const T value) {
+        MBED_ASSERT_WARN(sizeof(T) <= 8); // Make sure input type fits
+        CANPayload<T> * payload = data;
+        payload->value = value;
+        len = sizeof(T);
+    }
+
+    /** Get the payload data
+     */
+    template<class T>
+    void getPayload(T& val) {
+        MBED_ASSERT_WARN(len = sizeof(T)); // Make sure output fits
+        CANPayload<T> * payload = data;
+        val = payload->value;
+    }
+
+    /** Append operator: Appends data (value) to CAN message
+     */
+    template<class T>
+    CANMsg &operator<<(const T val) {
+        MBED_ASSERT_WARN(len + sizeof(T) <= 8);
+        *reinterpret_cast<T*>(&data[len]) = val;
+        len += sizeof(T);
+        return *this;
+    }
+
+    /** Extract operator: Extracts data (value) from CAN message
+     */
+    template<class T>
+    CANMsg &operator>>(T& val) {
+        MBED_ASSERT_WARN(sizeof(T) <= len);
+        val = *reinterpret_cast<T*>(&data[0]);
+        len -= sizeof(T);
+        memcpy(data, data + sizeof(T), len);
+        return *this;
+    }
+};
