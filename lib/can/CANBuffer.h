24bf074449ac7802e925ccf3c2b346f59d301558
Alex Petkovic <16639190+jetkov@users.noreply.github.com>
2020-02-09 23:39:45 -0800

    Ready to merge -- to be tested by arm application

--- lib/can/CANBuffer.h
+++ lib/can/CANBuffer.h
@@ -7,6 +7,7 @@
 #include "mbed.h"
 #include "CircularBuffer.h"
 #include "CANMsg.h"
+#include <stdint.h>
 
 class CANBuffer : CircularBuffer<CANMessage, CANBUFFER_DEFAULT_SIZE> {
 
@@ -18,6 +19,40 @@ public:
     };
 
     CANBuffer(CAN * CANInterface, BufferType type = rx);
+
+    /** Pop the transaction from the buffer
+     *
+     * @param data Data to be popped from the buffer
+     * @return True if the buffer is not empty and data contains a transaction, false otherwise
+     */
+    bool pop(CANMsg &canMSG);
+
+    /** Check if the buffer is empty
+     *
+     * @return True if the buffer is empty, false if not
+     */
+    bool empty() const;
+
+    /** Check if the buffer is full
+     *
+     * @return True if the buffer is full, false if not
+     */
+    bool full() const;
+
+    /** Reset the buffer
+     *
+     */
+    void reset();
+
+    /** Get the number of elements currently stored in the circular_buffer */
+    uint32_t size() const;
+
+    /** Peek into circular buffer without popping
+     *
+     * @param data Data to be peeked from the buffer
+     * @return True if the buffer is not empty and data contains a transaction, false otherwise
+     */
+    bool peek(CANMsg &canMSG) const;
 
 private:
 
