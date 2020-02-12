ded03024f30691e9f43d1c707dc021458a941996
Alex Petkovic <16639190+jetkov@users.noreply.github.com>
2020-02-12 02:48:16 -0800

    cleanup

--- apps/test-can/main.cpp
+++ apps/test-can/main.cpp
@@ -1,7 +1,6 @@
 #include "mbed.h"
 #include "CANMsg.h"
 
-Serial              pc(SERIAL_TX, SERIAL_RX);
 CAN                 can(CAN_RX, CAN_TX);
 CANMsg              rxMsg;
 CANMsg              txMsg;
@@ -10,8 +9,8 @@ DigitalOut          ledRX(LED2);
 Timer               timer;
 uint8_t             counter = 0;
 
-#define TX_ID = 100;
-#define RX_ID = 101;
+#define TX_ID 0x100
+#define RX_ID 0x101
  
 /**
  * @brief   Prints CAN msg to PC's serial terminal
@@ -20,14 +19,14 @@ uint8_t             counter = 0;
  * @retval
  */
 void printMsg(CANMessage& msg) {
-    pc.printf("  ID      = 0x%.3x\r\n", msg.id);
-    pc.printf("  Type    = %d\r\n", msg.type);
-    pc.printf("  Format  = %d\r\n", msg.format);
-    pc.printf("  Length  = %d\r\n", msg.len);
-    pc.printf("  Data    =");
+    printf("  ID      = 0x%.3x\r\n", msg.id);
+    printf("  Type    = %d\r\n", msg.type);
+    printf("  Format  = %d\r\n", msg.format);
+    printf("  Length  = %d\r\n", msg.len);
+    printf("  Data    =");
     for(int i = 0; i < msg.len; i++)
-        pc.printf(" 0x%.2X", msg.data[i]);
-    pc.printf("\r\n");
+        printf(" 0x%.2X", msg.data[i]);
+    printf("\r\n");
 }
  
 int main(void)
@@ -36,7 +35,7 @@ int main(void)
     ledTX = 0;                          // set transmit LED off
     ledRX = 0;                          // set recieve LED off
     timer.start();                      // start timer
-    pc.printf("CAN_Hello\r\n");
+    printf("CAN_Hello\r\n");
 
     while(1) {
         if(timer.read_ms() >= 1000) {   // check for timeout
@@ -44,29 +43,28 @@ int main(void)
             counter++;                  // increment counter
             txMsg.clear();              // clear Tx message storage
             txMsg.id = TX_ID;           // set ID
-            txMsg.setPayload(counter);  // copy counter value to CAN msg payload
+            txMsg << counter;  // copy counter value to CAN msg payload
             if(can.write(txMsg)) {      // transmit message
-                led = 0;                // turn the LED off
-                pc.printf("-------------------------------------\r\n");
-                pc.printf("CAN message sent\r\n");
+                printf("-------------------------------------\r\n");
+                printf("CAN message sent\r\n");
                 printMsg(txMsg);
-                pc.printf("  counter = %d\r\n", counter);
+                printf("  counter = %d\r\n", counter);
                 ledTX = !ledTX;
             }
             else
-                pc.printf("Transmission error\r\n");
+                printf("Transmission error\r\n");
         }
  
         if(can.read(rxMsg)) {
             ledRX = !ledRX;       // turn the LED on
-            pc.printf("-------------------------------------\r\n");
-            pc.printf("CAN message received\r\n");
+            printf("-------------------------------------\r\n");
+            printf("CAN message received\r\n");
             printMsg(rxMsg);
  
             // Filtering performed by software:
             if(rxMsg.id == RX_ID) {
                 rxMsg >> counter;    // extract data from the received CAN message
-                pc.printf("  counter = %d\r\n", counter);
+                printf("  counter = %d\r\n", counter);
                 timer.start();                  // transmission lag
             }
         }
