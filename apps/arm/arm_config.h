0b6e99978c14d3b39e3f33f67ff5ed5479fdad9b
Alex Petkovic <16639190+jetkov@users.noreply.github.com>
2020-02-13 03:54:09 -0500

    WIP- compiling

--- apps/arm/arm_config.h
+++ apps/arm/arm_config.h
@@ -17,93 +17,93 @@ Encoder::t_encoderConfig turnTableEncoderConfig = {
 };
 
 static Encoder::t_encoderConfig shoulderEncoderConfig = {
-        .pin_PWM = ENC_PWM_TRNTBL;
-        .degreesPerUnit = 360;
-        .inverted = false;
+        .pin_PWM = ENC_PWM_TRNTBL,
+        .degreesPerUnit = 360,
+        .inverted = false
 };
 
 static Encoder::t_encoderConfig elbowEncoderConfig = {
-        .pin_PWM = ENC_PWM_TRNTBL;
-        .degreesPerUnit = 360;
-        .inverted = false;
+        .pin_PWM = ENC_PWM_TRNTBL,
+        .degreesPerUnit = 360,
+        .inverted = false
 };
 
 static Encoder::t_encoderConfig wristLeftEncoderConfig = {
-        pin_ChannelA = ENC_QUAD_WRST_LHS_A,
-        pin_ChannelB = ENC_QUAD_WRST_LHS_A,
+        .pin_ChannelA = ENC_QUAD_WRST_LHS_A,
+        .pin_ChannelB = ENC_QUAD_WRST_LHS_A,
 
-        degreesPerUnit = 1.0,
-        inverted = false
+        .degreesPerUnit = 1.0,
+        .inverted = false
 };
 
 static Encoder::t_encoderConfig wristRightEncoderConfig = {
-        pin_ChannelA = ENC_QUAD_WRST_RHS_A,
-        pin_ChannelB = ENC_QUAD_WRST_RHS_B,
+        .pin_ChannelA = ENC_QUAD_WRST_RHS_A,
+        .pin_ChannelB = ENC_QUAD_WRST_RHS_B,
 
-        degreesPerUnit = 1.0,
-        inverted = false
+        .degreesPerUnit = 1.0,
+        .inverted = false
 };
 
 static Encoder::t_encoderConfig clawEncoderConfig = {
-        pin_ChannelA = ENC_CLAW_QUAD_A,
-        pin_ChannelB = ENC_CLAW_QUAD_B,
+        .pin_ChannelA = ENC_QUAD_CLAW_A,
+        .pin_ChannelB = ENC_QUAD_CLAW_B,
 
-        degreesPerUnit = 1.0,
-        inverted = false
+        .degreesPerUnit = 1.0,
+        .inverted = false
 };
 
 // Actuator config
 
-static ActuatorController::t_actuatorConfig turnTableAcutuatorConfig = {
-    velocityPID = {
-        P = 1.0, 
-        I = 0.0,
-        D = 0.0, 
-        bias = 0.0;
-        deadZoneError = 0.1;
+static ActuatorController::t_actuatorConfig turnTableActuatorConfig = {
+    .velocityPID = {
+        .P = 1.0, 
+        .I = 0.0,
+        .D = 0.0, 
+        .bias = 0.0,
+        .deadZoneError = 0.1,
     }, 
 
-    positionPID = {
-        P = 1.0, 
-        I = 0.0,
-        D = 0.0, 
-        bias = 0.0;
-        deadZoneError = 0.1;
+    .positionPID = {
+        .P = 1.0, 
+        .I = 0.0,
+        .D = 0.0,
+        .bias = 0.0,
+        .deadZoneError = 0.1
     }
 };
 
-static ActuatorController::t_actuatorConfig shoulderAcutuatorConfig = {
-    velocityPID = {
-        P = 1.0, 
-        I = 0.0,
-        D = 0.0, 
-        bias = 0.0;
-        deadZoneError = 0.1;
+static ActuatorController::t_actuatorConfig shoulderActuatorConfig = {
+    .velocityPID = {
+        .P = 1.0, 
+        .I = 0.0,
+        .D = 0.0, 
+        .bias = 0.0,
+        .deadZoneError = 0.1
     }, 
 
-    positionPID = {
-        P = 1.0, 
-        I = 0.0,
-        D = 0.0, 
-        bias = 0.0;
-        deadZoneError = 0.1;
+    .positionPID = {
+        .P = 1.0, 
+        .I = 0.0,
+        .D = 0.0, 
+        .bias = 0.0,
+        .deadZoneError = 0.1
     }
 };
 
-static ActuatorController::t_actuatorConfig elbowAcutuatorConfig = {
-    velocityPID = {
-        P = 1.0, 
-        I = 0.0,
-        D = 0.0, 
-        bias = 0.0;
-        deadZoneError = 0.1;
+static ActuatorController::t_actuatorConfig elbowActuatorConfig = {
+    .velocityPID = {
+        .P = 1.0, 
+        .I = 0.0,
+        .D = 0.0, 
+        .bias = 0.0,
+        .deadZoneError = 0.1
     }, 
 
-    positionPID = {
-        P = 1.0, 
-        I = 0.0,
-        D = 0.0, 
-        bias = 0.0;
-        deadZoneError = 0.1;
+    .positionPID = {
+        .P = 1.0, 
+        .I = 0.0,
+        .D = 0.0, 
+        .bias = 0.0,
+        .deadZoneError = 0.1
     }
 };
