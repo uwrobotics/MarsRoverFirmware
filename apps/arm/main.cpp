cae95cd9834bdf7ca4203a1d54b388d654b74100
Alex Petkovic <16639190+jetkov@users.noreply.github.com>
2020-02-13 03:41:48 -0500

    wip

--- apps/arm/main.cpp
+++ apps/arm/main.cpp
@@ -1,56 +1,17 @@
 #include "mbed.h"
 
+#include "arm_config.h"
+#include "Encoder.h"
+#include "EncoderAbsolute_PWM.h"
+#include "EncoderRelative_Quadrature.h"
+#include "ActuatorController.h"
+
 Motor turnTableMotor(MTR_PWM_TRNTBL, MTR_DIR_TRNTBL, false);
 Motor shoulderMotor(MTR_PWM_SHLDR, MTR_DIR_SHLDR, false);
 Motor elbowMotor(MTR_PWM_ELBW, MTR_DIR_ELBW, false);
 Motor wristLeftMotor(MTR_PWM_WRST_LHS, MTR_DIR_WRST_LHS, false);
 Motor wristRightMotor(MTR_PWM_WRIST_RHS, MTR_DIR_WRIST_RHS, false);
 Motor clawMotor(MTR_PWM_CLAW, MTR_DIR_CLAW, false);
-
-t_encoderConfig turnTableEncoderConfig {
-        pin_PWM = ENC_PWM_TRNTBL,
-        degreesPerUnit = 360,
-        inverted = false
-};
-
-t_encoderConfig shoulderEncoderConfig {
-        pin_PWM = ENC_PWM_TRNTBL,
-        degreesPerUnit = 360,
-        inverted = false
-};
-
-t_encoderConfig elbowEncoderConfig {
-        pin_PWM = ENC_PWM_TRNTBL,
-        degreesPerUnit = 360,
-        inverted = false
-};
-
-t_encoderConfig wristLeftEncoderConfig {
-        // Quadrature encoder pins
-        pin_ChannelA = ENC_QUAD_WRST_LHS_A,
-        pin_ChannelB = ENC_QUAD_WRST_LHS_A,
-
-        degreesPerUnit = 1.0,
-        inverted = false
-};
-
-t_encoderConfig wristRightEncoderConfig {
-        // Quadrature encoder pins
-        pin_ChannelA = ENC_QUAD_WRST_RHS_A,
-        pin_ChannelB = ENC_QUAD_WRST_RHS_B,
-
-        degreesPerUnit = 1.0,
-        inverted = false
-};
-
-t_encoderConfig clawEncoderConfig {
-        // Quadrature encoder pins
-        pin_ChannelA = ENC_CLAW_QUAD_A,
-        pin_ChannelB = ENC_CLAW_QUAD_B,
-
-        degreesPerUnit = 1.0,
-        inverted = false
-};
 
 EncoderAbsolute_PWM turnTableEncoder(turnTableEncoderConfig);
 EncoderAbsolute_PWM shoulderEncoder(shoulderEncoderConfig);
@@ -69,65 +30,7 @@ DigitalIn wristLimDown(LIM_WRST_DN);
 DigitalIn wristLimCenter(LIM_WRST_CNTR);
 DigitalIn clawLimOpen(LIM_CLAW_OPEN);
 
-t_actuatorConfig turnTableAcutuatorConfig {
-    velocityPID = {
-        P = 1.0, 
-        I = 0.0,
-        D = 0.0, 
-        bias = 0.0;
-        deadZoneError = 0.1;
-    }, 
-
-    positionPID = {
-        P = 1.0, 
-        I = 0.0,
-        D = 0.0, 
-        bias = 0.0;
-        deadZoneError = 0.1;
-    }
-};
-
-t_actuatorConfig shoulderAcutuatorConfig {
-    velocityPID = {
-        P = 1.0, 
-        I = 0.0,
-        D = 0.0, 
-        bias = 0.0;
-        deadZoneError = 0.1;
-    }, 
-
-    positionPID = {
-        P = 1.0, 
-        I = 0.0,
-        D = 0.0, 
-        bias = 0.0;
-        deadZoneError = 0.1;
-    }
-};
-
-t_actuatorConfig elbowAcutuatorConfig {
-    velocityPID = {
-        P = 1.0, 
-        I = 0.0,
-        D = 0.0, 
-        bias = 0.0;
-        deadZoneError = 0.1;
-    }, 
-
-    positionPID = {
-        P = 1.0, 
-        I = 0.0,
-        D = 0.0, 
-        bias = 0.0;
-        deadZoneError = 0.1;
-    }
-};
-
-ActuatorController(t_actuatorConfig actuatorConfig, 
-                       Motor * motor, Encoder * encoder, 
-                       DigitalIn * limSwitchMin = NULL, 
-                       DigitalIn * limSwitchMax = NULL);
-
+ActuatorController turnTableActuator(turnTableActuatorConfig, turnTableMotor, turnTableEncoder, turnTableLimRight, turnTableLimLeft);
 
 int main()
 {
