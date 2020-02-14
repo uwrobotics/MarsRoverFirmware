13b51678ca057507f07e8721be6bb8b6065b2516
Alex Petkovic <16639190+jetkov@users.noreply.github.com>
2020-02-14 07:07:54 -0500

    Add incomplete claw controller

---
+++ apps/arm/ClawController.cpp
@@ -0,0 +1,32 @@
+#include "ClawController.h"
+
+ClawController::ClawController(t_actuatorConfig actuatorConfig, 
+                       Motor &motor, Encoder &encoder,  
+                       DigitalIn &limSwitchMax, AnalogIn &forceSensor) : 
+        ActuatorController(actuatorConfig, motor, encoder, NULL_DIGITAL_IN, limSwitchMax), r_forceSensor(forceSensor) {};
+
+
+mbed_error_status_t ClawController::setGapVelocity_CmPerSec(float cmPerSec) {
+    float shaftVelocity_DegreesPerSec = 2.0 * cmPerSec + 0.0 * getGapDistance_Cm(); // TODO find mapping function (velocity dependent on position)
+    return setVelocity_DegreesPerSec(shaftVelocity_DegreesPerSec);
+}
+
+mbed_error_status_t ClawController::setGapDistance_Cm(float cm) {
+    float shaftPosition_Degrees = 2.0 * cm; // TODO find mapping function
+    return setAngle_Degrees(shaftPosition_Degrees);
+}
+
+float ClawController::getGapVelocity_CmPerSec() {
+    return getVelocity_DegreesPerSec() / 2.0 + 0.0 * getAngle_Degrees(); // TODO find mapping function (velocity dependent on position)
+}
+float ClawController::getGapDistance_Cm() {
+    return getAngle_Degrees() / 2.0; // TODO find mapping function
+}
+
+float ClawController::getGripForce_Newtons() {
+    return 0.0; // TODO
+}
+
+mbed_error_status_t ClawController::runPositionCalibration() {
+    return MBED_SUCCESS; // TODO
+}
