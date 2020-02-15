904da6b420ef252a99b70d3b7fa21d7621062625
Alex Petkovic <16639190+jetkov@users.noreply.github.com>
2020-02-15 07:08:22 -0500

    cleanup

--- apps/arm/main.cpp
+++ apps/arm/main.cpp
@@ -1,6 +1,7 @@
 #include "mbed.h"
 
-#include "arm_config.h"
+#include "ArmConfig.h"
+#include "AnalogIn.h"
 #include "Encoder.h"
 #include "EncoderAbsolute_PWM.h"
 #include "EncoderRelative_Quadrature.h"
@@ -20,13 +21,13 @@ Motor wristLeftMotor(MTR_PWM_WRST_LHS, MTR_DIR_WRST_LHS, false);
 Motor wristRightMotor(MTR_PWM_WRST_RHS, MTR_DIR_WRST_RHS, false);
 Motor clawMotor(MTR_PWM_CLAW, MTR_DIR_CLAW, false);
 
-EncoderAbsolute_PWM turnTableEncoder(turnTableEncoderConfig);
-EncoderAbsolute_PWM shoulderEncoder(shoulderEncoderConfig);
-EncoderAbsolute_PWM elbowEncoder(elbowEncoderConfig);
+EncoderAbsolute_PWM turnTableEncoder(ArmConfig::turnTableEncoderConfig);
+EncoderAbsolute_PWM shoulderEncoder(ArmConfig::shoulderEncoderConfig);
+EncoderAbsolute_PWM elbowEncoder(ArmConfig::elbowEncoderConfig);
 
-EncoderRelative_Quadrature wristLeftEncoder(wristLeftEncoderConfig);
-EncoderRelative_Quadrature wristRightEncoder(wristRightEncoderConfig);
-EncoderRelative_Quadrature clawEncoder(clawEncoderConfig);
+EncoderRelative_Quadrature wristLeftEncoder(ArmConfig::wristLeftEncoderConfig);
+EncoderRelative_Quadrature wristRightEncoder(ArmConfig::wristRightEncoderConfig);
+EncoderRelative_Quadrature clawEncoder(ArmConfig::clawEncoderConfig);
 
 DigitalIn turnTableLimLeft(LIM_TRNTBL_LHS);
 DigitalIn turnTableLimRight(LIM_TRNTBL_RHS);
@@ -39,15 +40,15 @@ DigitalIn clawLimOpen(LIM_CLAW_OPEN);
 
 AnalogIn clawForceSensor(FORCE_CLAW);
 
-ActuatorController turnTableActuator(turnTableActuatorConfig, turnTableMotor, turnTableEncoder, turnTableLimRight, turnTableLimLeft);
-ActuatorController shoulderActuator(shoulderActuatorConfig, shoulderMotor, shoulderEncoder);
-ActuatorController elbowActuator(elbowActuatorConfig, elbowMotor, elbowEncoder, elbowLimDown, elbowLimUp);
+ActuatorController turnTableActuator(ArmConfig::turnTableActuatorConfig, turnTableMotor, turnTableEncoder, turnTableLimRight, turnTableLimLeft);
+ActuatorController shoulderActuator(ArmConfig::shoulderActuatorConfig, shoulderMotor, shoulderEncoder);
+ActuatorController elbowActuator(ArmConfig::elbowActuatorConfig, elbowMotor, elbowEncoder, elbowLimDown, elbowLimUp);
 
-ActuatorController wristLeftActuator(wristLeftActuatorConfig, wristLeftMotor, wristLeftEncoder);
-ActuatorController wristRightActuator(wristRightActuatorConfig, wristRightMotor, wristRightEncoder);
+ActuatorController wristLeftActuator(ArmConfig::wristLeftActuatorConfig, wristLeftMotor, wristLeftEncoder);
+ActuatorController wristRightActuator(ArmConfig::wristRightActuatorConfig, wristRightMotor, wristRightEncoder);
 
 DifferentialWristController wristController(wristLeftActuator, wristRightActuator, wristLimUp, wristLimCenter, wristLimDown);
-ClawController clawController(clawActuatorConfig, clawMotor, clawEncoder, clawLimOpen, clawForceSensor);
+ClawController clawController(ArmConfig::clawActuatorConfig, clawMotor, clawEncoder, clawLimOpen, clawForceSensor);
 
 DigitalOut led1(LED1);
 
@@ -128,30 +129,30 @@ static mbed_error_status_t setPIDConstant(CANMsg &msg) {
 }
 
 static std::map<unsigned int, CANMsg::CANMsgHandler> canIDHandlerMap = {
-    {SET_TURNTABLE_CONTROL_MODE, &setControlMode},
-    {SET_SHOULDER_CONTROL_MODE,  &setControlMode},
-    {SET_ELBOW_CONTROL_MODE,     &setControlMode},
-    {SET_WRIST_CONTROL_MODE,     &setControlMode},
-    {SET_CLAW_CONTROL_MODE,      &setControlMode},
+    {CANID::SET_TURNTABLE_CONTROL_MODE, &setControlMode},
+    {CANID::SET_SHOULDER_CONTROL_MODE,  &setControlMode},
+    {CANID::SET_ELBOW_CONTROL_MODE,     &setControlMode},
+    {CANID::SET_WRIST_CONTROL_MODE,     &setControlMode},
+    {CANID::SET_CLAW_CONTROL_MODE,      &setControlMode},
 
-    {SET_TURNTABLE_MOTIONDATA,   &setMotionData},
-    {SET_SHOULDER_MOTIONDATA,    &setMotionData},
-    {SET_ELBOW_MOTIONDATA,       &setMotionData},
-    {SET_WRIST_PITCH_MOTIONDATA, &setMotionData},
-    {SET_WRIST_ROLL_MOTIONDATA,  &setMotionData},
-    {SET_CLAW_MOTIONDATA,        &setMotionData},
-    {SET_TOOL_TIP_DEPLOYMENT,    &setToolTipDeployment},
+    {CANID::SET_TURNTABLE_MOTIONDATA,   &setMotionData},
+    {CANID::SET_SHOULDER_MOTIONDATA,    &setMotionData},
+    {CANID::SET_ELBOW_MOTIONDATA,       &setMotionData},
+    {CANID::SET_WRIST_PITCH_MOTIONDATA, &setMotionData},
+    {CANID::SET_WRIST_ROLL_MOTIONDATA,  &setMotionData},
+    {CANID::SET_CLAW_MOTIONDATA,        &setMotionData},
+    {CANID::SET_TOOL_TIP_DEPLOYMENT,    &setToolTipDeployment},
 
-    {RUN_WRIST_CALIBRATION,      &runWristCalibration},
-    {RUN_CLAW_CALIBRATION,       &runClawCalibration},
+    {CANID::RUN_WRIST_CALIBRATION,      &runWristCalibration},
+    {CANID::RUN_CLAW_CALIBRATION,       &runClawCalibration},
 
-    {SET_PID_TUNING_MODE,        &setPIDTuningMode},
+    {CANID::SET_PID_TUNING_MODE,        &setPIDTuningMode},
 
-    {SET_PID_DEADZONE,           &setPIDConstant},
-    {SET_JOINT_PID_P,            &setPIDConstant},
-    {SET_JOINT_PID_I,            &setPIDConstant},
-    {SET_JOINT_PID_D,            &setPIDConstant},
-    {SET_JOINT_PID_BIAS,         &setPIDConstant}
+    {CANID::SET_PID_DEADZONE,           &setPIDConstant},
+    {CANID::SET_JOINT_PID_P,            &setPIDConstant},
+    {CANID::SET_JOINT_PID_I,            &setPIDConstant},
+    {CANID::SET_JOINT_PID_D,            &setPIDConstant},
+    {CANID::SET_JOINT_PID_BIAS,         &setPIDConstant}
 };
 
 CAN can1(CAN1_RX, CAN1_TX, ROVER_CANBUS_FREQUENCY);
