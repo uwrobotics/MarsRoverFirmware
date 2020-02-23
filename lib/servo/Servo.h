ab688a7abd440e73e7da29f0825979da8ca7b981
KrillNye <43658159+KrillNye@users.noreply.github.com>
2020-02-23 15:10:28 -0500

    Fixes #4: Servo controller (#28)
    * Wrote a header for servo control.
    
    More goes into servo control than I thought. Wrote a basic servo control library header file.
    
    * Incomplete
    
    * cpp commit
    
    * worked on CPP and H
    
    * Servo library completion
    
    Still needs to do testing
    
    * Tested, compiled on 2019 Directory
    
    * Update .gitignore
    
    * Fixes, see comments
    
    * Commented Issues fixes
    
    * Cleanup for merge -- Will be cleaned up more later
    
    * temp mv fix
    
    * temp mv fix
    
    * remove unclean files
    
    * Updated gitignore, untracked .vscode polution (settings.json)
    
    * removed unnecessary scope operators to be more conscise.
    
    * scaled from ms to us
    
    Co-authored-by: Alex Petkovic <16639190+jetkov@users.noreply.github.com>

---
+++ lib/servo/Servo.h
@@ -0,0 +1,48 @@
+#ifndef SERVO_H
+#define SERVO_H
+
+#include "mbed.h"                
+    class Servo {
+  
+    public:
+    
+    enum SERVO_TYPE {CONT_SERVO = 0, LIM_SERVO = 1}; //CONT_SERVO = 0 = CONTINUOUS Rotation; LIMITED_SERVO = 1 = LIMITED Range Rotation 
+
+    protected:
+        PinName pin;                    //PIN the servo PWM signal is attached to
+        PwmOut pwm;                     //PWM object, does not have default constructor so have to use INITIALIZER LIST to avoid COMPILER attempting to DEFAULT CONSTRUCT
+        
+        const int   PWM_FREQ = 50,          //DEFAULT PWM FREQUENCY, should work for both length and positional control 
+                    DEFAULT_MAX = 2,        //DEFAULT MAX WAVE LENGTH in MILISECONDS
+                    DEFAULT_MIN = 1,        //DEFAULT MIN WAVE LENGTH in MILISECONDS
+                    DEFAULT_RANGE = 180,    //DEFAULT RANGE for LIMITED Servos
+                    PERIOD = 1000 / PWM_FREQ; //DEFAULT PERIOD LENGTH in MILISECONDS
+
+        enum SERVO_TYPE rotate_type;            //Either CONTINUOUS or LIMITED rotation servo, see define statements.
+
+            float       range,                  //RANGE OF MOTION, only valid for LIMIT_SERVO types
+                        max_speed,              //MAXIMUM ROTATION SPEED in ANGLES PER SECOND, only valid for CONTINUOUS_SERVO types
+                        max_pulse_ms,           //PULSE LENGTH for MAX ANGLE 
+                        min_pulse_ms,           //PULSE LENGTH for MIN ANGLE
+                        pos,                    //POSITION of servo
+                        speed;                  //ROTATING SPEED in ANGLES PER SECOND
+
+        public:
+        
+            Servo(PinName pin_, SERVO_TYPE rotate_type_, float value, float max_pulse_ms_, float min_pulse_ms_);    //Copies previous constructors but with PIN
+            Servo(PinName pin_, SERVO_TYPE rotate_type_);              
+            Servo(PinName pin_, SERVO_TYPE rotate_type_, float value);    
+
+            bool set_range(float range_);       //Returns FALSE if Servo Type is CONTINUOUS
+            bool set_max_speed(float max_speed_);   //Returns FALSE if Servo Type is LIMITED
+            
+            bool set_position(float angle);     //Sets POS to angle if Servo Type LIMITED
+            bool set_speed(float speed_);       //Sets rotation SPEED if Servo Type is CONTINUOUS
+
+            float read(void);                   //Returns SPEED if CONTINUOUS and POSITION if LIMITED
+
+            void set_period(int period);         //Override default period (ONLY USE FOR SPECIFIC FREQ REQUIREMENT)
+
+    };
+
+#endif
