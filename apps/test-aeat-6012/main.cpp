66177ecf544197f2f8295d7c641964a6ef311daa
Alex Petkovic <16639190+jetkov@users.noreply.github.com>
2020-03-10 08:03:55 -0400

    Reading correct value from 0-4095

--- apps/test-aeat-6012/main.cpp
+++ apps/test-aeat-6012/main.cpp
@@ -22,8 +22,8 @@ POTENTIAL FIX: CHECK BIT SHFITING RELATED STUFF
 
     printf("connected");
 
-    char send[3] = {0x00, 0x00, 0x00};
-    char recieve[3] = {0x00, 0x00, 0x00};
+    char send[3] = {0x00, 0x00};
+    char recieve[3] = {0x00, 0x00};
 
     while(1){
         cs = 0;
@@ -37,7 +37,11 @@ POTENTIAL FIX: CHECK BIT SHFITING RELATED STUFF
         //bring CONTROL back to HIGH to stop signaling
         cs = 1;
 
-        printf("%02X  %02X %02X \r\n", recieve[2], recieve[1], recieve[0]);
+        // MAX FC(LSB) 7F(MSB)    MIN 0400
+
+        uint16_t value = (((uint16_t) recieve[1]) >> 3) | (((uint16_t) recieve[0]) << 5); 
+
+        printf("%02X %02X %d\r\n", recieve[1], recieve[0], value);
 
         wait_ms(100);
     }
