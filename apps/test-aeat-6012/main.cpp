6fab59ef31ecd1e39b9d55c6702c20a6e9d4ff5b
Alex Petkovic <16639190+jetkov@users.noreply.github.com>
2020-03-11 04:34:59 -0400

    Cln

--- apps/test-aeat-6012/main.cpp
+++ apps/test-aeat-6012/main.cpp
@@ -1,20 +1,10 @@
 #include "mbed.h"
 
 DigitalOut cs(SPI1_MOSI);
-
-uint16_t pos = 0;
+SPI enc(NC, SPI1_MISO, SPI1_SCK);
 
 int main(){
-/* ISSUE: ONLY OUTPUTTING 7 BITS"
 
-POTENTIAL FIX: ENSURING CLOCK SIGNAL IS AT LEAST 3.5 V
-
-POTENTIAL FIX: CHECK BIT SHFITING RELATED STUFF
-
-*/
-
-//    SPI enc(CONTROL, READ, CLOCK);
-    SPI enc(NC, SPI1_MISO, SPI1_SCK);
     enc.format(12,3);
     enc.frequency(100000);
 
@@ -22,29 +12,34 @@ POTENTIAL FIX: CHECK BIT SHFITING RELATED STUFF
 
     printf("connected");
 
-    char send[3] = {0x00, 0x00};
-    char recieve[3] = {0x00, 0x00};
+    char send[2] = {0x00, 0x00};
+    char recieve[2] = {0x00, 0x00};
+
+    uint16_t value = 0;
 
     while(1) {
+
+        // Assert CS to start signaling
         cs = 0;
 
-        //write low for wait bit
+        // Specced delay >500ns after CS assertion
         ThisThread::sleep_for(1);
 
-        //write 12 low dummy bits to recieve
+        // Write low dummy bytes to recieve
         pos = enc.write(send, 2, recieve, 2);
 
-        // Next step to use async SPI
+        // TODO: Next step to use async SPI
 
-        //bring CONTROL back to HIGH to stop signaling
+        // Deassert CS to stop signaling
         cs = 1;
 
-        // MAX FC(LSB) 7F(MSB)    MIN 0400
+        // MAX 7F(MSB) FC(LSB) , MIN 0004
 
-        uint16_t value = (((uint16_t) recieve[1]) >> 3) | (((uint16_t) recieve[0]) << 5); 
+        value = (((uint16_t) recieve[1]) >> 3) | (((uint16_t) recieve[0]) << 5); 
 
-        printf("%02X %02X %d\r\n", recieve[1], recieve[0], value);
+        printf("%02X %02X %d\r\n", recieve[0], recieve[1], value);
 
+        // Specced delay >500ns after CS deassertion
         ThisThread::sleep_for(1);
     }
 }
