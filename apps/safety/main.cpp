e3a50e2d4b1d79c285d69a7102e93d9160744c29
FelixWLWong <fk3wong@uwaterloo.ca>
2020-03-19 10:02:22 -0400

    Adding functionality to read and write to get current and voltage data

--- apps/safety/main.cpp
+++ apps/safety/main.cpp
@@ -2,16 +2,31 @@
 #include "CANMsg.h"
 #include "math.h"
 
+#define CURRENT_REGISTER 0x04
+#define VOLTAGE_REGISTER 0x01
 
 // leds on board
 DigitalOut led1(LED1);
 
 //  I2C bus initialization
 I2C i2c(I2C_SDA, I2C_SCK);
-char sensorData[2];
-const int SENSOR_SLAVE_ADDRESS = 0x1c;
+const int SENSOR_SLAVE_ADDRESS = 0x1c << 1;
+//configuration register 00h controls modes of chip
+// bit 15 - reset bit, wriitng 1 resets registers. bits 0-2 select operating mode
+// shunt voltage register 01h
+// bus voltage register 02h
+// power register 03h
+//current register 04h
+//calibration register 05h
+// mask/enable register 06h
+//alert limit register 07h
+
 
 const int k_interval_ms = 500;
+
+enum configurationMode{
+
+}
 
 // temperature conversion constants
 const float A = 0;
@@ -30,6 +45,7 @@ requires logic to:
 float processCurrentData();
 int processVoltageData();
 float readThermosistor();
+int calibrateSensor();
 
 int main()
 {
@@ -39,9 +55,10 @@ int main()
     led1 = 1;
 
     while (true) {
+
         // analyze current data
-        i2c.read(SENSOR_SLAVE_ADDRESS, sensorData, 2, false);
         led1 = !led1;
+
 
 
         //analyze voltage 
@@ -52,11 +69,35 @@ int main()
 }
 
 
-float processCurrentData()
+float getCurrentData()
 {
+    //some conversion factor for current data
+    float conversionFactor = 1;
+    char cmd[3] = {CURRENT_REGISTER};
+    
+    i2s.write(SENSOR_SLAVE_ADDRESS, cmd, 2);
+    i2c.read(SENSOR_SLAVE_ADDRESS, cmd, 2, false);
+    
+    char16_t currentData = cmd[1]; 
+    currentData = (cmd[2] << 8 | 0x00);
+    
+    return currentData * conversionFactor;
+}
 
 
-
+float getVoltageData()
+{
+    //some conversion factor for voltage data
+    float conversionFactor = 1;
+    char cmd[3] = {VOLTAGE_REGISTER};
+    
+    i2s.write(SENSOR_SLAVE_ADDRESS, cmd, 2);
+    i2c.read(SENSOR_SLAVE_ADDRESS, cmd, 2, false);
+    
+    char16_t voltageData = cmd[1]; 
+    voltageData = (cmd[2] << 8 | 0x00);
+    
+    return voltageData * conversionFactor;
 }
 
 
