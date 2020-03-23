9f444285e76fa029ded7fdf5dcafebe6f96221a5
FelixWLWong <fk3wong@uwaterloo.ca>
2020-03-23 19:25:44 -0400

    fixing errors

--- apps/safety/main.cpp
+++ apps/safety/main.cpp
@@ -3,6 +3,13 @@
 #include "math.h"
 #include "rover_config.h"
 
+//configuration register 00h controls modes of chip
+// bit 15 - reset bit, wriitng 1 resets registers. bits 0-2 select operating mode
+// bus voltage register 02h
+// power register 03h
+//calibration register 05h
+// mask/enable register 06h
+//alert limit register 07h
 #define CONFIG_REGISTER 0x00
 #define VOLTAGE_REGISTER 0x01
 #define CURRENT_REGISTER 0x04
@@ -14,24 +21,18 @@ DigitalOut led1(LED1);
 DigitalOut led2(LED2);
 DigitalOut led3(LED3);
 
+//Serial communications for debugging
+Serial pc(SERIAL_TX, SERIAL_RX, ROVER_DEFAULT_SERIAL_BAUD_RATE);
+
 //CAN initialization
-CAN can(CAN1_RX, CAN1_TX, ROVER_CANBUS_FREQUENCY);
+CAN can(CAN1_RX, CAN1_TX, ROVER_CANBUS_FREQUENCY); // dont know the actual CAN id's
 CANMsg rxMsg;
 //needs a specific CANID to update values
 const unsigned int expectedCANID = 0x000;
 
 //  I2C bus initialization
-I2C i2c(I2C_SDA, I2C_SCK);
+I2C i2c(CUR_SEN_I2C_SDA, CUR_SEN_I2C_SCL); //pinnames found from pinnames.h
 const int SENSOR_SLAVE_ADDRESS = 0x1c << 1;
-//configuration register 00h controls modes of chip
-// bit 15 - reset bit, wriitng 1 resets registers. bits 0-2 select operating mode
-// shunt voltage register 01h
-// bus voltage register 02h
-// power register 03h
-//current register 04h
-//calibration register 05h
-// mask/enable register 06h
-//alert limit register 07h
 
 enum configurationMode{
     powerReset = 0,
@@ -49,7 +50,7 @@ const int averageMode = 0;
 const float A = 0;
 const float B = 0;
 const float C = 0;
-const float rresistor = 0;
+const float rResistor = 0;
 
 //measurement thresholds, needs updating
 float currentUpperLim = 1;
@@ -61,11 +62,11 @@ float tempLowerLim = 0;
 
 //funcs
 int inRange(float upperLim, float lowerLim, float val);
-float processCurrentData();
-int processVoltageData();
+float getCurrentData();
+float getVoltageData();
 float readThermosistor();
-status calibrateSensor();
-status configureSensor(configurationMode operationMode, int resetRegisters);
+int calibrateSensor();
+int configureSensor(configurationMode operationMode, int resetRegisters);
 void initCAN();
 void processCANMsg(CANMsg *p_newMsg);
 
@@ -85,7 +86,7 @@ int main()
         if(can.read(rxMsg))
         {
             processCANMsg(&rxMsg);
-            *rxMsg >> command;
+            rxMsg >> command;
             rxMsg.clear();
         }
 
@@ -112,7 +113,7 @@ int main()
 void initCAN()
 {
     // will need new id's 
-    can.filter(RX_ID, TX_ID, ROVER_CANID_MASK, CANStandard);
+    can.filter(CAN1_RX, CAN1_TX, ROVER_CANID_MASK, CANStandard);
 }
 
 void processCANMsg(CANMsg *p_newMsg)
@@ -132,7 +133,8 @@ void processCANMsg(CANMsg *p_newMsg)
 
 int inRange(float upperLim, float lowerLim, float val)
 {
-    if (val > upperLim || val< lowerLim) ? return 1: return 0;
+    int inRange = (val > upperLim || val< lowerLim) ? 1: 0;
+    return inRange;
 }
 
 
@@ -142,7 +144,7 @@ float getCurrentData()
     float conversionFactor = 1;
     char cmd[3] = {CURRENT_REGISTER};
     
-    i2s.write(SENSOR_SLAVE_ADDRESS, cmd, 2);
+    i2c.write(SENSOR_SLAVE_ADDRESS, cmd, 2);
     i2c.read(SENSOR_SLAVE_ADDRESS, cmd, 2, false);
     
     char16_t currentData = cmd[1]; 
@@ -158,7 +160,7 @@ float getVoltageData()
     float conversionFactor = 1;
     char cmd[3] = {VOLTAGE_REGISTER};
     
-    i2s.write(SENSOR_SLAVE_ADDRESS, cmd, 2);
+    i2c.write(SENSOR_SLAVE_ADDRESS, cmd, 2);
     i2c.read(SENSOR_SLAVE_ADDRESS, cmd, 2, false);
     
     char16_t voltageData = cmd[1]; 
@@ -168,13 +170,13 @@ float getVoltageData()
 }
 
 
-float readThermosistor()
+float readThermosistor(float vin, float vout)
 {
     //voltage divider
-    float rthermo = rresistor *((vin/vout)-1);
+    float rthermo = rResistor *((vin/vout)-1);
 
     //convert resistance to temp (steinhart and hart eqn)
-    return 1/(A+B*log(rthermo)+C*exp(log(rthermo),3));
+    return 1/(A+B*log(rthermo)+C*pow(log(rthermo),3));
 }
 
 int configureSensor(configurationMode operationMode, int resetRegisters)
@@ -212,4 +214,5 @@ int configureSensor(configurationMode operationMode, int resetRegisters)
     cmd[2] |= dataByte >> 8;
 
     i2c.write(SENSOR_SLAVE_ADDRESS, cmd, 2);
+    return 0;
 }
