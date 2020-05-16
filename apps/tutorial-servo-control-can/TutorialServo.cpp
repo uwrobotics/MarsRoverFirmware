749472cca6c91408b9b4c228bad3663b554b5830
laineee <ieitua@edu.uwaterloo.ca>
2020-05-16 02:21:28 -0600

    firmware training updated

--- apps/tutorial-servo-control-can/TutorialServo.cpp
+++ apps/tutorial-servo-control-can/TutorialServo.cpp
@@ -3,46 +3,37 @@
 #include "mbed.h"
 #include "TutorialServo.h"
 
-class TutorialServo
+
+// Constructor: Takes a servo pin name (ex. PA_1), and optionally a servo range
+// that has a default value of 180.0 degrees, a minimum pulsewidth of 1ms, and a     
+// maximum pulsewidth of 2ms.
+TutorialServo :: TutorialServo(PinName servoPin, float servoRangeInDegrees, float minPulsewidthInMs, float maxPulsewidthInMs)
+	:m_servoPwmOut(servoPin), m_servoRangeInDegrees(servoRangeInDegrees), 
+	   m_minPulsewidthInMs(minPulsewidthInMs), m_maxPulsewidthInMs(maxPulsewidthInMs); //sevoPin like PA_1
 {
-private:
+	m_servoPwmOut.period(0.02); //call func to set period on m_servo
+}
 
-   PwmOut m_servoPwmOut; //need
-   float  m_servoRangeInDegrees;
-   float  m_minPulsewidthInMs;
-   float  m_maxPulsewidthInMs;
-   
-   // Constructor: Takes a servo pin name (ex. PA_1), and optionally a servo range
-   // that has a default value of 180.0 degrees, a minimum pulsewidth of 1ms, and a     
-   // maximum pulsewidth of 2ms.
-   TutorialServo(PinName servoPin, float servoRangeInDegrees, float minPulsewidthInMs, float maxPulsewidthInMs)
-   		:m_servoPwmOut(servoPin), m_servoRangeInDegrees(servoRangeInDegrees), 
-		   m_minPulsewidthInMs(minPulsewidthInMs), m_maxPulsewidthInMs(maxPulsewidthInMs); //sevoPin would be like PA_1
-   {
-   		m_servoPwmOut.period(0.02); //call func to set period on m_servo
-   }
+// Set servo range (ex. 270 deg)
+void TutorialServo :: setAngleRangeInDegrees(float degrees)
+{
+	m_servoRangeInDegrees = degrees;
+}
 
-   // Set servo range (ex. 270 deg)
-   void setAngleRangeInDegrees(float degrees)
-   {
-   		m_servoRangeInDegrees = degrees;
-   }
+// Set servo pulse width range (ex. 1ms, 2ms)
+void TutorialServo :: setPulsewidthRangeInMs(float minPulsewidthMs, float maxPulsewidthMs)
+{
+	m_minPulsewidthInMs = minPulsewidthMs;
+	m_maxPulsewidthInMs = maxPulsewidthMs;
+}
 
-   // Set servo pulse width range (ex. 1ms, 2ms)
-   void setPulsewidthRangeInMs(float minPulsewidthMs, float maxPulsewidthMs)
-   {
-   		m_minPulsewidthInMs = minPulsewidthMs;
-   		m_maxPulsewidthInMs = maxPulsewidthMs;
-   }
+// Set servo position (ex. 45 deg)
+void TutorialServo :: setPositionInDegrees(float degrees)
+{
+	//need % of range, use min and max pw
+	float pulseRange = maxPulsewidthInMs - minPulsewidthInMs;
+	
+	m_servoPwmOut.pulsewidth(minPulsewidthInMs + (degrees/m_servoRangeInDegrees *pulseRange));
+}
 
-   // Set servo position (ex. 45 deg)
-   void setPositionInDegrees(float degrees)
-   {
-   		//need % of range, use min and max pw
-   		float pulseRange = maxPulsewidthInMs - minPulsewidthInMs;
-   		
-   		m_servoPwmOut.pulsewidth(minPulsewidthInMs + (degrees/m_servoRangeInDegrees *pulseRange));
-	}
-
-};
 
--- apps/tutorial-servo-control-can/main.cpp
+++ apps/tutorial-servo-control-can/main.cpp
@@ -1,3 +1,63 @@
+//Bonus: Enable the CAN peripheral filter to only read messages with your ID
+//Not sure if this would work...
+
+/*
+#include "mbed.h"
+#include "TutorialServo.h"
+#include "PwmIn.h"
+#include "CANMsg.h"
+
+// Write a program to control a servo via CAN. Utilize the TutorialServo class that you wrote in the first part of the challenge. 
+// Use the same pin (PA_1) as used earlier in the training module. 
+// The program should be able to receive CAN messages which contain a percentage of the range of motion of the servo as a float. 
+
+#define SET_SERVO_RANGE 0x123 //ID
+
+float percentOfRange = 0;
+
+CAN can1(CAN_RX, CAN_TX, 500000); //(CANrx, CANtx, rover_bus_frequency), CAN_RX, CAN_TX are mapped elsewhere
+
+TutorialServo myservo(PA_1);
+
+static setPosition(CANMsg msg)
+{
+	msg.getPayload(percentOfRange) //percentOfRange will get val assigned by the function, its PBR (see getPayload func)
+	
+	myservo.setPositionInDegrees(percentOfRange);
+
+}
+
+//Bonus: Enable the CAN peripheral filter to only read messages with your ID
+//Not sure if this makes sense..
+bool readOnly(CANMSG message)
+{
+	if (message.id == SET_SERVO_RANGE)
+		return true;
+	else
+		return false;
+}
+
+
+int main()
+{
+	CANMsg msgrx;
+	
+	
+	while (true)
+	{
+		if (can1.read(readOnly(msgrx)))
+		{
+			setPosition(msgrx);
+		}
+		else
+			printf ("Wrong message ID or message ID not found!");
+	}
+}
+*/
+
+//~~~~~~~~~~~//
+
+
 #include "mbed.h"
 #include "TutorialServo.h"
 #include "PwmIn.h"
@@ -44,3 +104,5 @@ int main()
 			printf ("Message ID not found!");
 	}
 }
+
+
