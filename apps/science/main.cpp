3a3d2ce5e3cb7a1ab3d109d5628e09da6eb2b1da
Wolfgang Windholtz <wwindholtz@me.com>
2020-05-20 14:32:00 -0400

    First step

--- apps/science/main.cpp
+++ apps/science/main.cpp
@@ -1,12 +1,28 @@
 #include "mbed.h"
+#include "mbed_config.h"
+#include "can_config.h"
+
+#include "ArmConfig.h"
+#include "Encoder.h"
+#include "EncoderAbsolute_PWM.h"
+#include "EncoderRelative_Quadrature.h"
+#include "ActuatorController.h"
+#include "DifferentialWristController.h"
+#include "ClawController.h"
+#include "CANMsg.h"
+#include "CANBuffer.h"
+#include "Servo.h"
 
 DigitalOut led1(LED1);
+
+CAN can(CAN1_RX, CAN1_TX, ROVER_CANBUS_FREQUENCY);
 
 const int k_interval_ms = 500;
 
 int main()
 {
-    while (true) {
+    while (true)
+    {
         led1 = !led1;
         wait_ms(k_interval_ms);
     }
