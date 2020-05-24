25a98accfbfe269f128116a77f46313121fa6892
Melvin Wang <melvin.mc.wang@gmail.com>
2020-05-23 22:56:38 -0400

    Integrate clang-format github action (#88)
    * Integrate clang-format github action
    
    * fixes to appease clang-format
    
    * cr updates
    
    * removed clang 10 option
    
    * fix clang format
    
    * fix clang format
    
    * more clang-format shit

--- apps/arm/ArmConfig.h
+++ apps/arm/ArmConfig.h
@@ -1,7 +1,7 @@
 #pragma once
 
-#include "Encoder.h"
 #include "ActuatorController.h"
+#include "Encoder.h"
 
 constexpr uint8_t FLAG_DISABLE_LIMIT_SWITCH_CHECKS = (1U << 0);
 constexpr uint8_t FLAG_DISABLE_ANGLE_BOUNDS_CHECKS = (1U << 1);
@@ -9,158 +9,59 @@ constexpr uint8_t FLAG_DISABLE_FEEDBACK            = (1U << 2);
 
 namespace ArmConfig {
 
-    /*** ENCODER CONFIG ***/
+/*** ENCODER CONFIG ***/
 
-    static Encoder::t_encoderConfig turnTableEncoderConfig = {
-        .pin_PWM = ENC_PWM_TRNTBL,
-        .degreesPerUnit = 360,
-        .zeroOffset_Degrees = 0.0
-    };
+static Encoder::t_encoderConfig turnTableEncoderConfig = {
+    .pin_PWM = ENC_PWM_TRNTBL, .degreesPerUnit = 360, .zeroOffset_Degrees = 0.0};
 
-    static Encoder::t_encoderConfig shoulderEncoderConfig = {
-        .pin_PWM = ENC_PWM_TRNTBL,
-        .degreesPerUnit = 360,
-        .zeroOffset_Degrees = 0.0
-    };
+static Encoder::t_encoderConfig shoulderEncoderConfig = {
+    .pin_PWM = ENC_PWM_TRNTBL, .degreesPerUnit = 360, .zeroOffset_Degrees = 0.0};
 
-    static Encoder::t_encoderConfig elbowEncoderConfig = {
-        .pin_PWM = ENC_PWM_TRNTBL,
-        .degreesPerUnit = 360,
-        .zeroOffset_Degrees = 0.0
-    };
+static Encoder::t_encoderConfig elbowEncoderConfig = {
+    .pin_PWM = ENC_PWM_TRNTBL, .degreesPerUnit = 360, .zeroOffset_Degrees = 0.0};
 
-    static Encoder::t_encoderConfig wristLeftEncoderConfig = {
-        .pin_ChannelA = ENC_QUAD_WRST_LHS_A,
-        .pin_ChannelB = ENC_QUAD_WRST_LHS_A,
+static Encoder::t_encoderConfig wristLeftEncoderConfig = {.pin_ChannelA = ENC_QUAD_WRST_LHS_A,
+                                                          .pin_ChannelB = ENC_QUAD_WRST_LHS_A,
 
-        .degreesPerUnit = 1.0,
-        .zeroOffset_Degrees = 0.0
-    };
+                                                          .degreesPerUnit     = 1.0,
+                                                          .zeroOffset_Degrees = 0.0};
 
-    static Encoder::t_encoderConfig wristRightEncoderConfig = {
-        .pin_ChannelA = ENC_QUAD_WRST_RHS_A,
-        .pin_ChannelB = ENC_QUAD_WRST_RHS_B,
+static Encoder::t_encoderConfig wristRightEncoderConfig = {.pin_ChannelA = ENC_QUAD_WRST_RHS_A,
+                                                           .pin_ChannelB = ENC_QUAD_WRST_RHS_B,
 
-        .degreesPerUnit = 1.0,
-        .zeroOffset_Degrees = 0.0
-    };
+                                                           .degreesPerUnit     = 1.0,
+                                                           .zeroOffset_Degrees = 0.0};
 
-    static Encoder::t_encoderConfig clawEncoderConfig = {
-        .pin_ChannelA = ENC_QUAD_CLAW_A,
-        .pin_ChannelB = ENC_QUAD_CLAW_B,
+static Encoder::t_encoderConfig clawEncoderConfig = {.pin_ChannelA = ENC_QUAD_CLAW_A,
+                                                     .pin_ChannelB = ENC_QUAD_CLAW_B,
 
-        .degreesPerUnit = 1.0,
-        .zeroOffset_Degrees = 0.0
-    };
+                                                     .degreesPerUnit     = 1.0,
+                                                     .zeroOffset_Degrees = 0.0};
 
-    /*** ROTARY ACTUATOR CONFIG ***/
+/*** ROTARY ACTUATOR CONFIG ***/
 
-    static ActuatorController::t_actuatorConfig turnTableActuatorConfig = {
-        .velocityPID = {
-            .P = 1.0, 
-            .I = 0.0,
-            .D = 0.0, 
-            .bias = 0.0,
-            .deadZoneError = 0.1,
-        }, 
+static ActuatorController::t_actuatorConfig turnTableActuatorConfig = {
+    .velocityPID = {.P = 1.0, .I = 0.0, .D = 0.0, .bias = 0.0, .deadZoneError = 0.1},
+    .positionPID = {.P = 1.0, .I = 0.0, .D = 0.0, .bias = 0.0, .deadZoneError = 0.1}};
 
-        .positionPID = {
-            .P = 1.0, 
-            .I = 0.0,
-            .D = 0.0,
-            .bias = 0.0,
-            .deadZoneError = 0.1
-        }
-    };
+static ActuatorController::t_actuatorConfig shoulderActuatorConfig = {
+    .velocityPID = {.P = 1.0, .I = 0.0, .D = 0.0, .bias = 0.0, .deadZoneError = 0.1},
+    .positionPID = {.P = 1.0, .I = 0.0, .D = 0.0, .bias = 0.0, .deadZoneError = 0.1}};
 
-    static ActuatorController::t_actuatorConfig shoulderActuatorConfig = {
-        .velocityPID = {
-            .P = 1.0, 
-            .I = 0.0,
-            .D = 0.0, 
-            .bias = 0.0,
-            .deadZoneError = 0.1
-        }, 
+static ActuatorController::t_actuatorConfig elbowActuatorConfig = {
+    .velocityPID = {.P = 1.0, .I = 0.0, .D = 0.0, .bias = 0.0, .deadZoneError = 0.1},
+    .positionPID = {.P = 1.0, .I = 0.0, .D = 0.0, .bias = 0.0, .deadZoneError = 0.1}};
 
-        .positionPID = {
-            .P = 1.0, 
-            .I = 0.0,
-            .D = 0.0, 
-            .bias = 0.0,
-            .deadZoneError = 0.1
-        }
-    };
+static ActuatorController::t_actuatorConfig wristLeftActuatorConfig = {
+    .velocityPID = {.P = 1.0, .I = 0.0, .D = 0.0, .bias = 0.0, .deadZoneError = 0.1},
+    .positionPID = {.P = 1.0, .I = 0.0, .D = 0.0, .bias = 0.0, .deadZoneError = 0.1}};
 
-    static ActuatorController::t_actuatorConfig elbowActuatorConfig = {
-        .velocityPID = {
-            .P = 1.0, 
-            .I = 0.0,
-            .D = 0.0, 
-            .bias = 0.0,
-            .deadZoneError = 0.1
-        }, 
+static ActuatorController::t_actuatorConfig wristRightActuatorConfig = {
+    .velocityPID = {.P = 1.0, .I = 0.0, .D = 0.0, .bias = 0.0, .deadZoneError = 0.1},
+    .positionPID = {.P = 1.0, .I = 0.0, .D = 0.0, .bias = 0.0, .deadZoneError = 0.1}};
 
-        .positionPID = {
-            .P = 1.0, 
-            .I = 0.0,
-            .D = 0.0, 
-            .bias = 0.0,
-            .deadZoneError = 0.1
-        }
-    };
+static ActuatorController::t_actuatorConfig clawActuatorConfig = {
+    .velocityPID = {.P = 1.0, .I = 0.0, .D = 0.0, .bias = 0.0, .deadZoneError = 0.1},
+    .positionPID = {.P = 1.0, .I = 0.0, .D = 0.0, .bias = 0.0, .deadZoneError = 0.1}};
 
-    static ActuatorController::t_actuatorConfig wristLeftActuatorConfig = {
-        .velocityPID = {
-            .P = 1.0, 
-            .I = 0.0,
-            .D = 0.0, 
-            .bias = 0.0,
-            .deadZoneError = 0.1
-        }, 
-
-        .positionPID = {
-            .P = 1.0, 
-            .I = 0.0,
-            .D = 0.0, 
-            .bias = 0.0,
-            .deadZoneError = 0.1
-        }
-    };
-
-    static ActuatorController::t_actuatorConfig wristRightActuatorConfig = {
-        .velocityPID = {
-            .P = 1.0, 
-            .I = 0.0,
-            .D = 0.0, 
-            .bias = 0.0,
-            .deadZoneError = 0.1
-        }, 
-
-        .positionPID = {
-            .P = 1.0, 
-            .I = 0.0,
-            .D = 0.0, 
-            .bias = 0.0,
-            .deadZoneError = 0.1
-        }
-    };
-
-    static ActuatorController::t_actuatorConfig clawActuatorConfig = {
-        .velocityPID = {
-            .P = 1.0, 
-            .I = 0.0,
-            .D = 0.0, 
-            .bias = 0.0,
-            .deadZoneError = 0.1
-        }, 
-
-        .positionPID = {
-            .P = 1.0, 
-            .I = 0.0,
-            .D = 0.0, 
-            .bias = 0.0,
-            .deadZoneError = 0.1
-        }
-    };
-
-};
+};  // namespace ArmConfig
--- lib/controllers/ServoMotor.cpp
+++ lib/controllers/ServoMotor.cpp
@@ -1,20 +1,23 @@
 #include "ServoMotor.h"
+
 #include <cmath>
 
-ServoMotor::ServoMotor(PinName pwm, bool inverted, float min_pulsewidth_ms, float max_pulsewidth_ms, float limit) : 
-        Motor(NC, NC), m_servo(pwm, Servo::CONT_SERVO, limit, max_pulsewidth_ms, min_pulsewidth_ms), m_inverted(inverted) {};
+ServoMotor::ServoMotor(PinName pwm, bool inverted, float min_pulsewidth_ms, float max_pulsewidth_ms, float limit)
+    : Motor(NC, NC),
+      m_servo(pwm, Servo::CONT_SERVO, limit, max_pulsewidth_ms, min_pulsewidth_ms),
+      m_inverted(inverted){};
 
 ServoMotor::ServoMotor(t_motorConfig motorConfig) : ServoMotor(motorConfig.pwmPin, motorConfig.inverted) {}
 
 void ServoMotor::setPower(float percentage) {
-    m_servo.set_speed(percentage * (m_inverted ? -1.0 : +1.0));
+  m_servo.set_speed(percentage * (m_inverted ? -1.0 : +1.0));
 }
 
 ServoMotor& ServoMotor::operator=(int percentage) {
-    this->setPower(percentage);
-    return *this;
+  this->setPower(percentage);
+  return *this;
 }
 
 float ServoMotor::getPower() {
-    return m_servo.read();
+  return m_servo.read();
 }
