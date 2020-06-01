8d72cdbff2a7e8b619bcddb164d6b74f6e10e372
Wolfgang Windholtz <wwindholtz@me.com>
2020-06-01 13:16:26 -0400

    Clang formating

--- apps/science/main.cpp
+++ apps/science/main.cpp
@@ -1,29 +1,30 @@
-#include "mbed.h"
-#include "mbed_config.h"
-#include "can_config.h"
-
-#include "ScienceConfig.h"
+#include "ActuatorController.h"
+#include "CANBuffer.h"
+#include "CANMsg.h"
 #include "Encoder.h"
 #include "EncoderAbsolute_PWM.h"
 #include "EncoderRelative_Quadrature.h"
-#include "ActuatorController.h"
-#include "CANMsg.h"
-#include "CANBuffer.h"
-#include "Servo.h"
 #include "MoistureSensor.h"
+#include "ScienceConfig.h"
+#include "Servo.h"
+#include "can_config.h"
+#include "mbed.h"
+#include "mbed_config.h"
 
-//motors
+// motors
 Motor indexerMotor(MTR_PWM_1, MTR_DIR_1, false);
 Motor elevatorMotor(MTR_PWM_2, MTR_DIR_2, false);
 
-//Servo
-Servo coverServo(SRVO_PWM_1, ScienceConfig::coverServoType, ScienceConfig::coverServoRange, ScienceConfig::coverServoMaxPulse, ScienceConfig::coverServoMinPulse);
-Servo diggerServo(SRVO_PWM_2, ScienceConfig::diggerServoType, ScienceConfig::diggerServoRange, ScienceConfig::diggerServoMaxPulse, ScienceConfig::diggerServoMinPulse);
+// Servo
+Servo coverServo(SRVO_PWM_1, ScienceConfig::coverServoType, ScienceConfig::coverServoRange,
+                 ScienceConfig::coverServoMaxPulse, ScienceConfig::coverServoMinPulse);
+Servo diggerServo(SRVO_PWM_2, ScienceConfig::diggerServoType, ScienceConfig::diggerServoRange,
+                  ScienceConfig::diggerServoMaxPulse, ScienceConfig::diggerServoMinPulse);
 
-//encoders
+// encoders
 EncoderAbsolute_PWM elevatorEncoder(ScienceConfig::elevatorEncoderConfig);
 EncoderRelative_Quadrature indexerEncoder(ScienceConfig::centrifugeEncoderConfig);
-//limit switches
+// limit switches
 
 DigitalIn indexerLimFront(LIM_SW_1);
 DigitalIn indexerLimBack(LIM_SW_2);
@@ -31,10 +32,12 @@ DigitalIn elevatorLimTop(LIM_SW_3);
 DigitalIn elevatorLimBottom(LIM_SW_4);
 
 // Actuators
-ActuatorController indexerActuator(ScienceConfig::indexerActuatorConfig, indexerMotor, indexerEncoder, indexerLimFront, indexerLimBack);
-ActuatorController elevatorActuator(ScienceConfig::diggerLiftActuatorConfig, elevatorMotor, elevatorEncoder, elevatorLimBottom, elevatorLimTop);
+ActuatorController indexerActuator(ScienceConfig::indexerActuatorConfig, indexerMotor, indexerEncoder, indexerLimFront,
+                                   indexerLimBack);
+ActuatorController elevatorActuator(ScienceConfig::diggerLiftActuatorConfig, elevatorMotor, elevatorEncoder,
+                                    elevatorLimBottom, elevatorLimTop);
 
-//I2C
+// I2C
 
 MoistureSensor moistureSensor = MoistureSensor(TEMP_MOIST_I2C_SDA, TEMP_MOIST_I2C_SCL);
 
@@ -45,26 +48,24 @@ DigitalOut ledB(LED_B);
 
 CAN can(CAN1_RX, CAN1_TX, ROVER_CANBUS_FREQUENCY);
 
-static mbed_error_status_t setMotionData(CANMsg &msg)
-{
-    float motionData;
-    msg.getPayload(motionData);
+static mbed_error_status_t setMotionData(CANMsg &msg) {
+  float motionData;
+  msg.getPayload(motionData);
 
-    switch (msg.id)
-    {
+  switch (msg.id) {
     case CANID::SET_INDEXER_POS:
-        return indexerActuator.setMotionData(motionData);
+      return indexerActuator.setMotionData(motionData);
     case CANID::SET_ELEVATOR_POS:
-        return elevatorActuator.setMotionData(motionData);
+      return elevatorActuator.setMotionData(motionData);
     case CANID::SET_COVER_POS:
-        return coverServo.set_position(motionData);
+      return coverServo.set_position(motionData);
     case CANID::SET_DIGGER_POS:
-        return diggerServo.set_position(motionData);
+      return diggerServo.set_position(motionData);
     case CANID::SET_MOISTURE_SENSOR:
-        return moistureSensor.Is_Initialized();
+      return moistureSensor.Is_Initialized();
     default:
-        return MBED_ERROR_INVALID_ARGUMENT;
-    }
+      return MBED_ERROR_INVALID_ARGUMENT;
+  }
 }
 const int k_interval_ms = 500;
 
@@ -72,93 +73,83 @@ const int k_interval_ms = 500;
 Thread rxCANProcessorThread;
 Thread txCANProcessorThread;
 
-static CANMsg::CANMsgHandlerMap canHandleMap = {
-    {CANID::SET_INDEXER_POS, setMotionData},
-    {CANID::SET_ELEVATOR_POS, setMotionData},
-    {CANID::SET_COVER_POS, setMotionData},
-    {CANID::SET_DIGGER_POS, setMotionData},
-    {CANID::SET_MOISTURE_SENSOR, setMotionData}};    
+static CANMsg::CANMsgHandlerMap canHandleMap = {{CANID::SET_INDEXER_POS, setMotionData},
+                                                {CANID::SET_ELEVATOR_POS, setMotionData},
+                                                {CANID::SET_COVER_POS, setMotionData},
+                                                {CANID::SET_DIGGER_POS, setMotionData},
+                                                {CANID::SET_MOISTURE_SENSOR, setMotionData}};
 
-void rxCANProcessor()
-{
-    CANMsg rxMsg;
+void rxCANProcessor() {
+  CANMsg rxMsg;
 
-    while (true)
-    {
-        if (can.read(rxMsg))
-        {
-            if (canHandleMap.count(rxMsg.id) > 0)
-            {
-                canHandleMap[rxMsg.id](rxMsg);
-            }
-            else
-            {
-                // ruh roh
-            }
-        }
+  while (true) {
+    if (can.read(rxMsg)) {
+      if (canHandleMap.count(rxMsg.id) > 0) {
+        canHandleMap[rxMsg.id](rxMsg);
+      } else {
+        // ruh roh
+      }
     }
+  }
 }
 
 // Send outgoing CAN messages
-void txCANProcessor()
-{
-    const int txPeriod_millisec = 500;
-    CANMsg txMsg;
+void txCANProcessor() {
+  const int txPeriod_millisec = 500;
+  CANMsg txMsg;
 
-    while (true)
-    {
-        txMsg.id = SEND_INDEXER_POS;
-        txMsg.setPayload(indexerActuator.getAngle_Degrees());
-        can.write(txMsg);
-        ThisThread::sleep_for(txPeriod_millisec);
+  while (true) {
+    txMsg.id = SEND_INDEXER_POS;
+    txMsg.setPayload(indexerActuator.getAngle_Degrees());
+    can.write(txMsg);
+    ThisThread::sleep_for(txPeriod_millisec);
 
-        txMsg.id = SEND_ELEVATOR_POS;
-        txMsg.setPayload(elevatorActuator.getAngle_Degrees());
-        can.write(txMsg);
-        ThisThread::sleep_for(txPeriod_millisec);
+    txMsg.id = SEND_ELEVATOR_POS;
+    txMsg.setPayload(elevatorActuator.getAngle_Degrees());
+    can.write(txMsg);
+    ThisThread::sleep_for(txPeriod_millisec);
 
-        txMsg.id = SEND_COVER_POS;
-        txMsg.setPayload(coverServo.read());
-        can.write(txMsg);
-        ThisThread::sleep_for(txPeriod_millisec);
+    txMsg.id = SEND_COVER_POS;
+    txMsg.setPayload(coverServo.read());
+    can.write(txMsg);
+    ThisThread::sleep_for(txPeriod_millisec);
 
-        txMsg.id = SEND_DIGGER_POS;
-        txMsg.setPayload(diggerServo.read());
-        can.write(txMsg);
-        ThisThread::sleep_for(txPeriod_millisec);
+    txMsg.id = SEND_DIGGER_POS;
 
-        txMsg.id = SEND_DIGGER_POS;
-        txMsg.setPayload(diggerServo.read());
-        can.write(txMsg);
-        ThisThread::sleep_for(txPeriod_millisec);
+    txMsg.setPayload(diggerServo.read());
+    can.write(txMsg);
+    ThisThread::sleep_for(txPeriod_millisec);
 
-        // Read moisture returns an unsigned number so it needs to be cast to an int to be handled
-        txMsg.id = SEND_MOISTURE;
-        txMsg.setPayload((int)moistureSensor.Read_Moisture());
-        can.write(txMsg);
-        ThisThread::sleep_for(txPeriod_millisec);
+    txMsg.id = SEND_DIGGER_POS;
+    txMsg.setPayload(diggerServo.read());
+    can.write(txMsg);
+    ThisThread::sleep_for(txPeriod_millisec);
 
-        txMsg.id = SEND_TEMPERATURE;
-        txMsg.setPayload(moistureSensor.Read_Temperature());
-        can.write(txMsg);
-        ThisThread::sleep_for(txPeriod_millisec);
-    }
+    // Read moisture returns an unsigned number so it needs to be cast to an int to be handled
+    txMsg.id = SEND_MOISTURE;
+    txMsg.setPayload((int)moistureSensor.Read_Moisture());
+    can.write(txMsg);
+    ThisThread::sleep_for(txPeriod_millisec);
+
+    txMsg.id = SEND_TEMPERATURE;
+    txMsg.setPayload(moistureSensor.Read_Temperature());
+    can.write(txMsg);
+    ThisThread::sleep_for(txPeriod_millisec);
+  }
 }
 
-int main()
-{
-    printf("\r\n\r\n");
-    printf("SCIENCE APP STARTED!\r\n");
-    printf("====================\r\n");
+int main() {
+  printf("\r\n\r\n");
+  printf("SCIENCE APP STARTED!\r\n");
+  printf("====================\r\n");
 
-    rxCANProcessorThread.start(rxCANProcessor);
-    txCANProcessorThread.start(txCANProcessor);
+  rxCANProcessorThread.start(rxCANProcessor);
+  txCANProcessorThread.start(txCANProcessor);
 
-    while (true)
-    {
-        indexerActuator.update();
-        elevatorActuator.update();
+  while (true) {
+    indexerActuator.update();
+    elevatorActuator.update();
 
-        ThisThread::sleep_for(2);
-    }
+    ThisThread::sleep_for(2);
+  }
 }
