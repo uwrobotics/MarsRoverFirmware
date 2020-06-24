61d3c703c96cf35a2d65d69d37d6623ce8195f43
yreda1999 <yreda@uwaterloo.ca>
2020-06-23 20:22:57 -0400

    fixed test-moisture

--- apps/test-moisture/main.cpp
+++ apps/test-moisture/main.cpp
@@ -1,9 +1,8 @@
 #include "MoistureSensor.h"
 #include "mbed.h"
-#include "rover_config.h"
+#include "hw_bridge.h"
 
 DigitalOut led1(LED1);
-Serial pc(SERIAL_TX, SERIAL_RX, ROVER_DEFAULT_SERIAL_BAUD_RATE);
 
 int main() {
   led1 = 0;  // start with LED off
@@ -21,25 +20,25 @@ int main() {
 
     led1 = (sensor.Is_Initialized());  // turn on LED if the sensor's HW_ID code matches the known value
 
-    pc.printf("\r\nChecking Device ID...\r\n");  // read device HW_ID, reading of 85 is expected
+    printf("\r\nChecking Device ID...\r\n");  // read device HW_ID, reading of 85 is expected
     HW_ID = sensor.Read_HW_ID();
-    pc.printf("HW ID: %d \r\n", HW_ID);
+    printf("HW ID: %d \r\n", HW_ID);
 
     ThisThread::sleep_for(100);
 
-    pc.printf("\r\nReading Moisture...\r\n");  // read moisture from sensor, reading of 65534 indicates unsuccessful
+    printf("\r\nReading Moisture...\r\n");  // read moisture from sensor, reading of 65534 indicates unsuccessful
                                                // initialization
     moisture = sensor.Read_Moisture();
-    pc.printf("Moisture: %d \r\n", moisture);
+    printf("Moisture: %d \r\n", moisture);
 
     ThisThread::sleep_for(100);
 
-    pc.printf("\r\nReading Temperature...\r\n");  // read temperature from sensor, reading of -273.0 indicates
+    printf("\r\nReading Temperature...\r\n");  // read temperature from sensor, reading of -273.0 indicates
                                                   // unsuccessful initialization
     temperature = sensor.Read_Temperature();
-    pc.printf("Temperature: %f \r\n", temperature);
+    printf("Temperature: %f \r\n", temperature);
 
-    pc.printf("\r\n-----------------------------\r\n");
+    printf("\r\n-----------------------------\r\n");
 
     ThisThread::sleep_for(100);
   }
