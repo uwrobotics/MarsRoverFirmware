dce6275ae748b4aed5d3b59287ddf93dd1dac468
yreda1999 <yreda@uwaterloo.ca>
2020-06-23 20:07:36 -0400

    reached moisture app

--- apps/arm/main.cpp
+++ apps/arm/main.cpp
@@ -7,6 +7,7 @@
 #include "Encoder.h"
 #include "EncoderAbsolute_PWM.h"
 #include "EncoderRelative_Quadrature.h"
+#include "hw_bridge.h"
 #include "Servo.h"
 #include "mbed.h"
 #include "mbed_config.h"
