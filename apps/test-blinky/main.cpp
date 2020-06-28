f6f24fbca7136cc6f7cc7d5abf8942ea2dacde8a
Melvin Wang <melvin.mc.wang@gmail.com>
2020-06-28 18:45:34 -0400

    update blinky to latest

--- apps/test-blinky/main.cpp
+++ apps/test-blinky/main.cpp
@@ -1,30 +1,19 @@
 /* mbed Microcontroller Library
- * Copyright (c) 2018 ARM Limited
+ * Copyright (c) 2019 ARM Limited
  * SPDX-License-Identifier: Apache-2.0
  */
 
-#include "SystemReport.h"
 #include "mbed.h"
 
-DigitalOut led1(LED1);
+// Blinking rate in milliseconds
+#define BLINKING_RATE 500ms
 
-#define PRINT_AFTER_N_LOOPS 20
-
-// main() runs in its own thread in the OS
 int main() {
-  SystemReport sys_state(SLEEP_TIME * PRINT_AFTER_N_LOOPS /* Loop delay time in ms */);
+  // Initialise the digital pin LED1 as an output
+  DigitalOut led(LED1);
 
-  int count = 0;
   while (true) {
-    // Blink LED and wait 0.5 seconds
-    led1 = !led1;
-    ThisThread::sleep_for(5s);
-
-    if ((0 == count) || (PRINT_AFTER_N_LOOPS == count)) {
-      // Following the main thread wait, report on the current system status
-      sys_state.report_state();
-      count = 0;
-    }
-    ++count;
+    led = !led;
+    ThisThread::sleep_for(BLINKING_RATE);
   }
 }
