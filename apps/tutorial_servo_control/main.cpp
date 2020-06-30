4abdd2d8e79e1206d3969c84437454e58f2b5367
Yehia F <yehia.fahmy@uwaterloo.ca>
2020-06-29 21:18:57 -0400

    first commit

---
+++ apps/tutorial_servo_control/main.cpp
@@ -0,0 +1,16 @@
+#include "mbed.h"
+
+AnalogIn potVoltageIn(PA_0);
+PmwOut servoPwmOut(PA_1);
+
+int main()
+{
+
+  servoPwmOut.period(0.020);
+
+  while(1){
+    float potVoltage = potVoltageIn.read();
+    servoPwmOut.pulsewidth(0.001 + 0.001 * potVoltage / 3.3);
+  }
+  return 0;
+}
