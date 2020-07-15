upadhyaydhruv/MarsRover2020-firmware

add a third axis (roll) for the gimbal. the pin for the servo has not been chosen by electrical yet, so for now, just use an arbitrary free pin, will need to update this

apps/gimbtonomy/main.cpp
@@ -16,12 +16,22 @@
 #include "mbed_config.h"
 #include "rover_config.h"
 
+#define DEBUG
+
 #define ACK_FLAG (1UL << 0)
 
 // Init. Components
 // Servos
-ServoMotor panServoMotor(SRVO_PWM_CR, 44.0, 2.1, 0.9);  // 44 RPM (264 deg/sec) at 4.8V, max->2100us PW, min->900us PW
-LimServo pitchServo(SRVO_PWM_MG, 180, 2.1, 0.9);
+/*
+• Pan: HS-1425 CR (https://www.robotshop.com/en/hitec-hsr-1425cr-continuous-rotation-servo.html)
+• Tilt: HS-422 (https://www.robotshop.com/en/hitec-hs-422-servo-motor.html)
+• Roll: SG90 (https://datasheetspdf.com/pdf/791970/TowerPro/SG90/1)
+*/
+ServoMotor panServoMotor(SRVO_PWM_CR, false, 2.1, 0.9,
+                         44);  // 44 RPM (264 deg/sec) at 4.8V, max->2100us PW, min->900us PW.
+LimServo tiltServo(SRVO_PWM_HS, 180, 2.1, 0.9);
+/* @TODO: electrical hasn't choose a pin for this servo yet, I'm just using a random free pin for this for now*/
+LimServo rollServo(SRVO_PWM_SG, 180, 2, 1);
 
 // Absolute encoder
 EncoderAbsolute_PWM panEncoder(GimbtonomyConfig::panEncoderConfig);
@@ -74,7 +84,7 @@ void handleSetNeoPixelColor(CANMsg *p_newMsg) {
 void rxCANProcessor() {
   const int rxPeriod_millisec = 2;
 
-  float pan_pos = 0.0, pan_speed = 0.0, pitch_pos = 0.0;
+  float pan_pos = 0.0, pan_speed = 0.0, tilt_pos = 0.0, roll_pos = 0.0;
 
   while (true) {
     if (can1.read(rxMsg)) {
@@ -89,9 +99,14 @@ void rxCANProcessor() {
           panServoMotor.servoSetSpeed(pan_speed);
           break;
 
-        case CANID::SET_GIMBAL_PITCH_POS:
-          rxMsg.getPayload(pitch_pos);
-          pitchServo.setPosition(pitch_pos);
+        case CANID::SET_GIMBAL_TILT_POS:
+          rxMsg.getPayload(tilt_pos);
+          tiltServo.setPosition(tilt_pos);
+          break;
+
+        case CANID::SET_GIMBAL_ROLL_POS:
+          rxMsg.getPayload(roll_pos);
+          rollServo.setPosition(roll_pos);
           break;
 
         case CANID::SET_NEOPIXEL:
@@ -116,12 +131,16 @@ void txCANProcessor() {
     event_flags.wait_any(ACK_FLAG);
 
     // Send an acknowledgement CANMsg back to the Jetson
+#ifdef DEBUG
     printf("Sending neopixel acknowledgement message\r\n");
+#endif
     txMsg.clear();
     txMsg.id = CANID::NEOPIXEL_ACK;
     txMsg << true;
     can1.write(txMsg);
+#ifdef DEBUG
     printf("Updating neo pixels\r\n");
+#endif
   }
 }
 
@@ -135,9 +154,11 @@ int main() {
 
   while (true) {
     // Output info over serial
-    printf("pitchServo(pos): %f \r\n", pitchServo.read());
+#ifdef DEBUG
+    printf("tiltServo(pos): %f \r\n", tiltServo.read());
+    printf("rollServo(pos): %f \r\n", rollServo.read());
     printf("panServo(speed): %f \r\n", panServoMotor.servoRead());
-
+#endif
     panServoActuator.update();
 
     ThisThread::sleep_for(2);

uwrobotics/MarsRoverFirmware

apps/gimbtonomy/main.cpp
@@ -16,12 +16,22 @@
 #include "mbed_config.h"
 #include "rover_config.h"
 
+#define DEBUG
+
 #define ACK_FLAG (1UL << 0)
 
 // Init. Components
 // Servos
-ServoMotor panServoMotor(SRVO_PWM_CR, 44.0, 2.1, 0.9);  // 44 RPM (264 deg/sec) at 4.8V, max->2100us PW, min->900us PW
-LimServo pitchServo(SRVO_PWM_MG, 180, 2.1, 0.9);
+/*
+• Pan: HS-1425 CR (https://www.robotshop.com/en/hitec-hsr-1425cr-continuous-rotation-servo.html)
+• Pitch: HS-422 (https://www.robotshop.com/en/hitec-hs-422-servo-motor.html)
+• Roll: SG90 (https://datasheetspdf.com/pdf/791970/TowerPro/SG90/1)
+*/
+ServoMotor panServoMotor(SRVO_PWM_CR, false, 2.1, 0.9,
+                         38);  // 38 RPM (228 deg/sec) at 4.8V, max->2100us PW, min->900us PW.
+LimServo pitchServo(SRVO_PWM_HS, 180, 2.1, 0.9);
+/* @TODO: electrical hasn't choose a pin for this servo yet, I'm just using a random free pin for this for now*/
+LimServo rollServo(SRVO_PWM_SG, 180, 2, 1);
 
 // Absolute encoder
 EncoderAbsolute_PWM panEncoder(GimbtonomyConfig::panEncoderConfig);
@@ -74,7 +84,8 @@ void handleSetNeoPixelColor(CANMsg *p_newMsg) {
 void rxCANProcessor() {
   const int rxPeriod_millisec = 2;
 
-  float pan_pos = 0.0, pan_speed = 0.0, pitch_pos = 0.0;
+  float pan_pos = 0.0, pan_speed = 0.0, pitch_pos = 0.0, roll_pos = 0.0;
+  ActuatorController::t_actuatorControlMode controlMode;
 
   while (true) {
     if (can1.read(rxMsg)) {
@@ -89,9 +100,19 @@ void rxCANProcessor() {
           panServoMotor.servoSetSpeed(pan_speed);
           break;
 
+        case CANID::SET_GIMBAL_PAN_MODE:
+          rxMsg.getPayload(controlMode);
+          panServoActuator.setControlMode(controlMode);
+          break;
+
         case CANID::SET_GIMBAL_PITCH_POS:
           rxMsg.getPayload(pitch_pos);
           pitchServo.setPosition(pitch_pos);
+          break;
+
+        case CANID::SET_GIMBAL_ROLL_POS:
+          rxMsg.getPayload(roll_pos);
+          rollServo.setPosition(roll_pos);
           break;
 
         case CANID::SET_NEOPIXEL:
@@ -116,12 +137,16 @@ void txCANProcessor() {
     event_flags.wait_any(ACK_FLAG);
 
     // Send an acknowledgement CANMsg back to the Jetson
+#ifdef DEBUG
     printf("Sending neopixel acknowledgement message\r\n");
+#endif
     txMsg.clear();
     txMsg.id = CANID::NEOPIXEL_ACK;
     txMsg << true;
     can1.write(txMsg);
+#ifdef DEBUG
     printf("Updating neo pixels\r\n");
+#endif
   }
 }
 
@@ -135,9 +160,11 @@ int main() {
 
   while (true) {
     // Output info over serial
+#ifdef DEBUG
     printf("pitchServo(pos): %f \r\n", pitchServo.read());
+    printf("rollServo(pos): %f \r\n", rollServo.read());
     printf("panServo(speed): %f \r\n", panServoMotor.servoRead());
-
+#endif
     panServoActuator.update();
 
     ThisThread::sleep_for(2);