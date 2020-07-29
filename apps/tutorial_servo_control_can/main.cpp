8bc246cc527bb35d018a9cafe70bb1c0a11ecbb7
Dhruv Upadhyay <dmupadhyay12@gmail.com>
2020-07-29 17:17:12 -0400

    Formatted code

--- apps/tutorial_servo_control_can/main.cpp
+++ apps/tutorial_servo_control_can/main.cpp
@@ -1,19 +1,19 @@
-#include "TutorialServo.h"
 #include "CANMsg.h"
+#include "TutorialServo.h"
+#include "mbed.h"
 
 CAN can(D3, D4);
 CANMsg msg;
 uint8 in;
 TutorialServo servo(PA_1, 180.0);
 
-int main()
-{
-    can.frequency(500000);
+int main() {
+  can.frequency(500000);
 
-    while (true) {
-        if (can.read(msg)){
-            msg >> in;
-        }
-        servo.setPositionInDegrees(180*in/100);    
+  while (true) {
+    if (can.read(msg)) {
+      msg >> in;
     }
+    servo.setPositionInDegrees(180 * in / 100);
+  }
 }
