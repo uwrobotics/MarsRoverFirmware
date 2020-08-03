d3569e05ab4e132ed4caa69c54a600f611787821
Yehia F <yehia.fahmy@uwaterloo.ca>
2020-08-03 18:17:21 -0400

    started CAN implementation

--- apps/tutorial-servo/main.cpp
+++ apps/tutorial-servo/main.cpp
@@ -1,12 +1,25 @@
 #include "CANMsg.h"
 #include "TutorialServo.h"
+
 CAN can(CAN_RX, CAN_TX);
 CANMsg rxMsg;
-CANMsg txMsg;
+uint8_t counter = 0;
+
+#define TX_ID 0x100
+#define RX_ID 0x101
 
 int main() {
-  // declares an object of type TutorialServo called Motor1 to demonstrate that the object works
-  TutorialServo Motor1(PA_1);
+  can.frequency(500000);       // set bit rate to 50kbps
+  TutorialServo Motor1(PA_1);  // declares an object of type TutorialServo
+
+  while (1) {
+    if (can.read(rxMsg)) {
+      // we have recieved a message
+    } else {
+      // we have not recieved a message
+    }
+  }
+
   Motor1.setAngleRangeInDegrees(200);
   Motor1.setPulsewidthRangeInMs(1, 3);
   Motor1.setPositionInDegrees(90);
