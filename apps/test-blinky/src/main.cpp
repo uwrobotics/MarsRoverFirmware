a8a265839ccc3a0bf23e503e3fceac9eeb935fc1
jahnavithota2011 <34670246+jahnavithota2011@users.noreply.github.com>
2020-08-19 21:45:10 +0400

    Update main.cpp

--- apps/test-blinky/src/main.cpp
+++ apps/test-blinky/src/main.cpp
@@ -1,20 +1,67 @@
-/* mbed Microcontroller Library
- * Copyright (c) 2019 ARM Limited
- * SPDX-License-Identifier: Apache-2.0
- */
 
 #include "mbed.h"
 
-// Blinking rate in milliseconds
-#define BLINKING_RATE 500ms
+CAN can(CAN1_RX, CAN1_TX);
+
+
+class TutorialServo{
+
+  private:
+
+        PwmOut m_servoPwmOut;
+        float m_servoRangeInDegrees;
+        float m_minPulsewidthInMs;
+        float m_maxPulsewidthInMs;
+
+  public:
+
+        TutorialServo(PinName servoPin, float servoRangeInDegrees = 180, float minP$
+                 m_servoRangeInDegrees =  servoRangeInDegrees;
+                 m_minPulsewidthInMs = minPulsewidthInMs;
+                 m_maxPulsewidthInMs = maxPulsewidthInMs;
+
+        }
+
+        void setAngleRangeInDegrees(float degrees){
+
+                m_servoRangeInDegrees = degrees;
+        }
+
+        void setPulsewidthRangeInMs(float minPulsewidthMs, float maxPulsewidthMs){
+          
+                m_minPulsewidthInMs = minPulsewidthMs;
+                m_maxPulsewidthInMs = maxPulsewidthMs;
+
+        }
+
+        void setPositionInDegrees(float degrees){
+
+                float slope{0.0}, pulse{0.0};
+                slope = (m_maxPulsewidthInMs - m_minPulsewidthInMs)/(m_servoRangeIn$                pulse = (m_minPulsewidthInMs/1000) + degrees/slope;
+                m_servoPwmOut.pulsewidth(pulse);
+
+        }
+
+        float getRangeInDegrees(){
+
+                return m_servoRangeInDegrees;
+          
+        }
+
+};
 
 int main() {
-  // Initialise the digital pin LED1 as an output
-  DigitalOut led(LED1);
 
-  while (true) {
-    led = !led;
-    printf("Current LED State is %s\n", led ? "ON" : "OFF");
-    ThisThread::sleep_for(BLINKING_RATE);
-  }
+        TutorialServo servo(PA_1);
+        CANMessage msg;
+
+        while (true) {
+                if(can.read(msg)){
+
+                        printf("Message received: %d\n", msg.data[0]);
+                        float range{servo.getRangeInDegrees()};
+                        servo.setPositionInDegrees(range * msg.data[0]);
+                }
+        }
+        return 0;
 }
