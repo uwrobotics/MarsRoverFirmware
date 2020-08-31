55942a391b317ff9be6eb4a4758017e6a5f0c493
yreda1999 <yreda@uwaterloo.ca>
2020-08-31 16:30:09 -0400

    fix test-can

--- apps/test-can/src/main.cpp
+++ apps/test-can/src/main.cpp
@@ -9,8 +9,8 @@ DigitalOut ledRX(LED2);
 Timer timer;
 uint8_t counter = 0;
 
-#define TX_ID 0x100
-#define RX_ID 0x101
+#define TX_ID (HWBRIDGE::CANID)0x100
+#define RX_ID (HWBRIDGE::CANID)0x101
 
 /**
  * @brief   Prints CAN msg to PC's serial terminal
@@ -40,7 +40,7 @@ int main(void) {
       timer.reset();                   // reset timer
       counter++;                       // increment counter
       txMsg.clear();                   // clear Tx message storage
-      txMsg.id = TX_ID;                // set ID
+      txMsg.setID(TX_ID);              // set ID
       txMsg << counter;                // copy counter value to CAN msg payload
       if (can.write(txMsg)) {          // transmit message
         printf("-------------------------------------\r\n");
@@ -59,7 +59,7 @@ int main(void) {
       printMsg(rxMsg);
 
       // Filtering performed by software:
-      if (rxMsg.id == RX_ID) {
+      if (rxMsg.getID() == RX_ID) {
         rxMsg >> counter;  // extract data from the received CAN message
         printf("  counter = %d\r\n", counter);
         timer.start();  // transmission lag
