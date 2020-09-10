c5f3aba19e5c8136ce22937883d8510b9d4b70fe
jahnavithota2011 <jthota2001@gmail.com>
2020-09-10 20:04:07 +0400

    Training corrections made

---
+++ apps/tutorial_servo_control/src/main.cpp
@@ -0,0 +1,21 @@
+#include "mbed.h"
+#include "header.h"
+
+
+CAN can(CAN1_RX, CAN1_TX);
+
+int main() {
+
+        TutorialServo servo(PA_1);
+        CANMessage msg;
+
+        while (true) {
+                if(can.read(msg)){
+
+                        printf("Message received: %d\n", msg.data[0]);
+                        float range{servo.getRangeInDegrees()};
+                        servo.setPositionInDegrees(range * msg.data[0]);
+                }
+        }
+        return 0;
+}
