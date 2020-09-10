668eb206a80e37681df4ba59612af77e0e3e0ece
niiquaye <68795163+niiquaye@users.noreply.github.com>
2020-09-10 00:26:38 -0400

    Update TutorialServo.cpp
    Added Younes' Comments

--- lib/TutorialServo/TutorialServo.cpp
+++ lib/TutorialServo/TutorialServo.cpp
@@ -23,19 +23,27 @@ void TutorialServo::setPulseWidthRangeInMs(float minPulseWidthInMs, float maxPul
 	_minPulseWidthInMs = minPulseWidthInMs/1000;
 	_maxPulseWidthInMs = maxPulseWidthInMs/1000;
 }
-void TutorialServo::setServoPositionInDegrees(float degrees) {
-	
-	float value;
-	value = ((_maxPulseWidthInMs - _minPulseWidthInMs) / _servoRangeInDegrees) * degrees) + _minPulseWidthInMs;
-	_servoPwmOut.pulsewidth(value);
-}
 
 void TutorialServo::setServoPositionWithPercent(float percent) {
   //user cannot enter a percent bigger than 100
   percent = (percent >100) ? 100: percent;
-  
+  //user cannot enter a percent less than 0
+  percent = (percent < 0) ? 0: percent;
   float value;
   value = ((_maxPulseWidthInMs - _minPulseWidthInMs) / 100) * percent) + _minPulseWidthInMs;
-  _servoPWmOut.pulsewidth(value);
+  _servoPwmOut.pulsewidth(value);
 
+}
+void TutorialServo::setServoPositionInDegrees(float degrees) {
+	
+	float value;
+
+	value = (degrees/_servoRangeInDegrees)*100;
+	setServoPositionWithPercent(value);
+	/* alternate method to setting servo position
+
+	value = ((_maxPulseWidthInMs - _minPulseWidthInMs) / _servoRangeInDegrees) * degrees) + _minPulseWidthInMs;
+	_servoPwmOut.pulsewidth(value);
+
+	*/
 }
