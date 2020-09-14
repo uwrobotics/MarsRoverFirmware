85cc0d8eb08df53186533c6287f8b52f4da56d59
niiquaye <68795163+niiquaye@users.noreply.github.com>
2020-09-14 13:02:02 -0400

    Update TutorialServo.cpp

--- lib/TutorialServo/TutorialServo.cpp
+++ lib/TutorialServo/TutorialServo.cpp
@@ -1,28 +1,26 @@
 #include "TutorialServo.h"
 
-
-
-TutorialServo::TutorialServo(PinName servoPin, float servoRangeInDegrees , float minPulsewidthInMs , float maxPulsewidthInMs)                           
-	: _servoPwmOut(servoPin),
+TutorialServo::TutorialServo(PinName servoPin, float servoRangeInDegrees , float minPulsewidthInMs , 
+			     float maxPulsewidthInMs)                           
+    : _servoPwmOut(servoPin),
       _minPulseWidthInMs(minPulsewidthInMs),
       _maxPulseWidthInMs(maxPulsewidthInMs),
       _servoRangeInDegrees(servoRangeInDegrees) 
 {
-	_minPulseWidthInMs = _minPulseWidthInMs / 1000;
-	_maxPulseWidthInMs = _maxPulseWidthInMs / 1000;
-	//default period of servo is 50Hz or 20 milliseconds 
-	_servoPwmOut.period(0.020);
+ _minPulseWidthInMs = _minPulseWidthInMs / 1000;
+ _maxPulseWidthInMs = _maxPulseWidthInMs / 1000;
+ //default period of servo is 50Hz or 20 milliseconds 
+ _servoPwmOut.period(0.020);
 }
 
 void TutorialServo::setAngleRangeInDegrees(float degrees) {
-	_servoRangeInDegrees = degrees; 
+  _servoRangeInDegrees = degrees; 
 }
 
 void TutorialServo::setPulseWidthRangeInMs(float minPulseWidthInMs, float maxPulseWidthInMs) {
   _minPulseWidthInMs = minPulseWidthInMs / 1000;
   _maxPulseWidthInMs = maxPulseWidthInMs / 1000;
 }
-
 
 void TutorialServo::setServoPositionWithPercent(float percent) {
   // user cannot enter a percent bigger than 100
@@ -32,13 +30,12 @@ void TutorialServo::setServoPositionWithPercent(float percent) {
   float value;
   value = ((_maxPulseWidthInMs - _minPulseWidthInMs) / 100) * percent) + _minPulseWidthInMs;
   _servoPwmOut.pulsewidth(value);
-
 }
-void TutorialServo::setServoPositionInDegrees(float degrees) {
-	
+
+void TutorialServo::setServoPositionInDegrees(float degrees) {	
   float value;
 
-  value = (degrees/_servoRangeInDegrees)*100;
+  value = (degrees / _servoRangeInDegrees) * 100;
   setServoPositionWithPercent(value);
   /* alternate method to setting servo position
 
