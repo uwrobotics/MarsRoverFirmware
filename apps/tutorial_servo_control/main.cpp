fb9924c19c7cdcc360390b14d09cc8e19f52e6ce
niiquaye <68795163+niiquaye@users.noreply.github.com>
2020-09-16 19:48:24 -0400

    Update main.cpp

--- apps/tutorial_servo_control/main.cpp
+++ apps/tutorial_servo_control/main.cpp
@@ -2,11 +2,11 @@
 AnalogIn potVoltageIn(PA_0);
 PwmOut servoPwmOut(PA_1);
 
-int main(){
+int main() {
   servoPwmOut.period(0.020);
-  while(1){
+  while (1) {
     float potVoltage = potVoltageIn.read();
-    servoPwmOut.pulsewidth(0.001 + 0.001*potVoltage/3.3);
+    servoPwmOut.pulsewidth(0.001 + 0.001 * potVoltage / 3.3);
 
   }
   return 0;
