cdc1f5a4848b3f6e877a1fad762c826344b34aae
niiquaye <68795163+niiquaye@users.noreply.github.com>
2020-09-16 20:28:33 -0400

    Update TutorialServo.cpp

--- lib/TutorialServo/TutorialServo.cpp
+++ lib/TutorialServo/TutorialServo.cpp
@@ -7,7 +7,8 @@ TutorialServo::TutorialServo(PinName servoPin, float servoRangeInDegrees, float 
       _maxPulseWidthInMs(maxPulsewidthInMs),
       _servoRangeInDegrees(servoRangeInDegrees) {	      
   _minPulseWidthInMs = _minPulseWidthInMs / 1000;
-  _maxPulseWidthInMs = _maxPulseWidthInMs / 1000;  
+  _maxPulseWidthInMs = _maxPulseWidthInMs / 1000;
+  
   // default period of servo is 50Hz or 20 milliseconds
   _servoPwmOut.period(0.020);
 }
