3c41a9ecb0cf1ec5644104a063e2bb4b0ce2b8d9
niiquaye <68795163+niiquaye@users.noreply.github.com>
2020-09-16 20:12:25 -0400

    Update main.cpp

--- apps/tutorial_servo_control_can/main.cpp
+++ apps/tutorial_servo_control_can/main.cpp
@@ -1,13 +1,15 @@
-#include "TutorialServo.h"
+#include <cstdio>
+
 #include "CAN.h"
 #include "CANMsg.h"
+#include "TutorialServo.h"
 #include "hw_bridge.h"
 #include <cstdio>
 
 
-TutorialServo servo_can(PA_1); //create servo
+TutorialServo servo_can(PA_1);  // create servo
 
-//initialize CAN
+// initialize CAN
 
 CANMsg rxMsg;
 CANMsg txMsg;
@@ -16,28 +18,28 @@ CANMsg txMsg;
 CAN can(CAN_RX, CAN_TX, HWBRIDGE::ROVERCONFIG::ROVER_CANBUS_FREQUENCY);
 
 int main(){
-    float percent; 
-    rxMsg.id = RX_ID; //set message ID
+  float percent;
+  rxMsg.id = RX_ID;  // set message ID
 
-    while(1){
+  while (1) {
 
-        if (can.read(rxMsg)) {
+    if (can.read(rxMsg)) {
   
-        printf("-------------------------------------\r\n");
-        printf("CAN message received\r\n");
+      printf("-------------------------------------\r\n");
+      printf("CAN message received\r\n");
     
 
-    // Filtering performed by software:
-        if (rxMsg.id == RX_ID) {
-            rxMsg >> percent;  // extract data from the received CAN message
+      // Filtering performed by software:
+      if (rxMsg.id == RX_ID) {
+        rxMsg >> percent;  // extract data from the received CAN message
         
-                servo_can.setServoPositionWithPercent(percent);
+        servo_can.setServoPositionWithPercent(percent);
 
-            }	
+      }
  
-        }
-
     }
 
-    return 0;
+  }
+
+  return 0;
 }
