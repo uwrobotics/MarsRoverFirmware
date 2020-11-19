3899cfc1b4832449768e1dba64032f95bb0c3169
yreda1999 <yreda@uwaterloo.ca>
2020-11-18 20:23:46 -0500

    added tests for different iteration methods and tested

--- apps/test-lookup-table/src/main.cpp
+++ apps/test-lookup-table/src/main.cpp
@@ -10,6 +10,27 @@ mbed_error_status_t trivial2(CANMsg& msg);
 
 int main() {
   printf("\n LookupTable Test App Started \r\n");
+  printf("\r\n####### HOW TO ITERATE #######\r\n");
+  printf("Note: Won't be in order when iterating since based off hash-map. ");
+  printf("What's important is that key-value pairs match\r\n");
+  const lookup_table::LookupTable<std::string, int> week = {
+      {"Monday", 1}, {"Tuesday", 2}, {"Wednesday", 3}, {"Thursday", 4}, {"Friday", 5}, {"Saturday", 6}, {"Sunday", 7}};
+  // iterators:
+  for (auto it = week.begin(); it != week.end(); it++) {
+    printf("%s %d\r\n", it->first.c_str(), it->second);
+  }
+  printf("\r\n");
+  // range-based for loop
+  for (const auto& i : week) {
+    printf("%s %d\r\n", i.first.c_str(), i.second);
+  }
+  printf("\r\n");
+  // structured bindings
+  for (const auto& [key, value] : week) {
+    printf("%s %d\r\n", key.c_str(), value);
+  }
+
+  printf("\r\n####### HOW TO INDEX #######\r\n");
   // No default case
   const lookup_table::LookupTable<std::string, int> a = {{"Jan", 1}, {"Feb", 2}};
   printf("Existing Key: %d\r\n", a["Jan"].value_or(-1));
@@ -23,13 +44,6 @@ int main() {
   printf("Existing Key: %d\r\n", b[1]);
   printf("Non-Existing Key: %d\r\n", b[2]);
   printf("\r\n");
-  // Here's how you iterate over a map
-  // TODO IMPLEMENT THIS
-  /*for(auto [key, value] : b) {
-    // do things with key and value
-    (void) key;
-    (void) value;
-  }*/
 
   // Function pointer example
   const CANMsg::CANMsgHandlerMap c = {{HWBRIDGE::CANID::TPDO1, trivial1}, {HWBRIDGE::CANID::TPDO2, trivial2}};
--- lib/lookup-table/include/LookupTable.h
+++ lib/lookup-table/include/LookupTable.h
@@ -1,3 +1,8 @@
+/**
+ * This is a non-mutable data-type based off a std::unordered_map
+ * See test-lookup-table for usage
+ */
+
 #pragma once
 #include <iterator>
 #include <optional>
@@ -7,13 +12,12 @@ namespace lookup_table {
 namespace internal {
 struct UnusedDefaultValue {};
 }  // namespace internal
-// This a LUT type. It is not meant to be mutated after initilization.
 template <typename Key, typename Value, const auto defaultValue = internal::UnusedDefaultValue(),
           typename Hash = std::hash<Key>, typename KeyEqual = std::equal_to<Key>,
           typename Allocator = std::allocator<std::pair<const Key, Value>>>
 class LookupTable {
  private:
-  const std::unordered_map<Key, Value, Hash, KeyEqual, Allocator> _unordered_map;
+  const std::unordered_map<Key, Value, Hash, KeyEqual, Allocator> m_map;
 
  public:
   using key_type        = Key;
@@ -28,12 +32,13 @@ class LookupTable {
   using const_reference = const value_type &;
   using pointer         = typename std::allocator_traits<Allocator>::pointer;
   using const_pointer   = typename std::allocator_traits<Allocator>::const_pointer;
+  using const_iterator  = typename std::unordered_map<Key, Value, Hash, KeyEqual, Allocator>::const_iterator;
 
   LookupTable() = delete;
 
   // range based constructor
   template <typename InputIterator>
-  LookupTable(InputIterator first, InputIterator last) : _unordered_map(first, last) {}
+  LookupTable(InputIterator first, InputIterator last) : m_map(first, last) {}
   // copy constructor and assignment
   LookupTable(const LookupTable &copy) = default;
   LookupTable &operator=(const LookupTable &) = default;
@@ -43,36 +48,37 @@ class LookupTable {
   // initializer list constructor
   LookupTable(std::initializer_list<value_type> init, size_type bucket_count = 0, const Hash &hash = Hash(),
               const key_equal &equal = key_equal(), const Allocator &alloc = Allocator())
-      : _unordered_map(init, bucket_count, hash, equal, alloc) {}
+      : m_map(init, bucket_count, hash, equal, alloc) {}
 
-  typename std::unordered_map<Key, Value>::iterator begin() const {
-    return _unordered_map.begin();
+  const_iterator begin() const {
+    return m_map.begin();
   }
-  typename std::unordered_map<Key, Value>::iterator end() const {
-    return _unordered_map.end();
+
+  const_iterator end() const {
+    return m_map.end();
   }
 
   std::size_t size() const {
-    return _unordered_map.size();
+    return m_map.size();
   }
   std::size_t max_size() const {
-    return _unordered_map.max_size();
+    return m_map.max_size();
   }
 
   bool empty() const {
-    return _unordered_map.empty();
+    return m_map.empty();
   }
 
   template <typename V = Value>
   typename std::enable_if<std::is_same<V, decltype(defaultValue)>::value, V>::type at(const Key &key) const {
-    bool value_exists = _unordered_map.find(key) != _unordered_map.end();
-    return value_exists ? _unordered_map.at(key) : defaultValue;
+    bool value_exists = m_map.find(key) != m_map.end();
+    return value_exists ? m_map.at(key) : defaultValue;
   }
   template <typename V = Value>
   typename std::enable_if<!std::is_same<V, decltype(defaultValue)>::value, std::optional<V>>::type at(
       const Key &key) const {
-    bool value_exists = _unordered_map.find(key) != _unordered_map.end();
-    return value_exists ? std::optional<Value>{_unordered_map.at(key)} : std::nullopt;
+    bool value_exists = m_map.find(key) != m_map.end();
+    return value_exists ? std::optional<Value>{m_map.at(key)} : std::nullopt;
   }
 
   template <typename V = Value>
