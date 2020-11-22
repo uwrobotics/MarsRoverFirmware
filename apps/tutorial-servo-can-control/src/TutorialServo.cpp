8f7b05e94855e5d2bcfa8fe73429ddd7468f1a0f
Kiran Suren <47192545+kiransuren@users.noreply.github.com>
2020-11-21 23:33:58 -0500

    handle out of range parameter for set position

--- apps/tutorial-servo-can-control/src/TutorialServo.cpp
+++ apps/tutorial-servo-can-control/src/TutorialServo.cpp
@@ -1,4 +1,5 @@
 #include "TutorialServo.h"
+#include <cmath>
 
 TutorialServo::TutorialServo(PinName servoPin, float servoRangeInDegrees, float minPulsewidthInMs, float maxPulsewidthInMs)
               : m_servoRangeInDegrees(servoRangeInDegrees), m_minPulsewidthInMs(minPulsewidthInMs), m_maxPulsewidthInMs(maxPulsewidthInMs)
@@ -11,7 +12,7 @@ TutorialServo::TutorialServo(PinName servoPin, float servoRangeInDegrees, float 
 
 // Set servo position (ex. 45deg)
 void TutorialServo::setPositionInDegrees(const float degrees){
-    float finalPulsewidth = ((degrees/m_servoRangeInDegrees) * (m_maxPulsewidthInMs-m_minPulsewidthInMs)) + m_minPulsewidthInMs;
+    float finalPulsewidth = (((fabs(degrees) / m_servoRangeInDegrees) - floor(fabs(degrees)/m_servoRangeInDegrees)) * (m_maxPulsewidthInMs-m_minPulsewidthInMs)) + m_minPulsewidthInMs;
     m_servoPwmOut.pulsewidth_ms(finalPulsewidth);
 }
 
@@ -24,7 +25,7 @@ void TutorialServo::setPositionInDegrees(const float degrees){
 float TutorialServo::getServoRangeInDegrees() const{
     return m_servoRangeInDegrees;
 }
-
+(fabs(degrees) / m_servoRangeInDegrees) - floor(fabs(degrees)/m_servoRangeInDegrees)
 // Get the min pulse width in ms (ex: 1ms)
 float TutorialServo::getMinPulseWidthInMs() const{
     return m_minPulsewidthInMs;
