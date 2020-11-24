c573c731e28933c127cf5c40e4e4c34f8bb309e2
kiransuren <kirank.suren@gmail.com>
2020-11-24 13:37:28 -0500

    last clang-format?

--- apps/tutorial-servo-can-control/src/TutorialServo.cpp
+++ apps/tutorial-servo-can-control/src/TutorialServo.cpp
@@ -1,38 +1,40 @@
 #include "TutorialServo.h"
-#include <algorithm>
-TutorialServo::TutorialServo(PinName servoPin, float servoRangeInDegrees, float minPulsewidthInMs, float maxPulsewidthInMs)
-              : m_servoRangeInDegrees(servoRangeInDegrees), m_minPulsewidthInMs(minPulsewidthInMs), m_maxPulsewidthInMs(maxPulsewidthInMs)
-{
-    // Initialize servo pin and period
-    m_servoPwmOut(servoPin);
-    m_servoPwmOut.period_ms(m_periodInMs);
 
+#include <algorithm>
+TutorialServo::TutorialServo(PinName servoPin, float servoRangeInDegrees, float minPulsewidthInMs,
+                             float maxPulsewidthInMs)
+    : m_servoRangeInDegrees(servoRangeInDegrees),
+      m_minPulsewidthInMs(minPulsewidthInMs),
+      m_maxPulsewidthInMs(maxPulsewidthInMs) {
+  // Initialize servo pin and period
+  m_servoPwmOut(servoPin);
+  m_servoPwmOut.period_ms(m_periodInMs);
 }
 
 // Set servo position (ex. 45deg)
-void TutorialServo::setPositionInDegrees(const float degrees){
-
-    float finalPulsewidth = ((std::max(0,std::min(degrees,m_servoRangeInDegrees))/m_servoRangeInDegrees) * (m_maxPulsewidthInMs-m_minPulsewidthInMs)) + m_minPulsewidthInMs;
-    m_servoPwmOut.pulsewidth_ms(finalPulsewidth);
+void TutorialServo::setPositionInDegrees(const float degrees) {
+  float finalPulsewidth = ((std::max(0, std::min(degrees, m_servoRangeInDegrees)) / m_servoRangeInDegrees) *
+                           (m_maxPulsewidthInMs - m_minPulsewidthInMs)) +
+                          m_minPulsewidthInMs;
+  m_servoPwmOut.pulsewidth_ms(finalPulsewidth);
 }
 
- void TutorialServo::setPositionInPercentage(const float percentage){
-     float finalPulseWidth = (percentage * (m_maxPulsewidthInMs-m_minPulsewidthInMs)) + m_minPulsewidthInMs;
-     m_servoPwmOut.pulsewidth_ms(finalPulsewidth);
- }
+void TutorialServo::setPositionInPercentage(const float percentage) {
+  float finalPulseWidth = (percentage * (m_maxPulsewidthInMs - m_minPulsewidthInMs)) + m_minPulsewidthInMs;
+  m_servoPwmOut.pulsewidth_ms(finalPulsewidth);
+}
 
 // Get the servo range in degrees (ex. 90deg)
-float TutorialServo::getServoRangeInDegrees() const{
-    return m_servoRangeInDegrees;
+float TutorialServo::getServoRangeInDegrees() const {
+  return m_servoRangeInDegrees;
 }
-(fabs(degrees) / m_servoRangeInDegrees) - floor(fabs(degrees)/m_servoRangeInDegrees)
+
 // Get the min pulse width in ms (ex: 1ms)
-float TutorialServo::getMinPulseWidthInMs() const{
-    return m_minPulsewidthInMs;
+float TutorialServo::getMinPulseWidthInMs() const {
+  return m_minPulsewidthInMs;
 }
 
 // Get the max pulse width in ms (ex: 2ms)
-float TutorialServo::getMaxPulseWidthInMs() const{
-    return m_maxPulsewidthInMs;
+float TutorialServo::getMaxPulseWidthInMs() const {
+  return m_maxPulsewidthInMs;
 }
-
--- apps/tutorial-servo-can-control/src/main.cpp
+++ apps/tutorial-servo-can-control/src/main.cpp
@@ -1,39 +1,38 @@
-#include "TutorialServo.h"
-#include "mbed.h"
-#include "CANMsg.h"
 #include <cstdlib>
 
+#include "CANMsg.h"
+#include "TutorialServo.h"
+#include "mbed.h"
 
 TutorialServo tutorialServo(PA_1);
 CANBus can1(CAN1_RX, CAN1_TX);
 std::Thread rxCANProcessorThread(osPriorityAboveNormal);
 
-//Servo position update function for thread
+// Servo position update function for thread
 void updateServoPositon();
 
-
 int main() {
-    printf("Tutorial Servo App Start\n");
-    printf("=======================\r\n");
+  printf("Tutorial Servo App Start\n");
+  printf("=======================\r\n");
 
-    // Initialize CAN thread
-    rxCANProcessorThread.start(updateServoPosition);
+  // Initialize CAN thread
+  rxCANProcessorThread.start(updateServoPosition);
 
-    // Pretty sure this is required or the program will end and stop the CAN thread
-    while (true) {} 
+  // Pretty sure this is required or the program will end and stop the CAN thread
+  while (true) {
+  }
 }
-
 
 // Since the arm app used threads I attempted to use it too,
 // but for this application probably would just put it in the main loop
 void updateServoPosition() {
-    CANMsg rxMsg;
-    float rxPercentage {0};
-    while (true) {
-        if (can1.read(rxMsg)) {
-            rxMsg >> rxPercentage;
-            tutorialServo.setPositionInPercentage(rxPercentage);
-        }
-        ThisThread::sleep_for(2ms);
+  CANMsg rxMsg;
+  float rxPercentage{0};
+  while (true) {
+    if (can1.read(rxMsg)) {
+      rxMsg >> rxPercentage;
+      tutorialServo.setPositionInPercentage(rxPercentage);
     }
+    ThisThread::sleep_for(2ms);
+  }
 }
