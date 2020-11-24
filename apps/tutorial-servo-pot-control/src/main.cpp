a3c2a0a1435a8d55d0dd5643d9bc95bc374f50d7
kiransuren <kirank.suren@gmail.com>
2020-11-24 13:25:33 -0500

    used clang-format 11 on files

--- apps/tutorial-servo-pot-control/src/main.cpp
+++ apps/tutorial-servo-pot-control/src/main.cpp
@@ -4,20 +4,16 @@
  *
  */
 
-
 #include "mbed.h"
 
 AnalogIn potVoltageIn(PA_0);
 PwmOut servoPwmOut(PA_1);
 
+int main() {
+  servoPwmOut.period_ms(20);
 
-int main(){
-
-	servoPwmOut.period_ms(20);
-
-	while(1){
-		float potVoltage = potVoltageIn.read();
-		servoPwmOut.pulsewidth_ms(1+potVoltage/3.3);
-	}
+  while (1) {
+    float potVoltage = potVoltageIn.read();
+    servoPwmOut.pulsewidth_ms(1 + potVoltage / 3.3);
+  }
 }
-
