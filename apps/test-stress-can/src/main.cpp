12ebb72139e69ee023e892562e8c4caccee0ca8c
upadhyaydhruv <dmupadhyay12@gmail.com>
2020-12-21 21:46:24 -0500

    added basic boilerplate code for CAN transmission

--- apps/test-stress-can/src/main.cpp
+++ apps/test-stress-can/src/main.cpp
@@ -1,6 +1,28 @@
+#include "mbed.h"
+#include "CANBus.h"
+
+CANBus can(CAN_RX, CAN_TX, HWBRIDGE::ROVERCONFIG::ROVER_CANBUS_FREQUENCY);
+
+DigitalOut led(LED1);
+InterruptIn btn(BUTTON_1);
+
+constexpr uint16_t sent = 1000;
+uint16_t received = 0;
+ 
+// create an event queue
+EventQueue queue;
+ 
+void receiveMessage() {
+  received++;
+}
+ 
 int main() {
-    printf("Hello World\r\n");
-    while (true){
-        
-    }
+  // create a thread that'll run the event queue's dispatch function
+  Thread eventThread;
+  eventThread.start(callback(&queue, &EventQueue::dispatch_forever));
+ 
+  // wrap calls in queue.event to automatically defer to the queue's thread
+  btn.fall(queue.event(&receiveMessage));
+ 
+  for(;;);
 }
