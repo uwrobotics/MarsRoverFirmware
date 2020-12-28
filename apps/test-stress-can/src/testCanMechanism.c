49536bafb3452063f8a5e9fa290a97cddd48b209
upadhyaydhruv <dmupadhyay12@gmail.com>
2020-12-28 12:05:25 -0500

    Fixed more clang formatting issues

--- apps/test-stress-can/src/testCanMechanism.c
+++ apps/test-stress-can/src/testCanMechanism.c
@@ -5,17 +5,14 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
-
 #include <net/if.h>
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <sys/ioctl.h>
-
 #include <linux/can.h>
 #include <linux/can/raw.h>
 
-int main(void)
-{
+int main(void) {
 	int s;
 	int nbytes;
 	struct sockaddr_can addr;
