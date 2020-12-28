e47d86db8d3c3d06b8715ede461e10ed30c4e690
upadhyaydhruv <dmupadhyay12@gmail.com>
2020-12-28 11:50:40 -0500

    removed unnecessary variables and cleaned up useless code, fixed clang formatting

--- apps/test-stress-can/src/main.cpp
+++ apps/test-stress-can/src/main.cpp
@@ -3,17 +3,15 @@
 #include "CANMsg.h"
 
 CANBus can(CAN_RX, CAN_TX, HWBRIDGE::ROVERCONFIG::ROVER_CANBUS_FREQUENCY);
-
-DigitalOut led(LED1);
-InterruptIn btn(BUTTON_1);
-
-constexpr uint16_t sent = 1000;
-uint16_t received = 0;
  
 // create an event queue
 EventQueue queue;
 
-void receiveMessageOutside(CANMsg &msg) {
+void CANReceiveEventHandler(CANMsg &msg) {
+
+  static uint16_t received = 0;
+
+
   // Print data received from the CAN msg 
   printf("  Data    =");
   for (int i = 0; i < msg.len; i++) printf(" 0x%.2X", msg.data[i]);
@@ -24,14 +22,14 @@ void receiveMessageOutside(CANMsg &msg) {
  
 void CANMsgIRQHandler() {
   CANMsg msg;
-  if (can.read(msg)){
-    queue.call(&receiveMessageOutside, msg);
-  }
-  else {
+  if (can.read(msg)) {
+    queue.call(&CANReceiveEventHandler, msg);
+
+  } else {
     MBED_ASSERT(false);
   }
 }
- 
+
 int main() {
   // create a thread that'll run the event queue's dispatch function
   Thread rxCANProcessorThread(osPriorityRealtime);
@@ -40,5 +38,5 @@ int main() {
   // wrap calls in queue.event to automatically defer to the queue's thread
   can.attach(&CANMsgIRQHandler, CANBus::RxIrq);
  
-  while(true);
+  while (true);
 }
