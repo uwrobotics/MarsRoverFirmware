9c4b15c44250efe8b2efe826428121568b7bf64a
yreda1999 <yreda@uwaterloo.ca>
2020-12-28 19:27:21 -0500

    implement feedback

--- apps/pdb/src/main.cpp
+++ apps/pdb/src/main.cpp
@@ -1,14 +1,9 @@
-#ifdef TODO
 #include <cmath>
 
+#include "CANBus.h"
 #include "CANMsg.h"
 #include "hw_bridge.h"
 
-UltrasonicSensor frontLeft(ULTRASONIC_RX, ULTRASONIC_TX, SOME_SLAVE_ADDRESS),
-    frontRight(ULTRASONIC_RX, ULTRASONIC_TX, SOME_SLAVE_ADDRESS),
-    backLeft(ULTRASONIC_RX, ULTRASONIC_TX, SOME_SLAVE_ADDRESS),
-    backRight(ULTRASONIC_RX, ULTRASONIC_TX, SOME_SLAVE_ADDRESS);
-LEDMatrix matrix(LED_MATRIX_R_CHANNEL, LED_MATRIX_G_CHANNEL, LED_MATRIX_B_CHANNEL);
 AnalogIn railBattery(RAIL_BATTERY_ANLG_IN), rail5V(RAIL_5V_ANLG_IN), rail17V(RAIL_17V_ANLG_IN),
     rail24V(RAIL_24V_ANLG_IN);  // add voltage range (as percentage) to hw bridge also allocate can id for reporting if
                                 // outside range
@@ -18,7 +13,8 @@ CANBus can1(CAN1_RX, CAN1_TX, HWBRIDGE::ROVERCONFIG::ROVER_CANBUS_FREQUENCY);
 void rxCANProcessor();
 void txCANProcessor();
 
-void setLEDMatrix();  // simple switch statement that calls a different function based on contents of CAN msg
+static mbed_error_status_t setLEDMatrix(
+    CANMsg &msg);  // simple switch statement that calls a different function based on contents of CAN msg
 
 const static CANMsg::CANMsgHandlerMap canHandlerMap = {
     {HWBRIDGE::CANID::NEOPIXEL_SET, &setLEDMatrix}};  // rename NEOPIXEL_SET to LEDMATRIX_SET
@@ -27,9 +23,7 @@ int main() {
   Thread rxCANProcessorThread(osPriorityAboveNormal);
   Thread txCANProcessorThread(osPriorityBelowNormal);
   while (true) {
-    if (std::abs((railBattery.read() - 3) * 100 / 3) < some percent defined in hw bridge) {
-      send can msg to Xavier
-    }
+    // if (std::abs((railBattery.read() - 3) * 100 / 3) < some percent defined in hw bridge) {send to xavier}
     // repeat for each digital in
   }
 }
@@ -51,16 +45,7 @@ void txCANProcessor() {
     ThisThread::sleep_for(2ms);  // TODO: define the sleep rate in the bridge
   }
 }
-#else
-#define BLINKING_RATE 500ms
-int main() {
-  // Initialise the digital pin LED1 as an output
-  DigitalOut led(LED1);
 
-  while (true) {
-    led = !led;
-    printf("Current LED State is %s\n", led ? "ON" : "OFF");
-    ThisThread::sleep_for(BLINKING_RATE);
-  }
+static mbed_error_status_t setLEDMatrix(CANMsg &msg) {
+  return MBED_ERROR_INVALID_ARGUMENT;
 }
-#endif
