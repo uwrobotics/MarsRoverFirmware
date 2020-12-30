a0b8ec752351a9675548105691799c488e9dd55e
Steven Liu <lxlsteven011212@gmail.com>
2020-12-30 18:22:38 -0500

    plswork

--- apps/tutorial-servo-can-control-steven/TutorialServo.h
+++ apps/tutorial-servo-can-control-steven/TutorialServo.h
@@ -1,14 +1,14 @@
 #pragma once
 
- #include "mbed.h"
+#include "mbed.h"
 
 class TutorialServo {
-public:
+ public:
   // Constructor: Takes a servo pin name (ex. PA_1), and optionally a servo range
   // that has a default value of 180.0 degrees, a minimum pulsewidth of 1ms, and a
   // maximum pulsewidth of 2ms.
 
-TutorialServo(PinName servoPin, float servoRangeInDegrees = 180.0, float minPulsewidthInMs = 1,
+  TutorialServo(PinName servoPin, float servoRangeInDegrees = 180.0, float minPulsewidthInMs = 1,
                 float maxPulsewidthInMs = 2);
   // Set servo position (ex. 45 deg)
   void setPositionInDegrees(const float degrees);
@@ -19,9 +19,9 @@ TutorialServo(PinName servoPin, float servoRangeInDegrees = 180.0, float minPuls
   // Get the max pulse width in ms (ex: 2ms)
   float getMaxPulseWidthInMs() const;
 
-private:
+ private:
   PwmOut m_servoPwmOut;
   const float m_servoRangeInDegrees;
   const float m_minPulsewidthInMs;
   const float m_maxPulsewidthInMs;
- };
+};
