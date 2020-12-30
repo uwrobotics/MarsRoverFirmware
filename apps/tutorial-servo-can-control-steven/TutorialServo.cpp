c16fb404ddce5928c20b9b347a120c79e22c6e1d
Steven Liu <lxlsteven011212@gmail.com>
2020-12-30 18:15:22 -0500

    final_reversion

--- apps/tutorial-servo-can-control-steven/TutorialServo.cpp
+++ apps/tutorial-servo-can-control-steven/TutorialServo.cpp
@@ -1,30 +1,32 @@
 #include "TutorialServo.h"
+
 #include <cstdlib>
 
-TutorialServo::TutorialServo(PinName servoPin, float servoRangeInDegrees = 180.0, float minPulsewidthInMs = 1, float maxPulsewidthInMs = 2){
-    m_servoRangeInDegrees = servoRangeInDegrees;
-    m_minPulsewidthInMs = minPulsewidthInMs;
-    m_maxPulsewidthInMs = maxPulsewidthInMs;
-    m_servoPwmOut (servoPin);
-    m_servoPwmOut.period(0.020);
-}
+ TutorialServo::TutorialServo(PinName servoPin, float servoRangeInDegrees = 180.0, float minPulsewidthInMs = 1,
+                             float maxPulsewidthInMs = 2) {
+  m_servoRangeInDegrees = servoRangeInDegrees;
+  m_minPulsewidthInMs   = minPulsewidthInMs;
+  m_maxPulsewidthInMs   = maxPulsewidthInMs;
+  m_servoPwmOut(servoPin);
+  m_servoPwmOut.period(0.020);
+ }
 
-float TutorialServo::getServoRangeInDegrees( ) const{
-    return m_servoRangeInDegrees;
-}
+float TutorialServo::getServoRangeInDegrees() const {
+  return m_servoRangeInDegrees;
+ }
 
-float TutorialServo::getMinPulseWidthInMs( ) const{
-    return m_minPulsewidthInMs;
-}
+float TutorialServo::getMinPulseWidthInMs() const {
+  return m_minPulsewidthInMs;
+ }
 
-float TutorialServo::getMaxPulseWidthInMs( ) const{
-    return m_maxPulsewidthInMs;
-}
+float TutorialServo::getMaxPulseWidthInMs() const {
+  return m_maxPulsewidthInMs;
+ }
 
-void TutorialServo::setPositionInDegrees(const float degrees){
-    if(degrees >= 0 && degrees <= m_servoRangeInDegrees){
-        m_servoPwmOut.pulsewidth(m_minPulsewidthInMs*(1+degrees/m_servoRangeInDegrees));
-    }
-}
+void TutorialServo::setPositionInDegrees(const float degrees) {
+  if (degrees >= 0 && degrees <= m_servoRangeInDegrees) {
+    m_servoPwmOut.pulsewidth(m_minPulsewidthInMs * (1 + degrees / m_servoRangeInDegrees));
+  }
+ }
 
 
--- apps/tutorial-servo-can-control-steven/main.cpp
+++ apps/tutorial-servo-can-control-steven/main.cpp
@@ -3,25 +3,21 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-#include "mbed.h"
 #include "CANMsg.h"
 #include "TutorialServo.h"
+#include "mbed.h"
 
-//Declare global variables
+// Declare global variables
 CAN can(CAN1_RX, CAN1_TX);
 CANMsg msg;
 TutorialServo servo(PA_1);
 unit8_t convertedMessage = 0;
 
 // main() runs in its own thread in the OS
-int main() 
-{
-
-  while (true) 
-  {
-    if(can.read(msg))
-    {
-      msg >> convertedMessage; 
+int main() {
+  while (true) {
+    if (can.read(msg)) {
+      msg >> convertedMessage;
       servo.setPositionInDegrees(convertedMessage);
     }
   }
