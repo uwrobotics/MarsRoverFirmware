572e078838472e4e6202e3555e472b3f32f7da95
adamemily <Emily.Canada2@gmail.com>
2020-12-31 12:55:27 -0500

    Emily's FW Training, commit #5

--- apps/tutorial-servo-can-control/src/main.cpp
+++ apps/tutorial-servo-can-control/src/main.cpp
@@ -3,10 +3,9 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-#include "TutorialServo.h"
-
-#include "CANMsg.h"
 #include "mbed.h"  // header file for microcontroller library
+#include "CANMsg.h"
+#include "TutorialServo.h"
 
 // declare global objects
 CAN can_rx(D4, D10);
@@ -19,8 +18,7 @@ int main() {
   {
     if (can_rx.read(msg)) {
       uint8_t extractedMsg = 0;
-      // use >>operator to insert CAN packet's data to extractedMsg, which will convert to an integer 
-      // because of the unit8_t type
+      // insert CAN packet's data into extractedMsg with >>operator overload, which will convert to an integer bc of uint8_t
       msg >> extractedMsg;
       servo.setPositionInDegrees((extractedMsg / 100) * servo.getServoRangeInDegrees());
     }
--- apps/tutorial-servo-pot-control/src/main.cpp
+++ apps/tutorial-servo-pot-control/src/main.cpp
@@ -11,14 +11,14 @@ AnalogIn potVoltageIn(
 PwmOut servoPwmOut(
     PA_1);  // initializing pin 1 to output the PWM signal generated based on the potentiometer's read position
  
+// rest of code
 int main() {
   servoPwmOut.period(0.020);  // set PWM period to 20ms (50 Hz). Pulse times vary within the 20ms period
- 
   while (true)  // go forever
   {
-// initialize the voltage to a variable
+    // initialize the voltage to a variable
     float potVoltage = potVoltageIn.read();
-// voltage ranges from 0.0V to 3.3V. Map to duty cycle range of min 1ms and max 2ms & output
+    // voltage ranges from 0.0V to 3.3V. Map to duty cycle range of min 1ms and max 2ms & output
     servoPwmOut.pulsewidth(0.001 + 0.001 * potVoltage / 3.3);  
   }
 }
