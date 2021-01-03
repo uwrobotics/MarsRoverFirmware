94c4b05bec5bfac48a2b99167d8b0cbb3de9c8ac
kiransuren <kirank.suren@gmail.com>
2021-01-03 03:06:10 -0500

    implement spi.transfer for nonblocking spi reads

--- apps/test-netzer-encoder/src/main.cpp
+++ apps/test-netzer-encoder/src/main.cpp
@@ -2,46 +2,26 @@
 
 SPI spi(SPI_MOSI, SPI_MISO, SPI_SCK);  // mosi, miso, sclk (PA_7, PA_6, PA_5)
 
+const int WORDS = 2;				//Since we want 14 bits, left space for 2, 8 bit words
+const uint8_t tx_buffer[WORDS] = {0};		//Initialize dummy tx
+uint8_t rx_buffer[WORDS];			//Initialize rx buffer to receive data (16 bits in total)
+
+
+//Callback function, still need to figure out how to fully implement it
+void spi_read(int events){
+	printf("First buffer: %u  Second buffer: %u ", rx_buffer[0], rx_buffer[0]);
+}
+const event_callback_t spi_read_callback = spi_read;
+
 int main() {
+	
+	spi.format(16,3);		//I think this may have to be 8 bit?
+	spi.frequency(500000);
+		
+	ThisThread::sleep_for(3ms);
 
-  uint16_t upper, lower;
-  uint32_t msg;
-
-  uint16_t ack, start, zero, error, warn, crc;
-  uint32_t pos;
-
-  const uint8_t crcPolynomial = 0x03;
-
-  ThisThread::sleep_for(3ms);
-
-  // Setup the spi for 14 bit data, high steady state clock,
-  // second edge capture, with a 500 kHz clock rate
-  spi.format(16, 3);
-  spi.frequency(500000);
-
-  while (true) {
-    // Read
-    upper = spi.write(0x8000);
-    lower = spi.write(0x0000);
-
-    msg   = (((upper << 16) | lower) >> 2) & 0x0FFFFFFF;
-
-    ack   = (msg >> 27) & 1;
-    start = (msg >> 26) & 1;
-    zero  = (msg >> 25) & 1;
-
-    pos  = (msg >> 8) & 0x1FFFF;
-
-    error = (msg >> 7) & 1; // 1 = good
-    warn  = (msg >> 6) & 1; // 1 = good
-
-    crc   = msg & 0x3F;
-
-    uint32_t rem = (msg & 0x1FFFFFF) % crcPolynomial;
-
-    printf("Upper: 0x%04X \t Lower: 0x%04X \n", upper, lower);
-    printf("Ack: %d \t Start: %d \t Zero: %d \t Pos: %d \t Error: %d \t Warn: %d \t CRC: %d \t Rem: %d \n",
-           ack, start, zero, (int) pos, error, warn, crc, (int) rem);
-    printf("Pos: 0x%08X \t CRC: 0x%04X \t Msg: 0x%08X \n\n", (unsigned int) pos, crc, (unsigned int) msg);
-  }
+	while (true) {
+		
+		spi.transfer(tx_buffer, WORDS, rx_buffer, WORDS, spi_read_callback, NULL);
+	}
 }
