b870da2d7a47bd36f3809b9f1e1a7718ede7d428
upadhyaydhruv <dmupadhyay12@gmail.com>
2021-01-09 20:20:24 -0500

    Added functionality to test both methods successively, within 30 second limit

--- apps/test-stress-can/src/main.cpp
+++ apps/test-stress-can/src/main.cpp
@@ -7,15 +7,20 @@ CANBus can(CAN_RX, CAN_TX, HWBRIDGE::ROVERCONFIG::ROVER_CANBUS_FREQUENCY);
 // create an event queue
 EventQueue queue;
 
-void CANReceiveEventHandler(CANMsg &msg) {
-  static uint16_t received = 0;
+// This variable isn't made locally because the implementInterruptMethod function needs access to it to print the number
+// of messages received
+uint16_t ISRReceived = 0;
 
+// create a Timer to limit the length that the process can take
+Timer timer;
+
+void CANReceiveEventHandler(CANMsg &msg) {
   // Print data received from the CAN msg
   printf("  Data    =");
   for (int i = 0; i < msg.len; i++) printf(" 0x%.2X", msg.data[i]);
   printf("\r\n");
 
-  received++;
+  ISRReceived++;
 }
 
 void CANMsgIRQHandler() {
@@ -30,40 +35,46 @@ void CANMsgIRQHandler() {
 }
 
 int main() {
-  // create a thread that'll run the event queue's dispatch function
-  Thread rxCANProcessorThread(osPriorityRealtime);
-  rxCANProcessorThread.start(callback(&queue, &EventQueue::dispatch_forever));
 
-  // wrap calls in queue.event to automatically defer to the queue's thread
-  can.attach(&CANMsgIRQHandler, CANBus::RxIrq);
-
-  while (true)
-    ;
+  implementTraditionalMethod();
+  implementInterruptMethod();
 }
 
 void implementInterruptMethod() {
+  timer.reset();
+  timer.start();
   Thread rxCANProcessorThread(osPriorityRealtime);
   rxCANProcessorThread.start(callback(&queue, &EventQueue::dispatch_forever));
 
   // wrap calls in queue.event to automatically defer to the queue's thread
   can.attach(&CANMsgIRQHandler, CANBus::RxIrq);
 
-  while (true)
-    ;
+  while (true) {
+    if (timer.elapsed_time().count() >= 30000) break;
+  }
+
+  printf("Total Messages Received: " + ISRReceived);
+  printf("\r\n");
 }
 
 void implementTraditionalMethod() {
-  if (can.read(msg)) {
-    static uint16_t received = 0;
+  timer.reset();
+  timer.start();
+  static uint16_t received = 0;
+  CANMsg msg;
 
-    // Print data received from the CAN msg
-    printf("  Data    =");
-    for (int i = 0; i < msg.len; i++) printf(" 0x%.2X", msg.data[i]);
-    printf("\r\n");
+  // CAN messages are read and printed for a maximum of 30 seconds
+  while (timer.elapsed_time().count() <= 30000) {
+    if (can.read(msg)) {
+      // Print data received from the CAN msg
+      printf("  Data    =");
+      for (int i = 0; i < msg.len; i++) printf(" 0x%.2X", msg.data[i]);
+      printf("\r\n");
 
-    received++;
+      received++;
+    }
   }
 
-  while (true)
-    ;
+  printf("Total Messages Received: " + received);
+  printf("\r\n");
 }
