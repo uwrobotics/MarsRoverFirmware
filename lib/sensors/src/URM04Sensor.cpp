99c8d4d8c3fb33325f30309d6780600195d6090c
niiquaye <omarmon@uwaterloo.ca>
2021-01-11 22:21:51 -0500

    Minor Changes to Constructor/ Serial Communication / Command Buffer - URM04 Driver

--- lib/sensors/src/URM04Sensor.cpp
+++ lib/sensors/src/URM04Sensor.cpp
@@ -2,10 +2,7 @@
 
 // instantiate pin connected to the URM04 URM04Sensor
 URM04Sensor::URM04Sensor::URM04Sensor(PinName trig_pin, PinName _RX, PinName _TX, uint8_t default_address)
-    : m_trigPin(trig_pin), startAddr(default_address), RX(_RX), TX(_TX), serial(TX, RX, BAUD_RATE) {
-  // instantiate command buffer with all zeros
-
-  memset(&cmdst[0], 0, sizeof(cmdst));
+    : m_trigPin(trig_pin), m_startAddr(default_address), m_serial(_TX, _RX, BAUD_RATE) {
 }
 
 bool URM04Sensor::URM04Sensor::trigger_sensor() {
@@ -17,36 +14,33 @@ bool URM04Sensor::URM04Sensor::trigger_sensor() {
   // check sum represents the final bit in command buffer - made by adding all previous bits in command buffer
   uint8_t checkSum;
   // buffer header
-  cmdst[0] = 0x55;
-  cmdst[1] = 0xAA;
+  m_cmdst[0] = 0x55;
+  m_cmdst[1] = 0xAA;
   // device address
-  cmdst[2] = startAddr;
+  m_cmdst[2] = m_startAddr;
   // length
-  cmdst[3] = 0x00;
+  m_cmdst[3] = 0x00;
   // the command itself
-  cmdst[4] = 0x01;
+  m_cmdst[4] = 0x01;
   // checksum bit
-  checkSum = cmdst[0] + cmdst[1] + cmdst[2] + cmdst[3] + cmdst[4];
+  checkSum = m_cmdst[0] + m_cmdst[1] + m_cmdst[2] + m_cmdst[3] + m_cmdst[4];
   // instantiate the last element in the command buffer with checksum value
-  cmdst[5] = checkSum;
+  m_cmdst[5] = checkSum;
 
   /******** SEND COMMANDS OVER SERIAL *********************/
-  int num_bytes;
   // returns the number of bytes if successful write
-  num_bytes = serial.write(&cmdst[0], 6);
+  int num_bytes = m_serial.write(&m_cmdst[0], 6);
 
   // else if numb_btyes < 0 serial failed or if number of bytes written != 6 (because 6 bytes sent over serial)
-  if (num_bytes < 0 || num_bytes != 6) {
+  if (num_bytes != 6) {
     // error occured in trigger step
     return false;
   } else {
     // no error occured in trigger step
     // flush the buffer from serial
-    serial.sync();
+    m_serial.sync();
     // wait for at least 30 ms after calling trigger function
     ThisThread::sleep_for(35ms);
-    // reset command buffer - fill whole array with zeros
-    memset(&cmdst[0], 0, sizeof(cmdst));
     return true;
   }
 }
@@ -54,7 +48,7 @@ bool URM04Sensor::URM04Sensor::trigger_sensor() {
 // pass by reference a variable to hold the distance value - will give MAX_FLOAT if out of range
 bool URM04Sensor::URM04Sensor::read_distance(float& distance) {
   /****************** TRIGGER SENSOR BEFORE READING DISTANCE ********************/
-  // if trigger fails set distance to -1 and retuen false
+  // if trigger fails set distance to -1 and return false
   if (trigger_sensor() == false) {
     distance = -1;
     return false;
@@ -64,76 +58,70 @@ bool URM04Sensor::URM04Sensor::read_distance(float& distance) {
   // check sum represents the final bit in command buffer - made by adding all previous bits in command buffer
   uint8_t checkSum;
   // buffer header
-  cmdst[0] = 0x55;
-  cmdst[1] = 0xAA;
+  m_cmdst[0] = 0x55;
+  m_cmdst[1] = 0xAA;
   // device address
-  cmdst[2] = startAddr;
+  m_cmdst[2] = m_startAddr;
   // command length
-  cmdst[3] = 0x00;
+  m_cmdst[3] = 0x00;
   // the command itself
-  cmdst[4] = 0x02;
+  m_cmdst[4] = 0x02;
   // checksum bit
-  checkSum = cmdst[0] + cmdst[1] + cmdst[2] + cmdst[3] + cmdst[4];
+  checkSum = m_cmdst[0] + m_cmdst[1] + m_cmdst[2] + m_cmdst[3] + m_cmdst[4];
   // instantiate the last element in the command buffer with checksum value
-  cmdst[5] = checkSum;
+  m_cmdst[5] = checkSum;
 
   /************** SEND COMMANDS OVER SERIAL***********/
-  int w_num_bytes;
   // returns the number of bytes if successful read
-  w_num_bytes = serial.write(&cmdst[0], 6);
+  int w_num_bytes = m_serial.write(&m_cmdst[0], 6);
 
   // else if numb_btyes < 0 serial failed or if number of bytes read != 6 (because 6 bytes sent over serial)
-  if (w_num_bytes < 0 || w_num_bytes != 6) {
+  if (w_num_bytes != 6) {
     // return false indicating read command has failed
     distance = -1;
     return false;
   }
 
   // flush buffer from serial
-  serial.sync();
+  m_serial.sync();
 
-  // reset command buffer - fill whole array with zeros
-  memset(&cmdst[0], 0, sizeof(cmdst));
   /******* READ RETURN VALUE FROM SERIAL**************/
 
   // turn on reading mode for RS485
   m_trigPin.write(LOW);
   ThisThread::sleep_for(1ms);
 
-  int r_num_bytes;
   // returns the number of bytes if successful read
-  r_num_bytes = serial.read(&cmdst[0], 8);
+  int r_num_bytes = m_serial.read(&m_cmdst[0], 8);
 
   // else if numb_btyes < 0 serial failed or if number of bytes read != 8 (because 8 bytes sent over serial)
-  if (r_num_bytes < 0 || r_num_bytes != 8) {
+  if (r_num_bytes != 8) {
     distance = -1;
     return false;
   }
   /******** PARSE THROUGH THE DATA READ FROM SERIAL*********/
   else {
-    // create the checksum
-    checkSum = cmdst[0] + cmdst[1] + cmdst[2] + cmdst[3] + cmdst[4] + cmdst[5] + cmdst[6];
 
     // check if the checksum is incorrect
-    if (checkSum != cmdst[7]) {
+    if (m_cmdst[7] != m_cmdst[0] + m_cmdst[1] + m_cmdst[2] + m_cmdst[3] + m_cmdst[4] + m_cmdst[5] + m_cmdst[6]) {
       // return false if checksum failed
       distance = -1;
       return false;
     } else {
       // get distance from sensor
-      distance = (float)(cmdst[5] << 8) + (float)cmdst[6];
+      distance = (float)(m_cmdst[5] << 8) + (float)m_cmdst[6];
       // --------------------- or --------------------------
-      // distance = (float)(cmdst[5]*256) + (float)cmdst[6];
+      // distance = (float)(m_cmdst[5]*256) + (float)m_cmdst[6];
 
       // check if distance is out of range - if so return maximum float value
-      if (cmdst[5] == 0xFF && cmdst[6] == 0xFF) {
+      if (m_cmdst[5] == 0xFF && m_cmdst[6] == 0xFF) {
         distance = MAX_FLOAT;
       }
       return true;
     }
   }
   // flush serial
-  serial.sync();
+  m_serial.sync();
 }
 
 bool URM04Sensor::URM04Sensor::set_address(uint8_t _address) {
@@ -142,38 +130,33 @@ bool URM04Sensor::URM04Sensor::set_address(uint8_t _address) {
   ThisThread::sleep_for(1ms);
 
   /************ INSTANTIATE COMMANDS TO BE SENT OVER SERIAL************/
-  // reset the command array with all zeros
-  memset(&cmdst[0], 0, sizeof(cmdst));
   // check sum represents the final bit in command buffer - made by adding all previous bits in command buffer
   uint8_t checkSum;
   // buffer header
-  cmdst[0] = 0x55;
-  cmdst[1] = 0xAA;
+  m_cmdst[0] = 0x55;
+  m_cmdst[1] = 0xAA;
   // device address
-  cmdst[2] = 0xAB;
+  m_cmdst[2] = 0xAB;
   // command length
-  cmdst[3] = 0x01;
+  m_cmdst[3] = 0x01;
   // the command itself
-  cmdst[4] = 0x55;
+  m_cmdst[4] = 0x55;
   // set new Address
-  cmdst[5] = _address;
+  m_cmdst[5] = _address;
   // compute checksum bit
-  checkSum = cmdst[0] + cmdst[1] + cmdst[2] + cmdst[3] + cmdst[4] + cmdst[5];
+  checkSum = m_cmdst[0] + m_cmdst[1] + m_cmdst[2] + m_cmdst[3] + m_cmdst[4] + m_cmdst[5];
   // instantiate the last element in the command buffer with checksum value
-  cmdst[6] = checkSum;
+  m_cmdst[6] = checkSum;
 
   /************** SEND COMMANDS OVER SERIAL***********/
-  int w_num_bytes;
   // send command over serial - write
-  w_num_bytes = serial.write(&cmdst[0], 7);
+  int w_num_bytes = m_serial.write(&m_cmdst[0], 7);
   // check if command was successfully written to the serial
-  if (w_num_bytes < 0 || w_num_bytes != 7) {
+  if (w_num_bytes != 7) {
     return false;
   }
   // flush the serial
-  serial.sync();
-  // clean buffer
-  memset(&cmdst[0], 0, sizeof(cmdst));
+  m_serial.sync();
 
   /************* PARSE THROUGH DATA RECIEVED***********/
 
@@ -181,28 +164,24 @@ bool URM04Sensor::URM04Sensor::set_address(uint8_t _address) {
   m_trigPin.write(LOW);
   ThisThread::sleep_for(1ms);
 
-  int r_num_bytes;
   // read return value from serial
-  r_num_bytes = serial.read(&cmdst[0], 7);
+  int r_num_bytes = m_serial.read(&m_cmdst[0], 7);
   // check if read is successful
-  if (r_num_bytes < 0 || r_num_bytes != 7) {
+  if (r_num_bytes != 7) {
     return false;
   }
-  // check the checksum
-  checkSum = cmdst[0] + cmdst[1] + cmdst[2] + cmdst[3] + cmdst[4] + cmdst[5];
   // check if the checksum is incorrect
-  if (checkSum != cmdst[6]) {
+  if (m_cmdst[6] != m_cmdst[0] + m_cmdst[1] + m_cmdst[2] + m_cmdst[3] + m_cmdst[4] + m_cmdst[5]) {
     return false;
   } else {
     // check if the command went through successfully by checking if the
     // 6th element of the return buffer array == 0x01
-    if (cmdst[5] == 0x01) {
+    if (m_cmdst[5] == 0x01) {
       // new address is set
-      startAddr = _address;
+      m_startAddr = _address;
 
       // clean up
-      serial.sync();
-      memset(&cmdst[0], 0, sizeof(cmdst));
+      m_serial.sync();
 
       return true;
     }
