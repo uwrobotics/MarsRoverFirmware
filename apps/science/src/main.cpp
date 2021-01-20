4a5d02a314056f876f950b63e23e3a0698b2cb1c
Yehia-Fahmy <yehia.fahmy@uwaterloo.ca>
2021-01-19 23:30:57 -0500

    removed uneeded LED

--- apps/science/src/main.cpp
+++ apps/science/src/main.cpp
@@ -41,9 +41,6 @@ ActuatorController indexerActuator(ScienceConfig::indexerActuatorConfig, indexer
 // I2C
 MoistureSensor moistureSensor = MoistureSensor(TEMP_MOIST_I2C_SDA, TEMP_MOIST_I2C_SCL);
 
-// LED used for debugging
-DigitalOut led1(LED1);
-
 // Declaring CANBus object
 CANBus can(CAN1_RX, CAN1_TX, HWBRIDGE::ROVERCONFIG::ROVER_CANBUS_FREQUENCY);
 
