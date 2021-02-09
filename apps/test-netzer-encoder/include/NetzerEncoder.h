3bda06d0487bfdb0bec43b2f58f99da6765e6124
Kiran Surendran <kirank.suren@gmail.com>
2021-02-09 00:59:03 -0500

    implement code from cindy, pr245

--- apps/test-netzer-encoder/include/NetzerEncoder.h
+++ apps/test-netzer-encoder/include/NetzerEncoder.h
@@ -2,21 +2,32 @@
 
 #include "mbed.h"
 
+
+namespace NetzerEncoder {
+
+typedef void (*callback_ptr)(void);
+
 class NetzerEncoder {
 
 	public:
-		NetzerEncoder(int frequency);
-		static void spi_callback_debug(const int events);
-		void spi_read();
+		NetzerEncoder(PinName mosi, PinName miso, PinName sclk, 
+									callback_ptr callback, uint32_t frequency = DEFAULT_FREQUENCY_HZ);
+		bool spi_read();
+		uint16_t get_raw_data();
 	
-
 	private:
+		static constexpr uint32_t DEFAULT_FREQUENCY_HZ = 500000;  // frequency given by datasheet
 		static const int WORDS = 2;											// 2, 8 bit words
-		const uint8_t tx_buffer[WORDS] = {0};			// Initialize dummy tx
-		uint8_t rx_buffer[WORDS];									// Initialize rx buffer to receive data (16 bits in total)
-		int freq;																	// SPI read frequency
-		SPI spi{SPI_MOSI, SPI_MISO, SPI_SCK};			// most, miso, sclk (PA_7, PA_6, PA_5)
+		const uint8_t tx_buffer[WORDS] = {0};						// Initialize dummy tx
+		uint8_t rx_buffer[WORDS] = {0};									// Initialize rx buffer to receive data (16 bits in total)
+		uint16_t m_raw_data;														// between 14-19 bits
+		SPI spi;																				// most, miso, sclk (PA_7, PA_6, PA_5)
+  	callback_ptr m_callback;
+		uint32_t freq;																	// SPI read frequency
+		void spi_callback_debug(int events);
+		// User callback function to be invoked when an encoder read is complete (only for async reads)
 };
 
+}
 
 
--- apps/test-netzer-encoder/src/main.cpp
+++ apps/test-netzer-encoder/src/main.cpp
@@ -1,7 +1,47 @@
 #include "NetzerEncoder.h"
 #include "mbed.h"
 
+
+#define ENCODER_READ_FREQUENCY_HZ 1
+#define ENCODER_READ_PERIOD       (1000ms / (ENCODER_READ_FREQUENCY_HZ))
+
+void callback(void);
+void run_async();
+Timer timer;
+volatile bool encoder_position_updated = false;
+NetzerEncoder::NetzerEncoder encoder(SPI_MISO, SPI_MOSI, SPI_SCK, callback);
+
 int main(){
-	NetzerEncoder encoder(500000);
-	encoder.spi_read();
+	run_async();
+}
+
+
+void run_async() {
+  printf("\r\n--- Netzer Encoder Async Driver Test App ---\r\n");
+
+  while (true) {
+    timer.reset();
+    timer.start();
+
+    while (!encoder.spi_read()) {
+    }
+
+    while (!encoder_position_updated) {
+    }
+
+    timer.stop();
+
+    encoder_position_updated = false;
+
+    printf("Encoder reading raw: %d\r\n", encoder.get_raw_data());
+
+    printf("Read time: %llu us\r\n",
+           std::chrono::duration_cast<std::chrono::microseconds>(timer.elapsed_time()).count());
+
+    ThisThread::sleep_for(ENCODER_READ_PERIOD);
+  }
+}
+
+void callback(void) {
+  encoder_position_updated = true;
 }
