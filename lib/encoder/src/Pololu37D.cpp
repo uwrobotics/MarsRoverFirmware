e023e24d3964fcad0a88f2189bdc17d9c8528bfc
Yehia-Fahmy <yehia.fahmy@uwaterloo.ca>
2021-02-24 21:33:33 -0500

    implemented changes to the class functions

--- lib/encoder/src/Pololu37D.cpp
+++ lib/encoder/src/Pololu37D.cpp
@@ -10,19 +10,30 @@ Pololu37D::Pololu37D(const Config &config)
 
 bool Pololu37D::getAngleDeg(float &angle) {
   std::scoped_lock<Mutex> lock(m_mutex);
-  angle = (m_QEI.getPulses() * m_degreesPerCount * 2) -
-          m_zeroOffsetDeg;  // multiplying by 2 since our measurements were off by a factor of 2
+  angle = m_current_angle_deg;
   return true;
 }
 
 bool Pololu37D::getAngularVelocityDegPerSec(float &speed) {
   std::scoped_lock<Mutex> lock(m_mutex);
-  speed = m_QEI.getPulseVelocity_PulsesPerSec() * m_degreesPerCount;
+  speed = m_speed_deg_per_second;
   return true;
 }
 
 bool Pololu37D::reset() {
   std::scoped_lock<Mutex> lock(m_mutex);
   m_QEI.reset();
+  return true;
+}
+
+bool Pololu37D::read() {
+  m_previous_angle_deg = m_current_angle_deg;
+
+  m_current_angle_deg = (m_QEI.getPulses() * m_degreesPerCount * 2) -
+                        m_zeroOffsetDeg;  // multiplying by 2 since our measurements were off by a factor of 2
+
+  m_speed_deg_per_second =
+      (m_current_angle_deg - m_previous_angle_deg) * 2;  // multiply because we are measureing twice a second
+
   return true;
 }
