c9c6daf520b356873dbbc32a06d0d686fff7715c
Yehia-Fahmy <yehia.fahmy@uwaterloo.ca>
2021-02-27 21:05:52 -0500

    removed comments

--- apps/test-pololu370-encoder/src/main.cpp
+++ apps/test-pololu370-encoder/src/main.cpp
@@ -14,7 +14,6 @@ float theta_dot = 0;
 
 int main() {
   while (true) {
-    // printf("----------------------------\n");
     theta_timer.reset();
     theta_dot_timer.reset();
     for (int i = 0; i < 3; i++) {
@@ -25,12 +24,10 @@ int main() {
 }
 
 void read_all() {
-  // try reading the encoder
   // angle reading
   theta_timer.start();
   if (enc.getAngleDeg(theta)) {
     theta_timer.stop();
-    // printf("Angle reading took: %lluus\tAngle: %.3f\r\n", theta_timer.elapsed_time().count(), theta);
     Utility::Logger::printf("Angle reading took: %lluus\tAngle: %.3f\r\n", theta_timer.elapsed_time().count(), theta);
   } else {
     Utility::Logger::printf("an error occured :(");
@@ -39,8 +36,6 @@ void read_all() {
   theta_dot_timer.start();
   if (enc.getAngularVelocityDegPerSec(theta_dot)) {
     theta_dot_timer.stop();
-    // printf("Angular speed reading took: %lluus\tSpeed: %.3f\r\n", theta_dot_timer.elapsed_time().count(),
-    // theta_dot);
     Utility::Logger::printf("Angular speed reading took: %lluus\tSpeed: %.3f\r\n",
                             theta_dot_timer.elapsed_time().count(), theta_dot);
   } else {
