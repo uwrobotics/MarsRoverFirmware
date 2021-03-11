8cd322ce15fb866022dd0e0c607e3b26d2aaf7c7
Farris Matar <37193300+far-mat42@users.noreply.github.com>
2021-03-10 20:04:09 -0500

    Update main.cpp

--- apps/tutorial-servo-can-control/src/main.cpp
+++ apps/tutorial-servo-can-control/src/main.cpp
@@ -1,3 +1,28 @@
 #include "mbed.h"
 #include "TutorialServo.h"
+#include "CANBus.h"
+#include "CANMsg.h"
 
+float servoAngle;
+
+// CANBus
+CANBus can1(CAN1_RX, CAN1_TX, HWBRIDGE::ROVERCONFIG::ROVER_CANBUS_FREQUENCY);
+
+void rxCANProcessor() 
+{
+  CANMsg rxMsg;
+  
+  while (true)
+  {
+    if (can1.read(rxMsg))
+    {
+      canHandlerMap.at(rxMsg.getID())(rxMsg);
+    }
+    ThisThread::sleep_for(2ms);
+  }
+}
+
+int main() 
+{
+  
+}
