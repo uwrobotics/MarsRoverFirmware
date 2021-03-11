f634a9758c61149a6075baaf1b8a7dfaf2f49406
Farris Matar <37193300+far-mat42@users.noreply.github.com>
2021-03-11 17:10:12 -0500

    Update main.cpp

--- apps/tutorial-servo-can-control/src/main.cpp
+++ apps/tutorial-servo-can-control/src/main.cpp
@@ -4,6 +4,9 @@
 #include "CANMsg.h"
 
 float servoAngle;
+float percentage;
+
+TutorialServo testServo(PA_1, 180.0, 1.0, 2.0);
 
 // CANBus
 CANBus can1(CAN1_RX, CAN1_TX, HWBRIDGE::ROVERCONFIG::ROVER_CANBUS_FREQUENCY);
@@ -26,6 +29,19 @@ Thread rxCANProcessorThread(osPriorityAboveNormal);
 
 int main() 
 {
+  can1.setFilter(HWBRIDGE::CANFILTER::ROVER_CANID_FIRST_ARM_RX, CANStandard,
+                 HWBRIDGE::ROVERCONFIG::ROVER_CANID_FILTER_MASK);
   
   rxCANProcessorThread.start(rxCANProcessor);
+  while (1)
+  {
+    if (can1.read(rxMsg))
+    {
+      rxMsg >> percentage
+      servoAngle = testServo.getServoRangeInDegrees()*percentage;
+      testServo.setPositionInDegrees(servoAngle);
+    }
+    
+    ThisThread::sleep_for(1ms);
+  }
 }
