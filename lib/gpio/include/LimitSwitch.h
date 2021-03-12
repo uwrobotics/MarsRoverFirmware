5b3d5d323f3ab77e73490ac1f51c741540b22dbf
Mogbekeloluwa Adesiyun <msadesiy@uwaterloo.ca>
2021-03-11 19:47:54 -0500

    Added LimitSwitch header and cpp files

---
+++ lib/gpio/include/LimitSwitch.h
@@ -0,0 +1,11 @@
+#pragma once
+
+/*
+
+This is the note added to the "Create Limit Switch Class" issue on Github:
+
+Currently we've just been using DigitalIn for limit switches.
+The goal of this task is to create a limit switch driver to abstract away whether
+    the switch is active high or active low.
+
+*/
---
+++ lib/gpio/src/LimitSwitch.cpp
@@ -0,0 +1,4 @@
+#include "LimitSwitch.h"
+
+// I copied this piece of code from the PwmIn.cpp file. Find out what the piece of code means
+using namespace GPIO;
