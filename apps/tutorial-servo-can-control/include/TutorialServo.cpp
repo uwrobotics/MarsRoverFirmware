98577cdc65b7b7d32c15c6fd8e22a9d9eddc1ba2
Farris Matar <37193300+far-mat42@users.noreply.github.com>
2021-03-17 13:06:04 -0400

    Update TutorialServo.cpp

--- apps/tutorial-servo-can-control/include/TutorialServo.cpp
+++ apps/tutorial-servo-can-control/include/TutorialServo.cpp
@@ -4,13 +4,13 @@
 TutorialServo::TutorialServo(PinName servoPin, float servoRangeInDegrees, float minPulsewidthInMs, float maxPulsewidthInMs)
   : m_servoPin(servoPin), m_servoRangeInDegrees(servoRangeInDegrees), m_minPulsewidthInMs(minPulsewidthInMs), m_maxPulsewidthInMs(maxPulsewidthInMs)
 {
-  PwmOut servoPwmOut(servoPin);
+  PwmOut servoPwmOut(m_servoPin);
   servoPwmOut.period_ms(20);
 }
 
 void TutorialServo::setPositionInDegrees(const float degrees)
 {
-  if (degrees <= m_servoRangeInDegrees && degrees >= 0)
+  if (degrees <= m_servoRangeInDegrees && degrees >= 0 && m_minPulsewidthInMs + degrees/m_servoRangeInDegrees <= m_maxPulsewidthInMs)
   {
     servoPwmOut.pulsewidth_ms(m_minPulsewidthInMs + degrees/m_servoRangeInDegrees)
   }
