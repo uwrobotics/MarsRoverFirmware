431ea4c8ac694ed3246a84bd9f9216595aecf8b9
Farris Matar <37193300+far-mat42@users.noreply.github.com>
2021-03-17 12:55:26 -0400

    Update main.cpp

--- apps/tutorial-servo-can-control/src/main.cpp
+++ apps/tutorial-servo-can-control/src/main.cpp
@@ -11,12 +11,8 @@ TutorialServo testServo(PA_1, 180.0, 1.0, 2.0);
 // CANBus
 CANBus can1(CAN1_RX, CAN1_TX, HWBRIDGE::ROVERCONFIG::ROVER_CANBUS_FREQUENCY);
 
-Thread rxCANProcessorThread(osPriorityAboveNormal);
-
 int main() 
 {
-  can1.setFilter(HWBRIDGE::CANFILTER::ROVER_CANID_FIRST_ARM_RX, CANStandard,
-                 HWBRIDGE::ROVERCONFIG::ROVER_CANID_FILTER_MASK);
   
   CANMsg rxMsg;
   
@@ -24,7 +20,6 @@ int main()
   {
     if (can1.read(rxMsg))
     {
-      canHandlerMap.at(rxMsg.getID())(rxMsg);
       can1.getPayload(percentage);
       servoAngle = testServo.getServoRangeInDegrees()*percentage;
       testServo.setPositionInDegrees(servoAngle);
