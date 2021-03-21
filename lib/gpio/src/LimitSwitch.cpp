f242414488b6b2066120cfa3b32467ed40bc000a
Mogbekeloluwa Adesiyun <msadesiy@uwaterloo.ca>
2021-03-20 22:40:42 -0400

    Removed some of the comments

--- lib/gpio/src/LimitSwitch.cpp
+++ lib/gpio/src/LimitSwitch.cpp
@@ -5,11 +5,9 @@ using namespace GPIO;
 
 LimitSwitch::LimitSwitch(DigitalIn limitPin, bool ActiveHigh) : m_limitPin(limitPin), m_ActiveHigh(ActiveHigh) {}
 
-// Instead of having the setActiveHighorLow() function, you should have a function like "isPressed()
-// or any other descriptive name that just tells you if the LImit Switch ahs been pressed"
 // You are not writing to the Limit Switch so you only need to use the DigitalIn read function
+// Tells you if the Limit Switch has been pressed or not
 bool LimitSwitch::isPressed() {
-  // Tells you if the Limit Switch has been pressed or not
   if (!m_ActiveHigh) {
     return !limitPin.read();
   }
