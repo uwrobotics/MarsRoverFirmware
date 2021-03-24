35405b47d46a9d04b7315de8c0ad388c940f5392
far-mat42 <fmatar@LAPTOP-LUK3VBI3.localdomain>
2021-03-24 19:50:24 -0400

    final commit

--- apps/tutorial-servo-pot-control/src/main.cpp
+++ apps/tutorial-servo-pot-control/src/main.cpp
@@ -2,12 +2,10 @@
 AnalogIn potVoltageIn(PA_0);
 PwmOut servoPwmOut(PA_1);
 
-int main()
-{
+int main() {
   servoPwmOut.period_ms(20);
-  while (1)
-  {
+  while (1) {
     float potVoltage = potVoltageIn.read();
-    servoPwmOut.pulsewidth_ms(1 + potVoltage/3.3);
+    servoPwmOut.pulsewidth_ms(1 + potVoltage / 3.3);
   }
 }
