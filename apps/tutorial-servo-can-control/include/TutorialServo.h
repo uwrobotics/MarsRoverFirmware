5f131a1a99c2a03ed17b13c5d323b35574b135e1
far-mat42 <fmatar@LAPTOP-LUK3VBI3.localdomain>
2021-03-24 19:03:52 -0400

    Clang formatting fix

--- apps/tutorial-servo-can-control/include/TutorialServo.h
+++ apps/tutorial-servo-can-control/include/TutorialServo.h
@@ -3,25 +3,25 @@
 #include "mbed.h"
 
 class TutorialServo {
-  public:
-    TutorialServo(PinName servoPin, float servoRangeInDegrees = 180.0, float minPulsewidthInMs = 1, float maxPulsewidthInMs = 2);
-    
-    // Set servo position to a certain angle
-    void setPositionInDegrees(const float degrees);
-    
-    // Get servo range in degrees
-    float getServoRangeInDegrees() const;
-    
-    // Get the min pulse width in ms
-    float getMinPulsewidthInMs() const;
-    
-    // Get the max pulse width in ms
-    float getMaxPulsewidthInMs() const;
-  
-  private:
-    PwmOut m_servoPwmOut;
-    const float m_servoRangeInDegrees;
-    const float m_minPulsewidthInMs;
-    const float m_maxPulsewidthInMs;
-    
+ public:
+  TutorialServo(PinName servoPin, float servoRangeInDegrees = 180.0, float minPulsewidthInMs = 1,
+                float maxPulsewidthInMs = 2);
+
+  // Set servo position to a certain angle
+  void setPositionInDegrees(const float degrees);
+
+  // Get servo range in degrees
+  float getServoRangeInDegrees() const;
+
+  // Get the min pulse width in ms
+  float getMinPulsewidthInMs() const;
+
+  // Get the max pulse width in ms
+  float getMaxPulsewidthInMs() const;
+
+ private:
+  PwmOut m_servoPwmOut;
+  const float m_servoRangeInDegrees;
+  const float m_minPulsewidthInMs;
+  const float m_maxPulsewidthInMs;
 };
--- apps/tutorial-servo-can-control/src/main.cpp
+++ apps/tutorial-servo-can-control/src/main.cpp
@@ -1,7 +1,7 @@
-#include "mbed.h"
-#include "TutorialServo.h"
 #include "CANBus.h"
 #include "CANMsg.h"
+#include "TutorialServo.h"
+#include "mbed.h"
 
 float servoAngle;
 float percentage;
@@ -11,20 +11,16 @@ TutorialServo testServo(PA_1, 180.0, 1.0, 2.0);
 // CANBus
 CANBus can1(CAN1_RX, CAN1_TX, HWBRIDGE::ROVERCONFIG::ROVER_CANBUS_FREQUENCY);
 
-int main() 
-{
-  
+int main() {
   CANMsg rxMsg;
-  
-  while (1)
-  {
-    if (can1.read(rxMsg))
-    {
+
+  while (1) {
+    if (can1.read(rxMsg)) {
       rxMsg.getPayload(percentage);
-      servoAngle = testServo.getServoRangeInDegrees()*percentage;
+      servoAngle = testServo.getServoRangeInDegrees() * percentage;
       testServo.setPositionInDegrees(servoAngle);
     }
-    
+
     ThisThread::sleep_for(2ms);
   }
 }
