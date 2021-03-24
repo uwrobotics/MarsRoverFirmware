1457a696f6112c805936a744c158e13181712ccb
far-mat42 <fmatar@LAPTOP-LUK3VBI3.localdomain>
2021-03-24 18:43:04 -0400

    Clang formatting fix

--- apps/tutorial-servo-can-control/src/TutorialServo.cpp
+++ apps/tutorial-servo-can-control/src/TutorialServo.cpp
@@ -1,31 +1,31 @@
 #include "TutorialServo.h"
+
 #include "mbed.h"
 
-TutorialServo::TutorialServo(PinName servoPin, float servoRangeInDegrees, float minPulsewidthInMs, float maxPulsewidthInMs)
-  : m_servoPwmOut(servoPin), m_servoRangeInDegrees(servoRangeInDegrees), m_minPulsewidthInMs(minPulsewidthInMs), m_maxPulsewidthInMs(maxPulsewidthInMs)
-{
+TutorialServo::TutorialServo(PinName servoPin, float servoRangeInDegrees, float minPulsewidthInMs,
+                             float maxPulsewidthInMs)
+    : m_servoPwmOut(servoPin),
+      m_servoRangeInDegrees(servoRangeInDegrees),
+      m_minPulsewidthInMs(minPulsewidthInMs),
+      m_maxPulsewidthInMs(maxPulsewidthInMs) {
   m_servoPwmOut.period_ms(20);
 }
 
-void TutorialServo::setPositionInDegrees(const float degrees)
-{
-  if (degrees <= m_servoRangeInDegrees && degrees >= 0)
-  {
-    m_servoPwmOut.pulsewidth_ms(m_minPulsewidthInMs + (degrees/m_servoRangeInDegrees) * (m_maxPulsewidthInMs - m_minPulsewidthInMs));
+void TutorialServo::setPositionInDegrees(const float degrees) {
+  if (degrees <= m_servoRangeInDegrees && degrees >= 0) {
+    m_servoPwmOut.pulsewidth_ms(m_minPulsewidthInMs +
+                                (degrees / m_servoRangeInDegrees) * (m_maxPulsewidthInMs - m_minPulsewidthInMs));
   }
 }
 
-float TutorialServo::getServoRangeInDegrees() const
-{
+float TutorialServo::getServoRangeInDegrees() const {
   return m_servoRangeInDegrees;
 }
 
-float TutorialServo::getMinPulsewidthInMs() const
-{
+float TutorialServo::getMinPulsewidthInMs() const {
   return m_minPulsewidthInMs;
 }
 
-float TutorialServo::getMaxPulsewidthInMs() const
-{
+float TutorialServo::getMaxPulsewidthInMs() const {
   return m_maxPulsewidthInMs;
 }
