3a9d81e6b3c313e81b0cc8440eb7f489d6458e66
Mogbekeloluwa Adesiyun <msadesiy@uwaterloo.ca>
2021-03-25 21:09:16 -0400

    Added check for NC pin

--- lib/gpio/src/LimitSwitch.cpp
+++ lib/gpio/src/LimitSwitch.cpp
@@ -1,5 +1,7 @@
 #include "LimitSwitch.h"
 
+// 25th March 2021 - Feedback from Younes for limit switch task:
+//      make sure you write a test app
 // I copied this piece of code from the PwmIn.cpp file. Find out what the piece of code means
 using namespace GPIO;
 
@@ -7,9 +9,14 @@ LimitSwitch::LimitSwitch(DigitalIn limitPin, bool ActiveHigh) : m_limitPin(limit
 
 // You are not writing to the Limit Switch so you only need to use the DigitalIn read function
 // Tells you if the Limit Switch has been pressed or not
+
 bool LimitSwitch::isPressed() {
-  if (!m_ActiveHigh) {
+  // Make sure Pin is connected before checking if it's an active high or active low
+  if (limitPin.is_connected()) {
+    if (!m_ActiveHigh) {
+      return !limitPin.read();
+    }
     return !limitPin.read();
   }
-  return !limitPin.read();
+  return false;
 }
