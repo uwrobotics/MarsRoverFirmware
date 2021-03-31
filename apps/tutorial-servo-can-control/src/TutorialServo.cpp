30b8d4360338ad25554031b91dfdc1d5aeabfe7b
Taylor <taylor.mcnabb3@gmail.com>
2021-03-31 10:54:09 -0400

    Training for firmware

--- apps/tutorial-servo-can-control/src/TutorialServo.cpp
+++ apps/tutorial-servo-can-control/src/TutorialServo.cpp
@@ -4,16 +4,16 @@
 #include "mbed.h"
 
 TutorialServo::TutorialServo(PinName servoPin, float servoRangeInDegrees = 180.0, float minPulsewidthInMs = 1,
-                             float maxPulsewidthInMs = 2) {
-  m_servoPwmOut = servoPin;
+                             float maxPulsewidthInMs = 2)
+    : m_servoPwmOut(servoPin),
+      m_servoRangeInDegrees(servoRangeInDegrees),
+      m_minPulsewidthInMs(minPulsewidthInMs),
+      m_maxPulsewidthInMs(maxPulsewidthInMs) {
   m_servoPwmOut.period_ms(20);
-  m_servoRangeInDegrees = servoRangeInDegrees;
-  m_minPulsewidthInMs   = minPulsewidthInMs;
-  m_maxPulsewidthInMs   = maxPulsewidthInMs;
 }
 
 void TutorialServo::setPositionInDegrees(const float degrees) {
-  float pulse = m_minPulsewidthInMs + (degrees / m_servoRangeInDegrees);
+  float pulse = (degrees * (m_maxPulsewidthInMs - m_minPulsewidthInMs) / m_servoRangeInDegrees) + m_minPulsewidthInMs;
 
   if (pulse > m_maxPulsewidthInMs)
     pulse = m_maxPulsewidthInMs;
