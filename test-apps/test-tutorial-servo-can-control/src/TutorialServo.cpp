cfe115266a608803e8f9b7584580d2c86e5109a8
Jahnavi <jthota2001@gmail.com>
2021-06-06 21:49:22 +0400

    fixed pulsewidth()

--- test-apps/test-tutorial-servo-can-control/src/TutorialServo.cpp
+++ test-apps/test-tutorial-servo-can-control/src/TutorialServo.cpp
@@ -20,17 +20,17 @@ float TutorialServo::getMaxPulseWidthInMs( ) const{
 void TutorialServo::setPositionInDegrees(const float degrees){
     if (degrees < 0){
         /*Out of Range. Setting to min*/
-        m_servoPwmOut.pulsewidth_ms(m_minPulsewidthInMs);
+        m_servoPwmOut.pulsewidth(m_minPulsewidthInMs * 0.001);
         return;
     }
     else if(degrees > m_servoRangeInDegrees){
         /*Out of Range. Setting to max*/
-        m_servoPwmOut.pulsewidth_ms(m_maxPulsewidthInMs);
+        m_servoPwmOut.pulsewidth(m_maxPulsewidthInMs * 0.001);
         return;
     }
     float slope = (m_maxPulsewidthInMs - m_minPulsewidthInMs)/m_servoRangeInDegrees;
-    float pulse = (m_minPulsewidthInMs + degrees*slope);
-    printf("Setting to %d",degrees);
-    m_servoPwmOut.pulsewidth_ms(pulse);
+    float pulse = (m_minPulsewidthInMs + degrees*slope) * 0.001;
+    /*Setting to degrees*/
+    m_servoPwmOut.pulsewidth(pulse);
     return;
 }
