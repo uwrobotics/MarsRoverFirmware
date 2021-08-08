992194c2a3859f9eed0ec26e2384bc1cbc88f2d5
Dhruv Upadhyay <dmupadhyay12@gmail.com>
2021-08-07 22:29:03 -0400

    Fixed implementation errors within sensor module header

--- rover-apps/science/include/AdafruitSTEMMAModule.h
+++ rover-apps/science/include/AdafruitSTEMMAModule.h
@@ -1,20 +1,19 @@
 #pragma once
 
+#include "AdafruitSTEMMA.h"
 #include "Module.h"
 #include "mbed.h"
-#include "AdafruitSTEMMAModule.h"
-#include "AdafruitSTEMMA.h"
 
 class AdafruitSTEMMAModule final : public Module {
-    public:
-     AdafruitSTEMMAModule(PinName sda, PinName scl);
+ public:
+  AdafruitSTEMMAModule(PinName sda, PinName scl);
 
-     void periodic_1s(void) override;
-     void periodic_10s(void) override {}
-     void periodic_100ms(void) override {}
-     void periodic_10ms(void) override {}
-     void periodic_1ms(void) override {}
-    
-    private:
-        AdafruitSTEMMA moistureSensor;
-}
+  void periodic_1s(void) override;
+  void periodic_10s(void) override;
+  void periodic_100ms(void) override;
+  void periodic_10ms(void) override;
+  void periodic_1ms(void) override;
+
+ private:
+  Sensor::AdafruitSTEMMA moistureSensor;
+};
