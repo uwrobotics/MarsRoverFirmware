b39e14c5b14128eead4a26015f34229fa4e7ba57
Dhruv Upadhyay <dmupadhyay12@gmail.com>
2021-08-07 22:30:36 -0400

    Fixed implementation errors in the moistureSensorModule src file

--- rover-apps/science/src/AdafruitSTEMMAModule.cpp
+++ rover-apps/science/src/AdafruitSTEMMAModule.cpp
@@ -1,25 +1,31 @@
-#include "AdafruitSTEMMAModule.h"
 #include <ratio>
+
+#include "AdafruitSTEMMAModule.h"
 #include "Module.h"
 #include "mbed.h"
-#include "AdafruitSTEMMA.h"
 
 AdafruitSTEMMAModule::AdafruitSTEMMAModule(PinName sda, PinName scl) : moistureSensor(sda, scl) {
-    static int numFails = 0;
-    bool resetStatus = false;
+  static int numFails = 0;
+  bool resetStatus    = false;
 
-    while (!resetStatus && numFails < 10) {
-        resetStatus = moistureSensor.reset();
-    }
-    // if still error then log
+  while (!resetStatus && numFails < 10) {
+    resetStatus = moistureSensor.reset();
+  }
+  // if still error then log through printf (currently), plan to later log through LOGGER (TODO)
+  printf("Cannot Reset Moisture Sensor");
 }
 
-void AdafruitSTEMMAModule::periodic_100ms(void) {
-    static int numFails = 0;
-    bool updateStatus = false;
+void AdafruitSTEMMAModule::periodic_1s(void) {}
+void AdafruitSTEMMAModule::periodic_10s(void) {}
+void AdafruitSTEMMAModule::periodic_10ms(void) {}
+void AdafruitSTEMMAModule::periodic_1ms(void) {}
 
-    while (!updateStatus && numFails < 10) {
-        updateStatus = moistureSensor.update();
-    }
-    // if still error then log
+void AdafruitSTEMMAModule::periodic_100ms(void) {
+  static int numFails = 0;
+  bool updateStatus   = false;
+
+  while (!updateStatus && numFails < 10) {
+    updateStatus = moistureSensor.update();
+  }
+  // if still error then log through printf (currently), plan to later log through LOGGER (TODO)
 }
