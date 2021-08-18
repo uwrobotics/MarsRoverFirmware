27418c377642ef88acb69328e8964b7331e74df6
jahnavithota2011 <34670246+jahnavithota2011@users.noreply.github.com>
2021-08-18 23:17:59 +0400

    Create watchdog module (#389)
    * Watchdog module
    
    * clang fix
    
    * clang fix2
    
    * test-watchdog
    
    * Edited WatchdogWrapper.h
    
    * Fixed pure virtual function errors
    
    * fixed namespace error
    
    * Fixed error2
    
    * Cmake
    
    * Cmake
    
    * Cmake fix
    
    * Top-level Cmake
    
    * Changes made
    
    * clang fix
    
    * Update libs/utility/src/WatchdogWrapper.cpp
    
    Co-authored-by: Cindy Li <cindyli0213@hotmail.com>
    
    * Update test-apps/test-watchdog/src/main.cpp
    
    Co-authored-by: Cindy Li <cindyli0213@hotmail.com>
    
    * cmake
    
    * Changed periodic function name
    
    * watchdogwrapper fix
    
    Co-authored-by: Cindy Li <cindyli0213@hotmail.com>

--- rover-apps/science/include/AppConfig.h
+++ rover-apps/science/include/AppConfig.h
@@ -3,7 +3,11 @@
 #include <vector>
 
 #include "Module.h"
+#include "WatchdogModule.h"
+
+WatchdogModule science_watchdog;
 
 std::vector<Module*> gModules = {
     // put modules here
+    &science_watchdog,
 };
--- test-apps/test-watchdog/src/main.cpp
+++ test-apps/test-watchdog/src/main.cpp
@@ -1,12 +1,21 @@
 #include "WatchdogWrapper.h"
 #include "mbed.h"
 
+Thread pet_thread;
 std::chrono::milliseconds countdown_ms = 1000ms;
 std::chrono::milliseconds pet_ms       = 200ms;
 
+void pet_dog_task(std::chrono::milliseconds *pet_ms) {
+  while (1) {
+    Utility::WatchdogWrapper::petWatchdog();
+    ThisThread::sleep_for(*pet_ms);
+  }
+}
+
 int main() {
   Utility::WatchdogWrapper::logResetReason();
-  Utility::WatchdogWrapper::startWatchdog(countdown_ms, pet_ms);
+  Utility::WatchdogWrapper::startWatchdog(countdown_ms);
+  pet_thread.start(callback(pet_dog_task, &pet_ms));
   ThisThread::sleep_for(2000ms);
   MBED_ASSERT(false);
 }
