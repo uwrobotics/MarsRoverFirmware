b7415002434d4ff444e6e68200f14123b4a25e8c
Aaryan Shroff <aaryan.shroff@uwaterloo.ca>
2021-09-08 10:50:57 +0530

    Fix for degrees out of servo range and clang-format applied

--- test-apps/tutorial-servo-can-control/src/TutorialServo.cpp
+++ test-apps/tutorial-servo-can-control/src/TutorialServo.cpp
@@ -1,23 +1,39 @@
 #include "TutorialServo.h"
+
 #include "mbed.h"
 
-TutorialServo::TutorialServo(PinName servoPin, float servoRangeInDegrees, float minPulsewidthInMs, float maxPulsewidthInMs) : m_servoPwmOut(servoPin), m_servoRangeInDegrees(servoRangeInDegrees), m_minPulsewidthInMs(minPulsewidthInMs), m_maxPulsewidthInMs(maxPulsewidthInMs) {
-        m_servoPwmOut.period_ms(20);
+TutorialServo::TutorialServo(PinName servoPin, float servoRangeInDegrees, float minPulsewidthInMs,
+                             float maxPulsewidthInMs)
+    : m_servoPwmOut(servoPin),
+      m_servoRangeInDegrees(servoRangeInDegrees),
+      m_minPulsewidthInMs(minPulsewidthInMs),
+      m_maxPulsewidthInMs(maxPulsewidthInMs) {
+  m_servoPwmOut.period_ms(20);
 }
 
 void TutorialServo::setPositionInDegrees(const float degrees) {
+  float pulsewidth;
 
-    m_servoPwmOut.pulsewidth_ms(m_minPulsewidthInMs + (degrees / m_servoRangeInDegrees) * (m_maxPulsewidthInMs - m_maxPulsewidthInMs));
+  if (degrees > m_servoRangeInDegrees) {
+    pulsewidth = m_maxPulsewidthInMs;
+  } else if (degrees < 0) {
+    pulsewidth = m_minPulsewidthInMs;
+  } else {
+    pulsewidth = =
+        m_minPulsewidthInMs + (degrees / m_servoRangeInDegrees) * (m_maxPulsewidthInMs - m_maxPulsewidthInMs);
+  }
+
+  m_servoPwmOut.pulsewidth_ms(pulsewidth);
 }
 
 float TutorialServo::getServoRangeInDegrees() const {
-    return m_servoRangeInDegrees;
+  return m_servoRangeInDegrees;
 }
 
 float TutorialServo::getMinPulseWidthInMs() const {
-    return m_minPulsewidthInMs;
+  return m_minPulsewidthInMs;
 }
 
 float TutorialServo::getMaxPulseWidthInMs() const {
-    return m_maxPulsewidthInMs;
+  return m_maxPulsewidthInMs;
 }
--- test-apps/tutorial-servo-can-control/src/main.cpp
+++ test-apps/tutorial-servo-can-control/src/main.cpp
@@ -2,15 +2,15 @@
 #include "TutorialServo.h"
 
 TutorialServo servo(PA_1);
-CAN           can(PB_8, PB_9);
-CANMsg        rxMsg;
-float         percent;
+CAN can(PB_8, PB_9);
+CANMsg rxMsg;
+float percent;
 
 int main() {
-    while (1) {
-        if (can.read(rxMsg)) {
-            rxMsg.getPayload(percent);
-            servo.setPositionInDegrees(percent * servo.getServoRangeInDegrees());
-        }
+  while (1) {
+    if (can.read(rxMsg)) {
+      rxMsg.getPayload(percent);
+      servo.setPositionInDegrees(percent * servo.getServoRangeInDegrees());
     }
+  }
 }
