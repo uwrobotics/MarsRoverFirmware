34e55a71393a97d88282e1c36d367f48234f20de
Cindy Li <cindyli0213@hotmail.com>
2021-10-17 11:30:40 -0700

    fix folder structure

--- rover-apps/pdb_2021/include/AppConfig.h
+++ rover-apps/pdb_2021/include/AppConfig.h
@@ -5,9 +5,9 @@
 #include "Module.h"
 #include "PDBMonitoring.h"
 
-PDBMonitoring PDB;
+PDBMonitoring PDB_monitoring;
 
 std::vector<Module*> gModules = {
     // put modules here
-    &PDB,
+    &PDB_monitoring,
 };
