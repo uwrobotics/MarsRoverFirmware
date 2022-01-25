4d99dae4c69b9828d13514a35198c292ec2a9d60
jessieramseyer <ramseyer.jessie@gmail.com>
2022-01-24 20:53:23 -0500

    Added clang formatting

--- test-apps/tutorial-servo-can-control/src/TutorialServo.cpp
+++ test-apps/tutorial-servo-can-control/src/TutorialServo.cpp
@@ -1,42 +1,35 @@
 #include "TutorialServo.hpp"
+
 #include "mbed.h"
 
-TutorialServo::TutorialServo(PinName servoPin, float servoRangeInDegrees, float
-  minPulsewidthInMs, float maxPulsewidthInMs) :
-  m_servoPwmOut(servoPin),
-  m_servoRangeInDegrees(servoRangeInDegrees),
-  m_minPulsewidthInMs(minPulsewidthInMs),
-  m_maxPulsewidthInMs(maxPulsewidthInMs)
-{
+TutorialServo::TutorialServo(PinName servoPin, float servoRangeInDegrees, float minPulsewidthInMs,
+                             float maxPulsewidthInMs)
+    : m_servoPwmOut(servoPin),
+      m_servoRangeInDegrees(servoRangeInDegrees),
+      m_minPulsewidthInMs(minPulsewidthInMs),
+      m_maxPulsewidthInMs(maxPulsewidthInMs) {
   m_servoPwmOut.period_ms(20);
 }
 
 void TutorialServo::setPositionInDegrees(const float degrees) {
-  
-  if(degrees > m_servoRangeInDegrees) {
-      m_servoPwmOut.pulsewidth(m_maxPulsewidthInMs/1000);
+  if (degrees > m_servoRangeInDegrees) {
+    m_servoPwmOut.pulsewidth(m_maxPulsewidthInMs / 1000);
+  } else if (degrees < 0) {
+    m_servoPwmOut.pulsewidth(m_minPulsewidthInMs / 1000);
+  } else {
+    m_servoPwmOut.pulsewidth(m_minPulsewidthInMs / 1000 +
+                             (degrees / (m_servoRangeInDegrees * 1000)) * (m_maxPulsewidthInMs - m_minPulsewidthInMs));
   }
-  else if (degrees < 0 ) {
-      m_servoPwmOut.pulsewidth(m_minPulsewidthInMs/1000);
-  }
-  else{
-      m_servoPwmOut.pulsewidth(m_minPulsewidthInMs/1000 + (degrees/(m_servoRangeInDegrees * 1000))*(m_maxPulsewidthInMs-m_minPulsewidthInMs));
-  }  
 }
 
-float TutorialServo::getServoRangeInDegrees( ) const {
-
+float TutorialServo::getServoRangeInDegrees() const {
   return m_servoRangeInDegrees;
 }
 
-float TutorialServo::getMinPulseWidthInMs( ) const {
-  
+float TutorialServo::getMinPulseWidthInMs() const {
   return m_minPulsewidthInMs;
 }
 
-float TutorialServo::getMaxPulseWidthInMs( ) const {
- 
+float TutorialServo::getMaxPulseWidthInMs() const {
   return m_maxPulsewidthInMs;
 }
-
-
--- test-apps/tutorial-servo-can-control/src/main.cpp
+++ test-apps/tutorial-servo-can-control/src/main.cpp
@@ -1,20 +1,19 @@
 #include "CANInterface.h"
-#include "hw_bridge.h"
 #include "TutorialServo.hpp"
+#include "hw_bridge.h"
 
 TutorialServo servo(PA_1);
 CANBus can(CAN1_RX, CAN1_TX);
 
-int main(){
-
+int main() {
   float percent, degrees;
   CANMsg rxMsg;
 
-  while(true) {
+  while (true) {
     can.read(rxMsg);
 
     rxMsg.getPayload(percent);
     degrees = servo.getServoRangeInDegrees();
-    servo.setPositionInDegrees(degrees*percent);
+    servo.setPositionInDegrees(degrees * percent);
   }
 }
