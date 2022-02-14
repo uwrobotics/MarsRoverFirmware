e7b06e4b61c0ca45962131315aee16d340363949
Nihal Sahu <nihal.sahu@uwaterloo.ca>
2022-02-13 20:31:09 -0700

    finished servo control by CAN and potentiometer

--- test-apps/tutorial-servo-can-control/src/TutorialServo.cpp
+++ test-apps/tutorial-servo-can-control/src/TutorialServo.cpp
@@ -3,13 +3,14 @@
 
 TutorialServo::TutorialServo(PinName servoPin, float servoRangeInDegrees, float minPulsewidthInMs, float maxPulsewidthInMs) {
     this->m_servoPwmOut = new PwmOut(servoPin);
+    this->m_servoPwmOut->period_ms(20);  //setting the PWM period to 20ms
     this->m_servoRangeInDegrees = servoRangeInDegrees;
     this->m_minPulsewidthInMs = minPulsewidthInMs;
     this->m_maxPulsewidthInMs = maxPulsewidthInMs;
 }
 
 void TutorialServo::setPositionInDegrees(const float degrees) {
-    m_servoPwmOut->pulsewidth(degrees / m_servoRangeInDegrees);
+    this->m_servoPwmOut->pulsewidth((1 + degrees/this->m_servoRangeInDegrees) / 1000);
 }
 
 float TutorialServo::getServoRangeInDegrees() const {
