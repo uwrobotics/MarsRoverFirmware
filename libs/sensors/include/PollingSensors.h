82fd81474e01d2231acce8b8541eff8e0d1cc40d
Jahnavi <jthota2001@gmail.com>
2022-02-28 21:06:34 -0500

    clang fix

--- libs/sensors/include/PollingSensors.h
+++ libs/sensors/include/PollingSensors.h
@@ -4,40 +4,40 @@
 #include "mbed.h"
 
 namespace Sensor {
-    class PollingSensors final : Sensor {
-    public:
-        PollingSensors();
+class PollingSensors final : Sensor {
+ public:
+  PollingSensors();
 
-//        float read() override;
-//
-//        float alternateRead() override;
-//
-//        bool getStatus() const override;
-//
-//        [[nodiscard]] bool reset() override;
-//
-//        [[nodiscard]] bool update() override;
+  //        float read() override;
+  //
+  //        float alternateRead() override;
+  //
+  //        bool getStatus() const override;
+  //
+  //        [[nodiscard]] bool reset() override;
+  //
+  //        [[nodiscard]] bool update() override;
 
-        /* Functions specific to sensor modules*/
+  /* Functions specific to sensor modules*/
 
-        /* This function returns Moisture sensor value
-         * 0-300 - dry
-         * 300-700 - humid
-         * 700-900 - wet
-         * Returns -1 for fault
-         * */
-        float moisture_monitoring();
+  /* This function returns Moisture sensor value
+   * 0-300 - dry
+   * 300-700 - humid
+   * 700-900 - wet
+   * Returns -1 for fault
+   * */
+  float moisture_monitoring();
 
-        /* This function returns CO2 sensor value in ppm\
-         * Returns -1 for fault and 0 for preheating
-         * */
-        float C02_monitoring();
+  /* This function returns CO2 sensor value in ppm\
+   * Returns -1 for fault and 0 for preheating
+   * */
+  float C02_monitoring();
 
-    private:
-        /*Pins configuration for moisture sensing*/
-        AnalogIn moisture_in_adc;
+ private:
+  /*Pins configuration for moisture sensing*/
+  AnalogIn moisture_in_adc;
 
-        /*Pins configuration for CO2 sensing*/
-        AnalogIn CO2_in_adc;
-    };
-}
+  /*Pins configuration for CO2 sensing*/
+  AnalogIn CO2_in_adc;
+};
+}  // namespace Sensor
--- libs/sensors/src/PollingSensors.cpp
+++ libs/sensors/src/PollingSensors.cpp
@@ -8,25 +8,25 @@ PollingSensors::PollingSensors() : moisture_in_adc(MOISTURE_IN), CO2_in_adc(CO2_
 
 // The function currently logs a status message. Can be changed to return just a float indicating the sensor value
 float moisture_monitoring() {
-    float moisture_reading = (moisture_in_adc.read_voltage() * 5.0) / 1023.0;
+  float moisture_reading = (moisture_in_adc.read_voltage() * 5.0) / 1023.0;
 
-    if (moisture_reading < 0 && moisture_reading > 900) {
-        Utility::logger << "!!! MOISTURE FAULT:" << moisture_reading << "\n";
-        return -1;
-    }
-    return moisture_reading;
+  if (moisture_reading < 0 && moisture_reading > 900) {
+    Utility::logger << "!!! MOISTURE FAULT:" << moisture_reading << "\n";
+    return -1;
+  }
+  return moisture_reading;
 }
 
 float C02_monitoring() {
-    float CO2_reading = CO2_in_adc.read_voltage() * 1000;
+  float CO2_reading = CO2_in_adc.read_voltage() * 1000;
 
-    if (CO2_reading <= 0) {
-        Utility::logger << "!!! CO2 FAULT:" << CO2_reading << "\n";
-        return -1;
-    } else if (CO2_reading < 400) {
-        Utility::logger << "!!! CO2 PREHEATING:" << CO2_reading << "\n";
-        return 0;
-    }
-    float concentration = ((CO2_reading - 400) * 50.0) / 16.0;
-    return concentration;
+  if (CO2_reading <= 0) {
+    Utility::logger << "!!! CO2 FAULT:" << CO2_reading << "\n";
+    return -1;
+  } else if (CO2_reading < 400) {
+    Utility::logger << "!!! CO2 PREHEATING:" << CO2_reading << "\n";
+    return 0;
+  }
+  float concentration = ((CO2_reading - 400) * 50.0) / 16.0;
+  return concentration;
 }
