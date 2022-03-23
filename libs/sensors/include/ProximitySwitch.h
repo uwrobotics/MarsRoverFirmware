c0296d1324d6ce3fe3e7acf2537dc90927137c49
Nihal Sahu <nihal.sahu@uwaterloo.ca>
2022-03-23 08:32:53 -0400

    applied clang formatting

--- libs/sensors/include/ProximitySwitch.h
+++ libs/sensors/include/ProximitySwitch.h
@@ -4,18 +4,18 @@
 #include "mbed.h"
 
 namespace Sensor {
-    class ProximitySwitch final : Sensor {
-        public:
-         ProximitySwitch(PinName proximity_in);
+class ProximitySwitch final : Sensor {
+ public:
+  ProximitySwitch(PinName proximity_in);
 
-        //read returns 1 for HIGH, and 0 for LOW
-        float read() override{};
-        float alternateRead() override{};
-        bool getStatus() const override{};
-        [[nodiscard]] bool reset() override;
-        [[nodiscard]] bool update() override;
+  // read returns 1 for HIGH, and 0 for LOW
+  float read() override{};
+  float alternateRead() override{};
+  bool getStatus() const override{};
+  [[nodiscard]] bool reset() override;
+  [[nodiscard]] bool update() override;
 
-        private:
-         AnalogIn m_proximity_in_adc;
-    };
+ private:
+  AnalogIn m_proximity_in_adc;
+};
 }  // namespace Sensor
--- libs/sensors/src/ProximitySwitch.cpp
+++ libs/sensors/src/ProximitySwitch.cpp
@@ -4,10 +4,8 @@ using namespace Sensor;
 
 ProximitySwitch::ProximitySwitch(PinName proximity_in) : m_proximity_in_adc(proximity_in) {}
 
-
 /*https://www.dfrobot.com/product-2025.html*/
-float ProximitySwitch::read()
-{
+float ProximitySwitch::read() {
   float proximity_reading = m_proximity_in_adc.read();
 
   return proximity_reading;
