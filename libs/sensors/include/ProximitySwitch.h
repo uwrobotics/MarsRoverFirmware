6d30e8c2cf38e14c3b8d5d19b380e97b27823686
Nihal Sahu <nihal.sahu@uwaterloo.ca>
2022-04-04 15:19:21 -0400

    added more fixes

--- libs/sensors/include/ProximitySwitch.h
+++ libs/sensors/include/ProximitySwitch.h
@@ -9,11 +9,11 @@ class ProximitySwitch final : Sensor {
   ProximitySwitch(PinName proximity_in);
 
   // read() returns 1 for HIGH, 0 for LOW, and 2 for ERROR
-  float read() override
-  float alternateRead() override{}
-  bool getStatus() const override{}
-  [[nodiscard]] bool reset() override{}
-  [[nodiscard]] bool update() override{}
+  float read() override;
+  float alternateRead() override {}
+  bool getStatus() const override {}
+  [[nodiscard]] bool reset() override {}
+  [[nodiscard]] bool update() override {}
 
  private:
   AnalogIn m_proximity_in_adc;
