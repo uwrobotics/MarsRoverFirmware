514b07b95c77dc951df31222321ed1e91f356d80
Nihal Sahu <nihal.sahu@uwaterloo.ca>
2022-04-04 14:48:33 -0400

    added fixes

--- libs/sensors/include/ProximitySwitch.h
+++ libs/sensors/include/ProximitySwitch.h
@@ -8,12 +8,12 @@ class ProximitySwitch final : Sensor {
  public:
   ProximitySwitch(PinName proximity_in);
 
-  // read returns 1 for HIGH, and 0 for LOW
-  float read() override{};
-  float alternateRead() override{};
-  bool getStatus() const override{};
-  [[nodiscard]] bool reset() override;
-  [[nodiscard]] bool update() override;
+  // read() returns 1 for HIGH, 0 for LOW, and 2 for ERROR
+  float read() override
+  float alternateRead() override{}
+  bool getStatus() const override{}
+  [[nodiscard]] bool reset() override{}
+  [[nodiscard]] bool update() override{}
 
  private:
   AnalogIn m_proximity_in_adc;
--- libs/sensors/src/ProximitySwitch.cpp
+++ libs/sensors/src/ProximitySwitch.cpp
@@ -1,12 +1,17 @@
 #include "ProximitySwitch.h"
 
+#include "Logger.h"
+
 using namespace Sensor;
 
 ProximitySwitch::ProximitySwitch(PinName proximity_in) : m_proximity_in_adc(proximity_in) {}
 
 /*https://www.dfrobot.com/product-2025.html*/
 float ProximitySwitch::read() {
-  float proximity_reading = m_proximity_in_adc.read();
-
-  return proximity_reading;
+  if (m_proximity_in_adc.is_connected()) {
+    return m_proximity_in_adc.read()
+  } else {
+    Utility::logger << "ERROR: Sensor pin not connected";
+    return 2;
+  }
 }
