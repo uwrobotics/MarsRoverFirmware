0d85b9f9c66dba3aaeb1233a480da541b870db58
Anderson Hsieh <andy13579246810@gmail.com>
2022-04-13 00:57:31 -0400

    add clang-formatting

--- test-apps/tutorial-servo-can-control/src/main.cpp
+++ test-apps/tutorial-servo-can-control/src/main.cpp
@@ -1,30 +1,29 @@
-#include "mbed.h"
-#include "TutorialServo.h"
 #include "CANMsg.h"
+#include "TutorialServo.h"
+#include "mbed.h"
 
-//servo PWM control pin is connected to PA_1
+// servo PWM control pin is connected to PA_1
 PwmOut servoPwmOut(PA_1);
 
-//assume RX pin is PB_8 and TX pin is PB_9
+// assume RX pin is PB_8 and TX pin is PB_9
 CAN can(PB_8, PB_9);
 
-//note that txMsg is not declared since I'm not sending packets
+// note that txMsg is not declared since I'm not sending packets
 CANMsg rxMsg;
 
-int main(){
-    
-    //pass no paramter to TutorialServo's constructor, using 1ms ~ 2ms and 0 ~ 180 degrees as the range mapping
-    TutorialServo servo = TutorialServo();
-    float rangeOfMotionPercent;
+int main() {
+  // pass no paramter to TutorialServo's constructor, using 1ms ~ 2ms and 0 ~ 180 degrees as the range mapping
+  TutorialServo servo = TutorialServo();
+  float rangeOfMotionPercent;
 
-    while(1){
-        can.read(rxMsg);
+  while (1) {
+    can.read(rxMsg);
 
-        //a float is four byte, use >> operator to map it directly to rangeOfMotionPercent
-        rxMsg >> rangeOfMotionPercent;
+    // a float is four byte, use >> operator to map it directly to rangeOfMotionPercent
+    rxMsg >> rangeOfMotionPercent;
 
-        servo.setPositionInDegrees(rxMsg);
-    }
+    servo.setPositionInDegrees(rxMsg);
+  }
 
-    return 0;
+  return 0;
 }
--- test-apps/tutorial-servo-pot-control/src/main.cpp
+++ test-apps/tutorial-servo-pot-control/src/main.cpp
@@ -1,16 +1,16 @@
-#include  "mbed.h"
+#include "mbed.h"
 
 AnalogIn potVoltageIn(PA_0);
 PwmOut servoPwmOut(PA_1);
 
-int main(){
-		//set servo digital signal's period to 20ms
-		servoPwnOut.period_ms(20);
-	while(1){
-		//0.0 ~ 3.3V
-		float potVoltage = potVoltageIn.read();
+int main() {
+  // set servo digital signal's period to 20ms
+  servoPwnOut.period_ms(20);
+  while (1) {
+    // 0.0 ~ 3.3V
+    float potVoltage = potVoltageIn.read();
 
-		//map 0.0 ~ 3.3 V to 1ms ~ 2ms duty cycle of servo's digital signal
-		servoPwmOut.pulsewidth( ( 1 + potVoltage/3.3 ) / 1000 );
-	}
+    // map 0.0 ~ 3.3 V to 1ms ~ 2ms duty cycle of servo's digital signal
+    servoPwmOut.pulsewidth((1 + potVoltage / 3.3) / 1000);
+  }
 }
